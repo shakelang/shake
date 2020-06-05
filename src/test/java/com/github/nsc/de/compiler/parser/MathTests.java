b54665c2bfd2a7b3d9902af8e364a99598a4751e
Nicolas <nicoleo.mymail@gmail.com>
2020-06-05 22:20:33 +0200

    updated math parser tests and created logical parser tests

--- src/test/java/com/github/nsc/de/compiler/parser/MathTests.java
+++ src/test/java/com/github/nsc/de/compiler/parser/MathTests.java
@@ -1,6 +1,10 @@
 package com.github.nsc.de.compiler.parser;
 
 import com.github.nsc.de.compiler.parser.node.expression.*;
+import com.github.nsc.de.compiler.parser.node.logical.LogicalAndNode;
+import com.github.nsc.de.compiler.parser.node.logical.LogicalFalseNode;
+import com.github.nsc.de.compiler.parser.node.logical.LogicalOrNode;
+import com.github.nsc.de.compiler.parser.node.logical.LogicalTrueNode;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,15 +42,63 @@ public class MathTests {
         testBasic("10 ** 3", 10, 3, PowNode.class);
 
     }
-    private <T extends ExpressionNode> void testBasic(String input, double left, double right, Class<T> type) {
 
-        T pow = parseSingle('<'+type.getSimpleName()+"Test>", input, type);
+    @Test
+    public void testPointBeforeLine() {
+
+        AddNode add = parseSingle("<PointBeforeLineTest>", "1 + 2 * 3 ^ 4", AddNode.class);
+        assertNotNull(add.getLeft());
+        assertType(NumberNode.class, add.getLeft());
+        assertEquals(1, ((NumberNode) add.getLeft()).getNumber());
+        assertNotNull(add.getRight());
+        assertType(MulNode.class, add.getRight());
+
+        MulNode mul = (MulNode) add.getRight();
+        assertNotNull(mul.getLeft());
+        assertType(NumberNode.class, mul.getLeft());
+        assertEquals(2, ((NumberNode) mul.getLeft()).getNumber());
+        assertNotNull(mul.getRight());
+        assertType(PowNode.class, mul.getRight());
+
+        PowNode pow = (PowNode) mul.getRight();
         assertNotNull(pow.getLeft());
         assertType(NumberNode.class, pow.getLeft());
-        assertEquals(left, ((NumberNode) pow.getLeft()).getNumber());
+        assertEquals(3, ((NumberNode) pow.getLeft()).getNumber());
         assertNotNull(pow.getRight());
         assertType(NumberNode.class, pow.getRight());
-        assertEquals(right, ((NumberNode) pow.getRight()).getNumber());
+        assertEquals(4, ((NumberNode) pow.getRight()).getNumber());
+
+    }
+
+    @Test
+    public void testBrackets() {
+
+        MulNode node = parseSingle("<BracketTest>", "2 * (4 + 3)", MulNode.class);
+        assertNotNull(node.getLeft());
+        assertType(NumberNode.class, node.getLeft());
+        assertEquals(2, ((NumberNode) node.getLeft()).getNumber());
+        assertNotNull(node.getRight());
+        assertType(AddNode.class, node.getRight());
+
+        AddNode add = (AddNode) node.getRight();
+        assertNotNull(add.getLeft());
+        assertType(NumberNode.class, add.getLeft());
+        assertEquals(4, ((NumberNode) add.getLeft()).getNumber());
+        assertNotNull(add.getRight());
+        assertType(NumberNode.class, add.getRight());
+        assertEquals(3, ((NumberNode) add.getRight()).getNumber());
+
+    }
+
+    private <T extends ExpressionNode> void testBasic(String input, double left, double right, Class<T> type) {
+
+        T node = parseSingle('<'+type.getSimpleName().substring(type.getSimpleName().length() - 4)+"Test>", input, type);
+        assertNotNull(node.getLeft());
+        assertType(NumberNode.class, node.getLeft());
+        assertEquals(left, ((NumberNode) node.getLeft()).getNumber());
+        assertNotNull(node.getRight());
+        assertType(NumberNode.class, node.getRight());
+        assertEquals(right, ((NumberNode) node.getRight()).getNumber());
 
     }
 }
