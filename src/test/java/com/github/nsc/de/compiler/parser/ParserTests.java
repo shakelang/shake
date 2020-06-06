350490297631b36096759852a999e6be014aacdd
Nicolas <nicoleo.mymail@gmail.com>
2020-06-06 11:10:03 +0200

    updated tests
    added multi statement test
    added while test
    
    updated variable declaration test
    updated variable assignment test

--- src/test/java/com/github/nsc/de/compiler/parser/ParserTests.java
+++ src/test/java/com/github/nsc/de/compiler/parser/ParserTests.java
@@ -2,8 +2,11 @@ package com.github.nsc.de.compiler.parser;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static com.github.nsc.de.compiler.parser.ParserTestUtil.*;
+import static com.github.nsc.de.compiler.TestUtil.*;
 
 import com.github.nsc.de.compiler.parser.node.*;
+import com.github.nsc.de.compiler.parser.node.expression.NumberNode;
+import com.github.nsc.de.compiler.parser.node.logical.LogicalTrueNode;
 import org.junit.jupiter.api.Test;
 
 public class ParserTests {
@@ -11,67 +14,71 @@ public class ParserTests {
     @Test
     public void testVariableAssignment() {
 
-        Tree tree = parse("<VariableAssignmentTest>",
-                "i = 0; i2 = 0.1;");
+        VariableAssignmentNode node = parseSingle("<VariableAssignmentTest>", "i = 0", VariableAssignmentNode.class);
+        assertEquals("i", node.getName());
+        assertNotNull(node.getValue());
+        assertType(NumberNode.class, node.getValue());
 
-        Node[] nodes = tree.getChildren();
+        node = parseSingle("<VariableAssignmentTest>", "i = 0.1", VariableAssignmentNode.class);
+        assertEquals("i", node.getName());
+        assertNotNull(node.getValue());
+        assertType(NumberNode.class, node.getValue());
 
-        // Result should be 2 nodes
-        assertEquals(2, nodes.length);
-
-        // Result Nodes should be VariableDeclarationNodes
-        assertTrue(nodes[0] instanceof VariableAssignmentNode);
-        assertTrue(nodes[1] instanceof VariableAssignmentNode);
-
-        // Variable names
-        assertEquals("i", ((VariableAssignmentNode) nodes[0]).getName());
-        assertEquals("i2", ((VariableAssignmentNode) nodes[1]).getName());
+        node = parseSingle("<VariableAssignmentTest>", "i = true", VariableAssignmentNode.class);
+        assertEquals("i", node.getName());
+        assertNotNull(node.getValue());
+        assertType(LogicalTrueNode.class, node.getValue());
 
     }
 
     @Test
     public void testVariableDeclaration() {
 
-        Tree tree = parse("<VariableDeclarationTest>",
-                "var i; var i2 = 0;");
+        VariableDeclarationNode node = parseSingle("<VariableDeclarationTest>", "var i", VariableDeclarationNode.class);
+        assertEquals("i", node.getName());
+        assertNull(node.getAssignment());
 
-        Node[] nodes = tree.getChildren();
-
-        // Result should be 2 nodes
-        assertEquals(2, nodes.length);
-
-        // Result Nodes should be VariableDeclarationNodes
-        assertTrue(nodes[0] instanceof VariableDeclarationNode);
-        assertTrue(nodes[1] instanceof VariableDeclarationNode);
-
-        // Variable names
-        assertEquals("i", ((VariableDeclarationNode) nodes[0]).getName());
-        assertEquals("i2", ((VariableDeclarationNode) nodes[1]).getName());
-
-        // First declaration has no assignment
-        assertNull(((VariableDeclarationNode) nodes[0]).getAssignment());
-
-        // Second declaration has an assignment
-        assertNotNull(((VariableDeclarationNode) nodes[1]).getAssignment());
+        node = parseSingle("<VariableDeclarationTest>", "var i = 0", VariableDeclarationNode.class);
+        assertEquals("i", node.getName());
+        assertNotNull(node.getAssignment());
 
     }
 
     @Test
     public void testVariableUsage() {
 
-        Tree tree = parse("<VariableUsageTest>", "i");
-
-        Node[] nodes = tree.getChildren();
-
-        // Result should be 1 node
-        assertEquals(1, nodes.length);
-
-        // Result Node should be VariableUsageNode
-        assertTrue(nodes[0] instanceof VariableUsageNode);
+        VariableUsageNode node = parseSingle("<VariableUsageTest>", "i", VariableUsageNode.class);
 
         // Variable names
-        assertEquals("i", ((VariableUsageNode) nodes[0]).getName());
+        assertEquals("i", node.getName());
 
+    }
+
+    @Test
+    public void testMultiStatement() {
+
+        Tree node = parse("<MultiStatementTest>", "var i; i");
+
+        assertEquals(2, node.getChildren().length);
+        assertType(VariableDeclarationNode.class, node.getChildren()[0]);
+        assertType(VariableUsageNode.class, node.getChildren()[1]);
+
+        node = parse("<MultiStatementTest>", "var i\ni");
+
+        assertEquals(2, node.getChildren().length);
+        assertType(VariableDeclarationNode.class, node.getChildren()[0]);
+        assertType(VariableUsageNode.class, node.getChildren()[1]);
+
+    }
+
+    @Test
+    public void testWhile() {
+        WhileNode node = parseSingle("<WhileTest>", "while(true) { i }", WhileNode.class);
+        assertNotNull(node.getCondition());
+        assertNotNull(node.getBody());
+
+        assertType(LogicalTrueNode.class, node.getCondition());
+        assertType(Tree.class, node.getBody());
     }
 
 }
