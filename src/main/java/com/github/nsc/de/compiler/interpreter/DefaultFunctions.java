8b27fd97840e721051f815ced26bea3d4dfcf9bd
Nicolas <nicoleo.mymail@gmail.com>
2020-08-24 16:38:38 +0200

    Added println command

--- src/main/java/com/github/nsc/de/compiler/interpreter/DefaultFunctions.java
+++ src/main/java/com/github/nsc/de/compiler/interpreter/DefaultFunctions.java
@@ -12,6 +12,7 @@ public class DefaultFunctions {
         HashMap<String, Variable> functions = new HashMap<>();
 
         functions.put("print", new Variable("print", VariableType.FUNCTION, new Print(interpreter)));
+        functions.put("println", new Variable("println", VariableType.FUNCTION, new Println(interpreter)));
 
         return new VariableList(functions);
     }
@@ -24,12 +25,28 @@ public class DefaultFunctions {
 
         @Override
         public void call(FunctionCallNode node, Scope scope) {
-            String out = "";
-            for(int i = 0; i < node.getArgs().length-1; i++) {
-                out += this.getInterpreter().visit(node.getArgs()[i]).getValue() + ", ";
-            }
-            if(node.getArgs().length > 0) out += this.getInterpreter().visit(node.getArgs()[node.getArgs().length - 1]).getValue();
-            System.out.println(out);
+            System.out.print(formatPrintArgs(node, this.getInterpreter()));
         }
+    }
+
+    private static class Println extends Function {
+
+        public Println(Interpreter interpreter) {
+            super(new FunctionArgumentNode[] {}, null, null, interpreter);
+        }
+
+        @Override
+        public void call(FunctionCallNode node, Scope scope) {
+            System.out.println(formatPrintArgs(node, this.getInterpreter()));
+        }
+    }
+
+    private static String formatPrintArgs(FunctionCallNode node, Interpreter interpreter) {
+        StringBuilder out = new StringBuilder();
+        for(int i = 0; i < node.getArgs().length-1; i++) {
+            out.append(interpreter.visit(node.getArgs()[i]).getValue() + ", ");
+        }
+        if(node.getArgs().length > 0) out.append(interpreter.visit(node.getArgs()[node.getArgs().length - 1]).getValue());
+        return out.toString();
     }
 }
