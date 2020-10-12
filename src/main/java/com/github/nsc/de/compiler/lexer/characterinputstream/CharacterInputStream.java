ccbb6eb8c456e86959b2c56c01f918f6f00f5d65
Nicolas <nicoleo.mymail@gmail.com>
2020-10-12 18:03:10 +0200

    Added documentation for the CharacterInputStreams

--- src/main/java/com/github/nsc/de/compiler/lexer/characterinputstream/CharacterInputStream.java
+++ src/main/java/com/github/nsc/de/compiler/lexer/characterinputstream/CharacterInputStream.java
@@ -4,16 +4,103 @@ import com.github.nsc.de.compiler.lexer.Position;
 
 public interface CharacterInputStream {
 
+    /**
+     * Returns the source (mostly file) of the {@link CharacterInputStream}
+     *
+     * @return the source (mostly file) of the {@link CharacterInputStream}
+     *
+     * @author Nicolas Schmidt
+     */
     String getSource();
+
+    /**
+     * Returns the chars of the {@link CharacterInputStream}
+     *
+     * @return the chars of the {@link CharacterInputStream}
+     *
+     * @author Nicolas Schmidt
+     */
     char[] getContent();
+
+    /**
+     * Returns the actual position of the {@link CharacterInputStream}
+     *
+     * @return the actual position of the {@link CharacterInputStream}
+     *
+     * @author Nicolas Schmidt
+     */
     Position getPosition();
+
+    /**
+     * Checks if the {@link CharacterInputStream} has a next character left
+     *
+     * @return if the {@link CharacterInputStream} has a next character
+     *
+     * @author Nicolas Schmidt
+     */
     boolean hasNext();
-    Character next();
+
+    /**
+     * Checks if the {@link CharacterInputStream} has a given number of characters left
+     *
+     * @param number the num of characters to check
+     * @return if the {@link CharacterInputStream} has a given number of characters left
+     *
+     * @author Nicolas Schmidt
+     */
+    boolean has(int number);
+
+    /**
+     * Returns the next character and continues to the next token
+     *
+     * @return the next character
+     *
+     * @author Nicolas Schmidt
+     */
+    char next();
+
+    /**
+     * Skips a given number of characters
+     *
+     * @param number the number of characters to skip
+     *
+     * @author Nicolas Schmidt
+     */
     void skip(int number);
+
+    /**
+     * Skips the next character
+     *
+     * @author Nicolas Schmidt
+     */
     void skip();
-    Character actual();
-    default Character peek() { return peek(1); }
-    Character peek(int num);
-    String peek(int num, int num2);
+
+    /**
+     * Returns the actual character (the same as returned by {@link #next()} when used before)
+     *
+     * @return the actual character
+     *
+     * @author Nicolas Schmidt
+     */
+    char actual();
+
+    /**
+     * Gives back the next character without skipping
+     *
+     * @return the next character
+     *
+     * @author Nicolas Schmidt
+     */
+    default char peek() { return peek(1); }
+
+    /**
+     * Gives back a character of the {@link CharacterInputStream}
+     * (relative to the actual position)
+     *
+     * @return the character at the requested position
+     *
+     * @author Nicolas Schmidt
+     */
+    char peek(int num);
 
 }
--- src/main/java/com/github/nsc/de/compiler/lexer/characterinputstream/StringCharacterInputStream.java
+++ src/main/java/com/github/nsc/de/compiler/lexer/characterinputstream/StringCharacterInputStream.java
@@ -4,70 +4,260 @@ import com.github.nsc.de.compiler.lexer.Position;
 
 public class StringCharacterInputStream implements CharacterInputStream {
 
+    /**
+     * The source (mostly file) of the {@link StringCharacterInputStream}
+     */
     private final String source;
+
+    /**
+     * The characters of the {@link StringCharacterInputStream}
+     */
     private final char[] content;
+
+    /**
+     * The actual position of the {@link StringCharacterInputStream}
+     */
     private final Position position;
 
+
+    /**
+     * Constructor for {@link StringCharacterInputStream} with given position
+     *
+     * @param source the source (mostly file) of the characters
+     * @param content the content characters
+     * @param position the starting position
+     *
+     * @author Nicolas Schmidt
+     */
     public StringCharacterInputStream(String source, String content, Position position) {
+        // Set fields
         this.source = source;
-        this.content = content.replaceAll("\r\n", "\n").toCharArray();
+        this.content = content.toCharArray();
         this.position = position;
+
+        // Throw an error if the input content contains a dos-style line-separator
+        if(content.contains("\r\n")) throw new Error("Using this constructor you must not give a string that contains \"\\r\\n\" as line-separator");
+
+        // Throw an error if the given source and content are not equal to the values of the position
+        if(!source.equals(position.getSource())) throw new Error("The source of the given position and the given source should be the same");
+        if(!content.equals(position.getContent())) throw new Error("The content of the given position and the given content should be the same");
     }
 
+    /**
+     * Constructor for {@link StringCharacterInputStream} with default position (starting from the start)
+     *
+     * @param source the source (mostly file) of the characters
+     * @param content the content characters
+     *
+     * @author Nicolas Schmidt
+     */
     public StringCharacterInputStream(String source, String content) {
-        this(source, content, new Position(source, content));
+
+        // Replace windows line-separators with linux line-separators
+        content = content.replaceAll("\r\n", "\n");
+
+        // Set fields
+        this.source = source;
+        this.content = content.toCharArray();
+        this.position = new Position(this.source, content);
     }
 
+
+    /**
+     * Returns the source (mostly file) of the {@link CharacterInputStream}
+     *
+     * @return the source (mostly file) of the {@link CharacterInputStream}
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#getSource()
+     * @see StringCharacterInputStream#getContent()
+     * @see StringCharacterInputStream#getPosition()
+     */
+    @Override
+    public String getSource() {
+        // Return the source of the StringCharacterInputStream
+        return source;
+    }
+
+
+    /**
+     * Returns the chars of the {@link CharacterInputStream}
+     *
+     * @return the chars of the {@link CharacterInputStream}
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#getContent()
+     * @see StringCharacterInputStream#getSource()
+     * @see StringCharacterInputStream#getPosition()
+     */
+    @Override
+    public char[] getContent() {
+        // Return the content of the StringCharacterInputStream
+        return content;
+    }
+
+
+    /**
+     * Returns the actual position of the {@link CharacterInputStream}
+     *
+     * @return the actual position of the {@link CharacterInputStream}
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#getPosition()
+     * @see StringCharacterInputStream#getContent()
+     * @see StringCharacterInputStream#getSource()
+     */
+    @Override
+    public Position getPosition() {
+        // Return the actual position of the StringCharacterInputStream
+        return this.position.copy();
+    }
+
+
+    /**
+     * Checks if the {@link CharacterInputStream} has a next character left
+     *
+     * @return if the {@link CharacterInputStream} has a next character
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#hasNext()
+     * @see StringCharacterInputStream#has(int number)
+     */
     @Override
     public boolean hasNext() {
-        return this.position.getIndex() < this.content.length-1;
+        // We could also use has(1) here, but for performance reasons that should be better
+        return this.position.getIndex() + 1 < this.content.length;
     }
 
+
+    /**
+     * Checks if the {@link CharacterInputStream} has a given number of characters left
+     *
+     * @param number the num of characters to check
+     * @return if the {@link CharacterInputStream} has a given number of characters left
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#has(int number)
+     * @see StringCharacterInputStream#hasNext()
+     */
     @Override
-    public Character next() {
-        skip();
-        return this.content[this.position.getIndex()];
+    public boolean has(int number) {
+        // throw an error, if the given number is smaller than 1
+        if(number < 1) throw new Error("The given number must be 1 or bigger");
+        return this.position.getIndex() + number < this.content.length;
     }
 
+
+    /**
+     * Returns the next character and continues to the next token
+     *
+     * @return the next character
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#next()
+     */
+    @Override
+    public char next() {
+        // Skip and return the actual character
+        skip();
+        return actual();
+    }
+
+
+    /**
+     * Skips a given number of characters
+     *
+     * @param number the number of characters to skip
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#skip(int number)
+     * @see StringCharacterInputStream#skip()
+     */
+    @Override
+    public void skip(int number) {
+        // Skip as many times, as required
+        for(int i = 0; i < number; i++) skip();
+    }
+
+
+    /**
+     * Skips the next character
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#skip()
+     * @see StringCharacterInputStream#skip(int number)
+     */
     @Override
     public void skip() {
+        // if the actual position is a line-separator go to next line, if not then to next column
         if(this.peek() == '\n') this.position.nextLine();
         else this.position.nextColumn();
     }
 
+
+    /**
+     * Returns the actual character (the same as returned by {@link #next()} when used before)
+     *
+     * @return the actual character
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#actual()
+     */
     @Override
-    public void skip(int number) {
-        for(int i = 0; i < number; i++) skip();
+    public char actual() {
+        // return the character at the actual position
+        return this.content[this.position.getIndex()];
     }
 
+
+    /**
+     * Gives back the next character without skipping
+     *
+     * @return the next character
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#peek()
+     * @see StringCharacterInputStream#peek(int num)
+     */
     @Override
-    public Character actual() {
-        return peek(0);
+    public char peek() {
+        // we could also use peek(1) here, but for performance reasons a direct implementation is better
+        // throw an error if the StringCharacterInputStream has not enough tokens left
+        if(!hasNext()) throw new Error("Not enough characters left");
+
+        // return the content at the required position
+        return this.content[this.position.getIndex() + 1];
     }
 
-    @Override
-    public Character peek(int num) {
-        return this.position.getIndex() + num < this.content.length ? this.content[this.position.getIndex() + num] : null;
-    }
 
+    /**
+     * Gives back the next character without skipping
+     *
+     * @return the next character
+     *
+     * @author Nicolas Schmidt
+     *
+     * @see CharacterInputStream#peek(int num)
+     * @see StringCharacterInputStream#peek()
+     */
     @Override
-    public String peek(int num1, int num2) {
-        return this.position.getIndex() + num1 < this.content.length && this.position.getIndex() + num2 < this.content.length ?
-                new String(this.content).substring(this.position.getIndex() + num1, this.position.getIndex() + num2 + 1) : "";
-    }
+    public char peek(int num) {
+        // throw an error if the StringCharacterInputStream has not enough tokens left
+        // (this will also automatically throw an error, if a number that is smaller than 1 is given as input, because
+        // of the implementation in the has method)
+        if(!has(num)) throw new Error("Not enough characters left");
 
-    @Override
-    public Position getPosition() {
-        return this.position.copy();
-    }
-
-    @Override
-    public String getSource() {
-        return source;
-    }
-
-    @Override
-    public char[] getContent() {
-        return content;
+        // return the content at the required position
+        return this.content[this.position.getIndex() + num];
     }
 }
