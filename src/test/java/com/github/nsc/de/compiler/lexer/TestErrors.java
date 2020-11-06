a9efbdc28d081761083f22d3bedc1092c342cf83
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2020-11-06 17:33:11 +0100

    Replaced character 'Ü' with unicode in TestErrors.java

--- src/test/java/com/github/nsc/de/compiler/lexer/TestErrors.java
+++ src/test/java/com/github/nsc/de/compiler/lexer/TestErrors.java
@@ -90,7 +90,7 @@ public class TestErrors {
         // Unexpected Token
         Lexer.LexerError error = assertThrows(Lexer.LexerError.class, () -> {
 
-            CharacterInputStream chars = new StringCharacterInputStream("<tests>", "ü");
+            CharacterInputStream chars = new StringCharacterInputStream("<tests>", "\u00dc");
             Lexer lexer = new Lexer(chars);
             lexer.makeTokens();
 
@@ -100,10 +100,10 @@ public class TestErrors {
 
         assertSame(0, error.getStart().getIndex());
         assertSame(0, error.getEnd().getIndex());
-        assertEquals("Unrecognised Token: 'ü'", error.getDetails());
+        assertEquals("Unrecognised Token: '\u00dc'", error.getDetails());
         assertEquals("UnexpectedTokenError", error.getName());
         assertEquals("<tests>:1:1", error.getMarker().getSource());
-        assertEquals("1  ü", error.getMarker().getPreview());
+        assertEquals("1  \u00dc", error.getMarker().getPreview());
         assertEquals("   ^", error.getMarker().getMarker());
 
         System.out.println(Formatting.FGColor.GREEN + "\u2705 Correct error was thrown" + Formatting.RESET);
