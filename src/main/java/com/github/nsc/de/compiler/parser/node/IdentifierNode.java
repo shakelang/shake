b9359c7816f672164ed3da1c41fafd061d5ebb5a
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2020-12-02 17:54:53 +0100

    Created toString for IdentifierNode

--- src/main/java/com/github/nsc/de/compiler/parser/node/IdentifierNode.java
+++ src/main/java/com/github/nsc/de/compiler/parser/node/IdentifierNode.java
@@ -22,4 +22,12 @@ public class IdentifierNode implements ValuedNode {
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return "IdentifierNode{" +
+                "parent=" + parent +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
