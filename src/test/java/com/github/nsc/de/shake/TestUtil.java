fd5016a80ba2df6e5eb1db27987520bc613999dd
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2021-05-19 23:30:34 +0200

    Updated CompilerError test and TestUtil to be written in kotlin

--- src/test/java/com/github/nsc/de/shake/TestUtil.java
+++
@@ -1,9 +0,0 @@
-package com.github.nsc.de.shake;
-
-import static org.junit.jupiter.api.Assertions.assertSame;
-
-public class TestUtil {
-
-    public static void assertType(Class<?> expected, Object actual) { assertSame(expected, actual.getClass()); }
-
-}
