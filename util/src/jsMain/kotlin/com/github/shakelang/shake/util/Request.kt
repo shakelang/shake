b70603344fe016dc57c5fa73b9e87458faab5f09
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2021-06-16 14:40:21 +0200

    Created request library for file-requests in browser

---
+++ util/src/jsMain/kotlin/com/github/shakelang/shake/util/Request.kt
@@ -0,0 +1,43 @@
+package com.github.shakelang.shake.util
+
+import org.w3c.xhr.XMLHttpRequest
+import kotlin.js.Promise
+
+
+@Suppress("unused")
+object Request {
+
+    fun request(method: RequestMethod, url: String): Promise<XMLHttpRequest> {
+        return Promise {
+            rs, rj ->
+            run {
+                var xhttp = XMLHttpRequest()
+                xhttp.open(method.toString(), url, true);
+                xhttp.onreadystatechange = {
+                    rs(xhttp)
+                }
+            }
+        }
+    }
+
+    fun get(url: String) = request(RequestMethod.GET, url)
+    fun post(url: String) = request(RequestMethod.POST, url)
+
+    fun requestString(method: RequestMethod, url: String) = request(method, url).then { it.responseText }
+    fun getString(url: String) = requestString(RequestMethod.GET, url)
+    fun postString(url: String) = requestString(RequestMethod.POST, url)
+
+    fun requestXml(method: RequestMethod, url: String) = request(method, url).then { it.responseXML }
+    fun getXml(url: String) = requestXml(RequestMethod.GET, url)
+    fun postXml(url: String) = requestXml(RequestMethod.POST, url)
+
+    fun requestJson(method: RequestMethod, url: String): Promise<dynamic> = requestString(method, url).then { JSON.parse(it) }
+    fun getJson(url: String) = requestJson(RequestMethod.GET, url)
+    fun postJson(url: String) = requestJson(RequestMethod.POST, url)
+
+    enum class RequestMethod {
+        GET, POST;
+        override fun toString(): String = name
+    }
+
+}
