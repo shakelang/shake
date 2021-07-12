94db62dcb7055e826860845bcf0ab84680f1be87
Minimine <64435955+nsc-de@users.noreply.github.com>
2021-07-12 22:27:53 +0200

    :pencil: Create documentation for JsonIntegerElement.kt

--- util/src/commonMain/kotlin/com/github/shakelang/shake/util/json/elements/JsonIntegerElement.kt
+++ util/src/commonMain/kotlin/com/github/shakelang/shake/util/json/elements/JsonIntegerElement.kt
@@ -2,12 +2,25 @@ package com.github.shakelang.shake.util.json.elements
 
 import com.github.shakelang.shake.util.json.JSON
 
+/**
+ * A json representation of double values
+ *
+ * @author [Nicolas Schmidt &lt;@nsc-de&gt;](https://github.com/nsc-de)
+ */
 class JsonIntegerElement (
 
+    /**
+     * The value of the [JsonDoubleElement]
+     */
     override val value: Long
 
 ) : JsonPrimitive {
 
+    /**
+     * Override toString to generate via [JSON.stringify]
+     *
+     * @author [Nicolas Schmidt &lt;@nsc-de&gt;](https://github.com/nsc-de)
+     */
     override fun toString(): String = JSON.stringify(value)
 
 }
