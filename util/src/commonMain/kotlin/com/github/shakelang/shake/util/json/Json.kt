55cfaddf0ec9f4f2485b4bc23f99682fe757e9d2
Minimine <64435955+nsc-de@users.noreply.github.com>
2021-07-13 16:53:22 +0200

    :pencil: Create documentation for Json.kt

--- util/src/commonMain/kotlin/com/github/shakelang/shake/util/json/Json.kt
+++ util/src/commonMain/kotlin/com/github/shakelang/shake/util/json/Json.kt
@@ -6,8 +6,21 @@ import com.github.shakelang.shake.util.json.processing.JsonGenerator
 import com.github.shakelang.shake.util.json.processing.JsonLexer
 import com.github.shakelang.shake.util.json.processing.JsonParser
 
+/**
+ * Api for processing json
+ *
+ * @author [Nicolas Schmidt &lt;@nsc-de&gt;](https://github.com/nsc-de)
+ */
 @Suppress("unused")
 object JSON {
+
+    /**
+     * Parse json code
+     *
+     * @param code the json to parse
+     *
+     * @author [Nicolas Schmidt &lt;@nsc-de&gt;](https://github.com/nsc-de)
+     */
     fun parse(code: String): Any {
         val source = CharacterSource.from(code, "JSOn.parse")
         val chars = SourceCharacterInputStream(source)
@@ -16,6 +29,16 @@ object JSON {
         val parser = JsonParser(tokens)
         return parser.parse()
     }
+
+    /**
+     * Stringify json
+     *
+     * @param input the objects to stringify
+     * @param indent the indent for the json (or null for no indent). Can be an Int (number of spaces) or a string
+     * to indent with
+     *
+     * @author [Nicolas Schmidt &lt;@nsc-de&gt;](https://github.com/nsc-de)
+     */
     fun stringify(input: Any?, indent: Any? = null): String {
         return JsonGenerator.generate(
             input, indent = when (indent) {
@@ -27,4 +50,10 @@ object JSON {
         )
     }
 }
+
+/**
+ * Api for processing json
+ *
+ * @author [Nicolas Schmidt &lt;@nsc-de&gt;](https://github.com/nsc-de)
+ */
 typealias json = JSON
