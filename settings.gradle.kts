79047d8d33ae6bae9d4d8b07cf6147e503631c9d
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2021-11-30 10:45:18 +0100

    Rename Java-Bytecode module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "Shake"
 
 include(":util:parseutils")
 include(":util:shason")
-include(":util:bytecode")
+include(":util:jvmlib")
 include(":lexer")
 include(":parser")
 include(":interpreter")
