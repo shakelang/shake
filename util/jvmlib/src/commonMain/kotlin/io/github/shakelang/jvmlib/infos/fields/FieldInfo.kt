e460b4d795d805bfa7f6fbde01891302f6ed5f19
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2021-12-11 15:16:15 +0100

    Store direct addresses instead of UShorts in FieldInfo

--- util/jvmlib/src/commonMain/kotlin/io/github/shakelang/jvmlib/infos/fields/FieldInfo.kt
+++ util/jvmlib/src/commonMain/kotlin/io/github/shakelang/jvmlib/infos/fields/FieldInfo.kt
@@ -4,27 +4,32 @@ import io.github.shakelang.jvmlib.infos.ClassInfo
 import io.github.shakelang.jvmlib.infos.attributes.AttributeMap
 import io.github.shakelang.jvmlib.infos.constants.ConstantPool
 import io.github.shakelang.jvmlib.infos.constants.ConstantUser
+import io.github.shakelang.jvmlib.infos.constants.ConstantUtf8Info
 import io.github.shakelang.parseutils.streaming.DataInputStream
 import io.github.shakelang.shason.json
 
 class FieldInfo(
     val access_flags: UShort,
-    val name_index: UShort,
-    val descriptor_index: UShort,
+    val name: ConstantUtf8Info,
+    val descriptor: ConstantUtf8Info,
     val attributes: AttributeMap,
 ) : ConstantUser {
 
-    override val uses get() = arrayOf(name_index, descriptor_index, *attributes.uses)
+    val nameIndex get() = classInfo.constantPool.indexOf(name).toUShort()
+    val descriptorIndex get() = classInfo.constantPool.indexOf(descriptor).toUShort()
+
+    override val uses get() = arrayOf(nameIndex, descriptorIndex, *attributes.uses)
     val users: Array<ConstantUser> get() = arrayOf(this, *attributes.users)
 
     private lateinit var clazz: ClassInfo
+    val classInfo: ClassInfo get() = clazz
 
     override fun toString() = json.stringify(toJson())
 
     fun toJson() = mapOf(
         "access_flags" to access_flags,
-        "name_index" to name_index,
-        "descriptor_index" to descriptor_index,
+        "name_index" to name,
+        "descriptor_index" to descriptor,
         "attributes" to attributes.toJson()
     )
 
@@ -36,10 +41,12 @@ class FieldInfo(
     companion object {
         fun fromStream(pool: ConstantPool, stream: DataInputStream): FieldInfo {
             val access_flags = stream.readUnsignedShort()
-            val name_index = stream.readUnsignedShort()
-            val descriptor_index = stream.readUnsignedShort()
+            val nameIndex = stream.readUnsignedShort()
+            val name = pool.getUtf8(nameIndex)
+            val descriptorIndex = stream.readUnsignedShort()
+            val descriptor = pool.getUtf8(descriptorIndex)
             val attributes = AttributeMap.fromStream(pool, stream)
-            return FieldInfo(access_flags, name_index, descriptor_index, attributes)
+            return FieldInfo(access_flags, name, descriptor, attributes)
         }
     }
 }
