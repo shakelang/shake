f5f1ab45158921c823f41b71ab841212f4892b97
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2021-12-12 14:18:58 +0100

    Update ConstantInvokeDynamicInfo

--- util/jvmlib/src/commonMain/kotlin/io/github/shakelang/jvmlib/infos/constants/ConstantInvokeDynamicInfo.kt
+++ util/jvmlib/src/commonMain/kotlin/io/github/shakelang/jvmlib/infos/constants/ConstantInvokeDynamicInfo.kt
@@ -2,39 +2,25 @@ package io.github.shakelang.jvmlib.infos.constants
 
 import io.github.shakelang.parseutils.streaming.DataInputStream
 
-class ConstantInvokeDynamicInfo : ConstantInfo {
+class ConstantInvokeDynamicInfo(
+    val bootstrapMethodAttributeIndex: UShort,
+    val nati: UShort
+) : ConstantInfo() {
+
+    lateinit var nameAndType: ConstantNameAndTypeInfo
+    val nameAndTypeIndex: UShort get() = constantPool.indexOf(nameAndType).toUShort()
 
     override val tag: Byte get() = ConstantInvokeDynamicInfo.tag
     override val tagName: String get() = name
-
-    val byte0: Byte
-    val byte1: Byte
-    val byte2: Byte
-    val byte3: Byte
-
-    constructor(byte0: Byte, byte1: Byte, byte2: Byte, byte3: Byte) : super() {
-        this.byte0 = byte0
-        this.byte1 = byte1
-        this.byte2 = byte2
-        this.byte3 = byte3
-    }
-
-    constructor(bytes: ByteArray) : super() {
-        if (bytes.size != 4) throw Error("Expecting to get 4 bytes!")
-        byte0 = bytes[0]
-        byte1 = bytes[1]
-        byte2 = bytes[2]
-        byte3 = bytes[3]
-    }
-    override fun toJson() = super.toJson().with("bytes", arrayOf(byte0, byte1, byte2, byte3))
+    override fun toJson() = super.toJson()
+        .with("bootstrapMethodAttributeIndex", bootstrapMethodAttributeIndex)
+        .with("nameAndTypeIndex", nameAndTypeIndex)
 
     companion object {
         fun contentsFromStream(stream: DataInputStream): ConstantInvokeDynamicInfo {
             return ConstantInvokeDynamicInfo(
-                stream.readByte(),
-                stream.readByte(),
-                stream.readByte(),
-                stream.readByte()
+                stream.readUnsignedShort(),
+                stream.readUnsignedShort(),
             )
         }
 
