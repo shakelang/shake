83f9daea6a43a43e7493226946e0fd409a614b7d
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2021-12-13 15:27:35 +0100

    Use kotlin by shortcut in ConstantPool

--- util/jvmlib/src/commonMain/kotlin/io/github/shakelang/jvmlib/infos/constants/ConstantPool.kt
+++ util/jvmlib/src/commonMain/kotlin/io/github/shakelang/jvmlib/infos/constants/ConstantPool.kt
@@ -4,7 +4,7 @@ import io.github.shakelang.jvmlib.infos.ClassInfo
 import io.github.shakelang.parseutils.streaming.input.DataInputStream
 import io.github.shakelang.parseutils.streaming.output.DataOutputStream
 
-class ConstantPool(val constants: MutableList<ConstantInfo>) : MutableList<ConstantInfo>, ConstantUser {
+class ConstantPool(val constants: MutableList<ConstantInfo>) : MutableList<ConstantInfo> by constants, ConstantUser {
 
     constructor() : this(mutableListOf())
 
@@ -19,53 +19,6 @@ class ConstantPool(val constants: MutableList<ConstantInfo>) : MutableList<Const
 
     constructor(constants: Array<ConstantInfo>) : this(constants.toMutableList())
 
-    override val size: Int
-        get() = constants.size
-
-    override fun contains(element: ConstantInfo): Boolean {
-        return constants.contains(element)
-    }
-
-    override fun containsAll(elements: Collection<ConstantInfo>): Boolean {
-        return constants.containsAll(elements)
-    }
-
-    override fun get(index: Int): ConstantInfo {
-        return constants[index-1]
-    }
-
-    operator fun get(index: UShort): ConstantInfo {
-        return this[index.toInt()]
-    }
-
-    override fun indexOf(element: ConstantInfo): Int {
-        return constants.indexOf(element)
-    }
-
-    override fun isEmpty(): Boolean {
-        return constants.isEmpty()
-    }
-
-    override fun iterator(): MutableIterator<ConstantInfo> {
-        return constants.iterator()
-    }
-
-    override fun lastIndexOf(element: ConstantInfo): Int {
-        return constants.lastIndexOf(element)
-    }
-
-    override fun listIterator(): MutableListIterator<ConstantInfo> {
-        return constants.listIterator()
-    }
-
-    override fun listIterator(index: Int): MutableListIterator<ConstantInfo> {
-        return constants.listIterator(index)
-    }
-
-    override fun subList(fromIndex: Int, toIndex: Int): MutableList<ConstantInfo> {
-        return constants.subList(fromIndex, toIndex)
-    }
-
     fun toJson(): List<Map<String, Any>> {
         return this.constants.map { it.toJson() }
     }
@@ -78,8 +31,8 @@ class ConstantPool(val constants: MutableList<ConstantInfo>) : MutableList<Const
         return constants.toString()
     }
 
-    fun getUtf8(nameIndex: Int): ConstantUtf8Info = this[nameIndex].toUtf8()
-    fun getClass(nameIndex: Int): ConstantClassInfo = this[nameIndex].toClass()
+    fun getUtf8(nameIndex: Int) = this[nameIndex].toUtf8()
+    fun getClass(nameIndex: Int) = this[nameIndex].toClass()
     fun getFieldRef(nameIndex: Int) = this[nameIndex].toFieldRef()
     fun getMethodRef(nameIndex: Int) = this[nameIndex].toMethodRef()
     fun getInterfaceMethodRef(nameIndex: Int) = this[nameIndex].toInterfaceMethodRef()
@@ -93,8 +46,8 @@ class ConstantPool(val constants: MutableList<ConstantInfo>) : MutableList<Const
     fun getMethodType(nameIndex: Int) = this[nameIndex].toMethodType()
     fun getInvokeDynamic(nameIndex: Int) = this[nameIndex].toInvokeDynamic()
 
-    fun getUtf8(nameIndex: UShort): ConstantUtf8Info = this.getUtf8(nameIndex.toInt())
-    fun getClass(nameIndex: UShort): ConstantClassInfo = this.getClass(nameIndex.toInt())
+    fun getUtf8(nameIndex: UShort) = this.getUtf8(nameIndex.toInt())
+    fun getClass(nameIndex: UShort) = this.getClass(nameIndex.toInt())
     fun getFieldRef(nameIndex: UShort) = this.getFieldRef(nameIndex.toInt())
     fun getMethodRef(nameIndex: UShort) = this.getMethodRef(nameIndex.toInt())
     fun getInterfaceMethodRef(nameIndex: UShort) = this.getInterfaceMethodRef(nameIndex.toInt())
@@ -432,45 +385,8 @@ class ConstantPool(val constants: MutableList<ConstantInfo>) : MutableList<Const
     fun expectInvokeDynamic(bootstrapMethod: UShort, nameAndType: ConstantNameAndTypeInfo): ConstantInvokeDynamicInfo
         = findInvokeDynamic(bootstrapMethod, nameAndType) ?: throw Exception("No invoke dynamic constant found for bootstrapMethod: $bootstrapMethod, nameAndType: $nameAndType")
 
-
-    override fun add(element: ConstantInfo): Boolean {
-        return constants.add(element)
-    }
-
-    override fun add(index: Int, element: ConstantInfo) {
-        constants.add(index, element)
-    }
-
-    override fun addAll(index: Int, elements: Collection<ConstantInfo>): Boolean {
-        return constants.addAll(index, elements)
-    }
-
-    override fun addAll(elements: Collection<ConstantInfo>): Boolean {
-        return constants.addAll(elements)
-    }
-
     override fun clear() {
         constants.clear()
-    }
-
-    override fun remove(element: ConstantInfo): Boolean {
-        return constants.remove(element)
-    }
-
-    override fun removeAll(elements: Collection<ConstantInfo>): Boolean {
-        return constants.removeAll(elements)
-    }
-
-    override fun removeAt(index: Int): ConstantInfo {
-        return constants.removeAt(index)
-    }
-
-    override fun retainAll(elements: Collection<ConstantInfo>): Boolean {
-        return constants.retainAll(elements)
-    }
-
-    override fun set(index: Int, element: ConstantInfo): ConstantInfo {
-        return constants.set(index, element)
     }
 
     @Deprecated("DON'T USE THIS METHOD, IT CORRUPTS THE CLASS FILE")
