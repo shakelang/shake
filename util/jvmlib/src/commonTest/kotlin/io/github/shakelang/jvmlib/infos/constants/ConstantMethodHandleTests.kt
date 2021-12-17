ebed4b5a3d2388823c9ed302de2a99dd101654cf
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2021-12-17 17:03:18 +0100

    Create tests for ConstantMethodHandleInfo

---
+++ util/jvmlib/src/commonTest/kotlin/io/github/shakelang/jvmlib/infos/constants/ConstantMethodHandleTests.kt
@@ -0,0 +1,69 @@
+package io.github.shakelang.jvmlib.infos.constants
+
+import io.github.shakelang.parseutils.bytes.stream
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ConstantMethodHandleTests {
+
+    private val testConstants: Array<ConstantInfo> get() = arrayOf(ConstantUtf8Info("java/lang/Object"))
+
+    @Test
+    fun test() {
+        val constant = ConstantMethodHandleInfo(2, 2u)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.reference)
+        assertEquals(2u, constant.referenceIndex)
+        assertEquals(2, constant.referenceKind)
+        assertEquals(15, constant.tag)
+        assertEquals("constant_method_handle_info", constant.tagName)
+    }
+
+    @Test
+    fun testContentFromStream() {
+        val inputStream = byteArrayOf(0x02, 0x00, 0x02).stream()
+        val constant = ConstantMethodHandleInfo.contentsFromStream(inputStream)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.reference)
+        assertEquals(2u, constant.referenceIndex)
+        assertEquals(2, constant.referenceKind)
+        assertEquals(15, constant.tag)
+        assertEquals("constant_method_handle_info", constant.tagName)
+    }
+
+    @Test
+    fun testFromStream() {
+        val inputStream = byteArrayOf(0x0f, 0x02, 0x00, 0x02).stream()
+        val constant = ConstantMethodHandleInfo.fromStream(inputStream)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.reference)
+        assertEquals(2u, constant.referenceIndex)
+        assertEquals(2, constant.referenceKind)
+        assertEquals(15, constant.tag)
+        assertEquals("constant_method_handle_info", constant.tagName)
+    }
+
+    @Test
+    fun testContentsFromBytes() {
+        val bytes = byteArrayOf(0x02, 0x00, 0x02)
+        val constant = ConstantMethodHandleInfo.contentsFromBytes(bytes)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.reference)
+        assertEquals(2u, constant.referenceIndex)
+        assertEquals(2, constant.referenceKind)
+        assertEquals(15, constant.tag)
+        assertEquals("constant_method_handle_info", constant.tagName)
+    }
+
+    @Test
+    fun testFromBytes() {
+        val bytes = byteArrayOf(0x0f, 0x02, 0x00, 0x02)
+        val constant = ConstantMethodHandleInfo.fromBytes(bytes)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.reference)
+        assertEquals(2u, constant.referenceIndex)
+        assertEquals(2, constant.referenceKind)
+        assertEquals(15, constant.tag)
+        assertEquals("constant_method_handle_info", constant.tagName)
+    }
+}
