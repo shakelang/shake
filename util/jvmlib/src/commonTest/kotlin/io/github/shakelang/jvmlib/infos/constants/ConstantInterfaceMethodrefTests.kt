28ca84a24ee7b148af31a94e8601e862caa8eed3
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2021-12-17 15:58:37 +0100

    Create tests for ConstantInterfaceMethodrefInfo

---
+++ util/jvmlib/src/commonTest/kotlin/io/github/shakelang/jvmlib/infos/constants/ConstantInterfaceMethodrefTests.kt
@@ -0,0 +1,80 @@
+package io.github.shakelang.jvmlib.infos.constants
+
+import io.github.shakelang.parseutils.bytes.stream
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ConstantInterfaceMethodrefTests {
+
+    private val testConstants: Array<ConstantInfo> get() = arrayOf(
+        ConstantClassInfo(3u),
+        ConstantUtf8Info("java/lang/Object"),
+        ConstantNameAndTypeInfo(6u, 5u),
+        ConstantUtf8Info("foo"),
+        ConstantUtf8Info("I")
+    )
+
+    @Test
+    fun test() {
+        val constant = ConstantInterfaceMethodrefInfo(2u, 4u)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.classRef)
+        assertEquals(2u, constant.classRefIndex)
+        assertEquals(pool[4], constant.nameTypeRef)
+        assertEquals(4u, constant.nameTypeRefIndex)
+        assertEquals(11, constant.tag)
+        assertEquals("constant_interface_methodref_info", constant.tagName)
+    }
+
+    @Test
+    fun testContentFromStream() {
+        val inputStream = byteArrayOf(0x00, 0x02, 0x00, 0x04).stream()
+        val constant = ConstantInterfaceMethodrefInfo.contentsFromStream(inputStream)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.classRef)
+        assertEquals(2u, constant.classRefIndex)
+        assertEquals(pool[4], constant.nameTypeRef)
+        assertEquals(4u, constant.nameTypeRefIndex)
+        assertEquals(11, constant.tag)
+        assertEquals("constant_interface_methodref_info", constant.tagName)
+    }
+
+    @Test
+    fun testFromStream() {
+        val inputStream = byteArrayOf(0x0b, 0x00, 0x02, 0x00, 0x04).stream()
+        val constant = ConstantInterfaceMethodrefInfo.fromStream(inputStream)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.classRef)
+        assertEquals(2u, constant.classRefIndex)
+        assertEquals(pool[4], constant.nameTypeRef)
+        assertEquals(4u, constant.nameTypeRefIndex)
+        assertEquals(11, constant.tag)
+        assertEquals("constant_interface_methodref_info", constant.tagName)
+    }
+
+    @Test
+    fun testContentsFromBytes() {
+        val bytes = byteArrayOf(0x00, 0x02, 0x00, 0x04)
+        val constant = ConstantInterfaceMethodrefInfo.contentsFromBytes(bytes)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.classRef)
+        assertEquals(2u, constant.classRefIndex)
+        assertEquals(pool[4], constant.nameTypeRef)
+        assertEquals(4u, constant.nameTypeRefIndex)
+        assertEquals(11, constant.tag)
+        assertEquals("constant_interface_methodref_info", constant.tagName)
+    }
+
+    @Test
+    fun testFromBytes() {
+        val bytes = byteArrayOf(0x0b, 0x00, 0x02, 0x00, 0x04)
+        val constant = ConstantInterfaceMethodrefInfo.fromBytes(bytes)
+        val pool = ConstantPool(mutableListOf(constant, *testConstants))
+        assertEquals(pool[2], constant.classRef)
+        assertEquals(2u, constant.classRefIndex)
+        assertEquals(pool[4], constant.nameTypeRef)
+        assertEquals(4u, constant.nameTypeRefIndex)
+        assertEquals(11, constant.tag)
+        assertEquals("constant_interface_methodref_info", constant.tagName)
+    }
+}
