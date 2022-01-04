c944cf467e319f6f45c135e1b8615b91c1dad6cb
Minimine <64435955+nsc-de@users.noreply.github.com>
2022-01-04 23:29:51 +0100

    Create tool for generating shas assambly out of the generated bytecode again

---
+++ shake/shastools/src/commonMain/kotlin/io/github/shakelang/shake/shasambly/generator/shas/ShasGenerator.kt
@@ -0,0 +1,175 @@
+@file:Suppress("unused")
+package io.github.shakelang.shake.shasambly.generator.shas
+
+import io.github.shakelang.parseutils.bytes.toBytes
+import io.github.shakelang.parseutils.bytes.toHexString
+import io.github.shakelang.parseutils.streaming.input.InputStream
+import io.github.shakelang.parseutils.streaming.input.asCountingInputStream
+import io.github.shakelang.parseutils.streaming.input.asDataInputStream
+import io.github.shakelang.parseutils.streaming.output.ByteArrayOutputStream
+import io.github.shakelang.parseutils.streaming.output.OutputStream
+import io.github.shakelang.shake.shasambly.interpreter.Opcodes
+import io.github.shakelang.shake.shasambly.interpreter.natives.Natives
+import io.github.shakelang.shake.shasambly.interpreter.natives.nativeFunctions
+
+class ShasGenerator(input: InputStream) {
+
+    private val counter = input.asCountingInputStream()
+    private val input = counter.asDataInputStream()
+
+    private lateinit var out: OutputStream
+
+    fun parse(): ByteArray {
+        val out = ByteArrayOutputStream()
+
+        doParse()
+
+        return out.toByteArray()
+    }
+
+    fun dumpParse(out: OutputStream) {
+        this.out = out
+        doParse()
+    }
+
+    private fun doParse() {
+        Natives.initNativeFunctions()
+        while(input.available() > 0) {
+            print_opcode()
+        }
+    }
+
+    private fun oprint(contents: String) {
+        out.write(contents.toCharArray().map { it.code.toUByte().toByte() }.toByteArray())
+    }
+
+    private fun oprintln(contents: String) {
+        oprint("${contents}\n")
+    }
+
+    fun print_opcode() {
+        oprint("[0x${counter.getCount().toUInt().toBytes().toHexString()}] ")
+        when(val opcode = input.readByte()) {
+            Opcodes.INCR_STACK -> oprintln("incr_stack ${input.readUnsignedShort()}")
+            Opcodes.DECR_STACK -> oprintln("decr_stack")
+            Opcodes.JUMP_STATIC -> oprintln("jump_static ${input.readUnsignedInt()}")
+            Opcodes.JUMP_DYNAMIC -> oprint("jump_dynamic")
+            Opcodes.JUMP_IF -> oprintln("jump_if ${input.readUnsignedInt()}")
+            Opcodes.INVOKE_NATIVE -> oprintln("invoke_native ${nativeFunctions[input.readUnsignedShort().toInt()]?.first ?: throw Error("Unknown native")}")
+            Opcodes.GLOB_ADDR -> oprintln("glob_addr ${input.readUnsignedShort()}")
+
+            Opcodes.B_GET_LOCAL -> oprintln("b_get_local ${input.readUnsignedShort()}")
+            Opcodes.S_GET_LOCAL -> oprintln("s_get_local ${input.readUnsignedShort()}")
+            Opcodes.I_GET_LOCAL -> oprintln("i_get_local ${input.readUnsignedShort()}")
+            Opcodes.L_GET_LOCAL -> oprintln("l_get_local ${input.readUnsignedShort()}")
+            Opcodes.B_STORE_LOCAL -> oprintln("b_store_local ${input.readUnsignedShort()}")
+            Opcodes.S_STORE_LOCAL -> oprintln("s_store_local ${input.readUnsignedShort()}")
+            Opcodes.I_STORE_LOCAL -> oprintln("i_store_local ${input.readUnsignedShort()}")
+            Opcodes.L_STORE_LOCAL -> oprintln("l_store_local ${input.readUnsignedShort()}")
+            Opcodes.B_PUSH -> oprintln("bpush 0x${input.readUnsignedByte().toString(16)}")
+            Opcodes.S_PUSH -> oprintln("spush ${input.readShort()}")
+            Opcodes.I_PUSH -> oprintln("ipush ${input.readInt()}")
+            Opcodes.L_PUSH -> oprintln("lpush ${input.readLong()}")
+
+            Opcodes.B_ADD -> oprintln("badd")
+            Opcodes.B_SUB -> oprintln("bsub")
+            Opcodes.B_MUL -> oprintln("bmul")
+            Opcodes.B_DIV -> oprintln("bdiv")
+            Opcodes.B_MOD -> oprintln("bmod")
+            Opcodes.S_ADD -> oprintln("sadd")
+            Opcodes.S_SUB -> oprintln("ssub")
+            Opcodes.S_MUL -> oprintln("smul")
+            Opcodes.S_DIV -> oprintln("sdiv")
+            Opcodes.S_MOD -> oprintln("smod")
+            Opcodes.I_ADD -> oprintln("iadd")
+            Opcodes.I_SUB -> oprintln("isub")
+            Opcodes.I_MUL -> oprintln("imul")
+            Opcodes.I_DIV -> oprintln("idiv")
+            Opcodes.I_MOD -> oprintln("imod")
+            Opcodes.L_ADD -> oprintln("ladd")
+            Opcodes.L_SUB -> oprintln("lsub")
+            Opcodes.L_MUL -> oprintln("lmul")
+            Opcodes.L_DIV -> oprintln("ldiv")
+            Opcodes.L_MOD -> oprintln("lmod")
+            Opcodes.F_ADD -> oprintln("fadd")
+            Opcodes.F_SUB -> oprintln("fsub")
+            Opcodes.F_MUL -> oprintln("fmul")
+            Opcodes.F_DIV -> oprintln("fdiv")
+            Opcodes.F_MOD -> oprintln("fmod")
+            Opcodes.D_ADD -> oprintln("dadd")
+            Opcodes.D_SUB -> oprintln("dsub")
+            Opcodes.D_MUL -> oprintln("dmul")
+            Opcodes.D_DIV -> oprintln("ddiv")
+            Opcodes.D_MOD -> oprintln("dmod")
+            Opcodes.B_EQ -> oprintln("b_eq")
+            Opcodes.S_EQ -> oprintln("s_eq")
+            Opcodes.I_EQ -> oprintln("i_eq")
+            Opcodes.L_EQ -> oprintln("l_eq")
+            Opcodes.F_EQ -> oprintln("f_eq")
+            Opcodes.D_EQ -> oprintln("d_eq")
+
+            Opcodes.B_BIGGER -> oprintln("b_bigger")
+            Opcodes.S_BIGGER -> oprintln("s_bigger")
+            Opcodes.I_BIGGER -> oprintln("i_bigger")
+            Opcodes.L_BIGGER -> oprintln("l_bigger")
+            Opcodes.F_BIGGER -> oprintln("f_bigger")
+            Opcodes.D_BIGGER -> oprintln("d_bigger")
+            Opcodes.B_SMALLER -> oprintln("b_smaller")
+            Opcodes.S_SMALLER -> oprintln("s_smaller")
+            Opcodes.I_SMALLER -> oprintln("i_smaller")
+            Opcodes.L_SMALLER -> oprintln("l_smaller")
+            Opcodes.F_SMALLER -> oprintln("f_smaller")
+            Opcodes.D_SMALLER -> oprintln("d_smaller")
+            Opcodes.B_BIGGER_EQ -> oprintln("b_bigger_eq")
+            Opcodes.S_BIGGER_EQ -> oprintln("s_bigger_eq")
+            Opcodes.I_BIGGER_EQ -> oprintln("i_bigger_eq")
+            Opcodes.L_BIGGER_EQ -> oprintln("l_bigger_eq")
+            Opcodes.F_BIGGER_EQ -> oprintln("f_bigger_eq")
+            Opcodes.D_BIGGER_EQ -> oprintln("d_bigger_eq")
+            Opcodes.B_SMALLER_EQ -> oprintln("b_smaller_eq")
+            Opcodes.S_SMALLER_EQ -> oprintln("s_smaller_eq")
+            Opcodes.I_SMALLER_EQ -> oprintln("i_smaller_eq")
+            Opcodes.L_SMALLER_EQ -> oprintln("l_smaller_eq")
+            Opcodes.F_SMALLER_EQ -> oprintln("f_smaller_eq")
+            Opcodes.D_SMALLER_EQ -> oprintln("d_smaller_eq")
+
+            Opcodes.BOOL_NOT -> oprintln("not")
+
+            Opcodes.B_GET_GLOBAL -> oprintln("b_get_global ${input.readUnsignedInt().toString(16)}")
+            Opcodes.S_GET_GLOBAL -> oprintln("s_get_global ${input.readUnsignedInt().toString(16)}")
+            Opcodes.I_GET_GLOBAL -> oprintln("i_get_global ${input.readUnsignedInt().toString(16)}")
+            Opcodes.L_GET_GLOBAL -> oprintln("l_get_global ${input.readUnsignedInt().toString(16)}")
+
+            Opcodes.B_GET_GLOBAL_DYNAMIC -> oprintln("b_get_global_dynamic")
+            Opcodes.S_GET_GLOBAL_DYNAMIC -> oprintln("s_get_global_dynamic")
+            Opcodes.I_GET_GLOBAL_DYNAMIC -> oprintln("i_get_global_dynamic")
+            Opcodes.L_GET_GLOBAL_DYNAMIC -> oprintln("l_get_global_dynamic")
+
+            Opcodes.B_STORE_GLOBAL -> oprintln("b_store_global ${input.readUnsignedInt().toString(16)}")
+            Opcodes.S_STORE_GLOBAL -> oprintln("s_store_global ${input.readUnsignedInt().toString(16)}")
+            Opcodes.I_STORE_GLOBAL -> oprintln("i_store_global ${input.readUnsignedInt().toString(16)}")
+            Opcodes.L_STORE_GLOBAL -> oprintln("l_store_global ${input.readUnsignedInt().toString(16)}")
+
+            Opcodes.B_STORE_GLOBAL_DYNAMIC -> oprintln("b_store_global_dynamic")
+            Opcodes.S_STORE_GLOBAL_DYNAMIC -> oprintln("s_store_global_dynamic")
+            Opcodes.I_STORE_GLOBAL_DYNAMIC -> oprintln("i_store_global_dynamic")
+            Opcodes.L_STORE_GLOBAL_DYNAMIC -> oprintln("l_store_global_dynamic")
+
+            Opcodes.B_NEG -> oprintln("bneg")
+            Opcodes.S_NEG -> oprintln("sneg")
+            Opcodes.I_NEG -> oprintln("ineg")
+            Opcodes.L_NEG -> oprintln("lneg")
+            Opcodes.F_NEG -> oprintln("fneg")
+            Opcodes.D_NEG -> oprintln("dneg")
+
+            Opcodes.B_ABS -> oprintln("babs")
+            Opcodes.S_ABS -> oprintln("sabs")
+            Opcodes.I_ABS -> oprintln("iabs")
+            Opcodes.L_ABS -> oprintln("labs")
+            Opcodes.F_ABS -> oprintln("fabs")
+            Opcodes.D_ABS -> oprintln("dabs")
+            else -> throw Error("Wrong opcode 0x${opcode.toBytes().toHexString()} at position 0x${counter.getCount().toUInt().toBytes().toHexString()}")
+        }
+    }
+
+}
---
+++ shake/shastools/src/jvmMain/kotlin/io/github/shakelang/shake/shasambly/ShasGenerate.kt
@@ -0,0 +1,21 @@
+package io.github.shakelang.shake.shasambly
+
+import io.github.shakelang.shake.shasambly.generator.shas.ShasGenerator
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+fun main(args: Array<String>) {
+    if(args.isEmpty()) throw Error("Expecting at least one argument")
+    if(args.size > 2) throw Error("Expecting maximum 2 arguments")
+    if(!args[0].endsWith(".shasx")) throw Error("Input file has to end with .shas")
+    val f = File(args[0])
+    val inputStream = FileInputStream(f).buffered()
+    val generator = ShasGenerator(inputStream)
+    val output = if(args.size == 1) args[0].substring(0, args[0].length - 1) else args[1]
+    val out = FileOutputStream(output).buffered()
+    generator.dumpParse(out)
+    inputStream.close()
+    out.flush()
+    out.close()
+}
