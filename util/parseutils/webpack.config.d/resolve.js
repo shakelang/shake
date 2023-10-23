048c6130c609bed7613e7523364e17eafca48f90
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-10-23 21:56:42 +0200

    Updates

--- util/parseutils/webpack.config.d/resolve.js
+++ util/parseutils/webpack.config.d/resolve.js
@@ -5,12 +5,12 @@ config.resolve.alias = {
     "path": require.resolve("path-browserify"),
     "fs": false,
 }
-
-config.resolve.modules = [
-    path.resolve("src/commonMain/resources"),
-    path.resolve("src/commonTest/resources"),
-    path.resolve("src/jsMain/resources"),
-    path.resolve("src/jsTest/resources"),
-    path.resolve("src/jsMain/kotlin"),
-    path.resolve("src/jsTest/kotlin")
-]
+//
+// config.resolve.modules = [
+//     path.resolve("src/commonMain/resources"),
+//     path.resolve("src/commonTest/resources"),
+//     path.resolve("src/jsMain/resources"),
+//     path.resolve("src/jsTest/resources"),
+//     path.resolve("src/jsMain/kotlin"),
+//     path.resolve("src/jsTest/kotlin")
+// ]
