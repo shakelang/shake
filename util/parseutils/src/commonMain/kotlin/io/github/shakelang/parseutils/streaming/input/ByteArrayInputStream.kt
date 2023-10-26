cdf1a1ff5a5e82868ce18cef72d654bec9d7cda9
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-10-26 18:38:20 +0200

    Write documentation for ByteArrayInputStream

--- util/parseutils/src/commonMain/kotlin/io/github/shakelang/parseutils/streaming/input/ByteArrayInputStream.kt
+++ util/parseutils/src/commonMain/kotlin/io/github/shakelang/parseutils/streaming/input/ByteArrayInputStream.kt
@@ -1,23 +1,86 @@
 package io.github.shakelang.parseutils.streaming.input
 
+/**
+ * A [ByteArrayInputStream] is an [InputStream] that reads from a [ByteArray]
+ *
+ * @param buf The [ByteArray] to read from
+ *
+ * @since 0.1.0
+ * @version 0.1.1
+ * @author Nicolas Schmidt &lt;@nsc-de&gt;
+ *
+ * @see InputStream
+ */
 class ByteArrayInputStream(
-    private var buf: ByteArray
+
+    /**
+     * The [ByteArray] to read from
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
+    private var buf: ByteArray,
 ) : InputStream() {
 
+    /**
+     * The current position in the [ByteArray]
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     private var pos = 0
+
+    /**
+     * The mark of the [ByteArrayInputStream]
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     private var mark = 0
+
+    /**
+     * The count of the [ByteArrayInputStream]
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     private var count = 0
 
     init {
         this.count = buf.size
     }
 
+    /**
+     * Creates a [ByteArrayInputStream] from a [ByteArray]
+     *
+     * @param buf The [ByteArray] to read from
+     * @param offset The offset to start reading from
+     * @param length The length of the [ByteArrayInputStream]
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     constructor(buf: ByteArray, offset: Int, length: Int) : this(buf) {
         this.count = length
         this.pos = offset
         this.mark = offset
     }
 
+    /**
+     * Reads a single byte from the [ByteArrayInputStream]
+     *
+     * @return the read byte or -1 if the end of the [ByteArrayInputStream] is reached
+     * @throws IOException if an I/O error occurs
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     override fun read(): Int {
         if (pos >= count) {
             return -1
@@ -25,6 +88,19 @@ class ByteArrayInputStream(
         return buf[pos++].toInt() and 0xff
     }
 
+    /**
+     * Reads bytes from the [ByteArrayInputStream] into a [ByteArray]
+     *
+     * @param b The [ByteArray] to read into
+     * @param off The offset to start reading into
+     * @param len The length of the [ByteArray] to read into
+     * @return the number of bytes read or -1 if the end of the [ByteArrayInputStream] is reached
+     * @throws IOException if an I/O error occurs
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     override fun read(b: ByteArray, off: Int, len: Int): Int {
         var l = len
         if (pos >= count) {
@@ -38,6 +114,17 @@ class ByteArrayInputStream(
         return l
     }
 
+    /**
+     * Skips bytes in the [ByteArrayInputStream]
+     *
+     * @param n The number of bytes to skip
+     * @return the number of bytes skipped
+     * @throws IOException if an I/O error occurs
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     override fun skip(n: Long): Long {
         var l = n
         if (pos + l > count) {
@@ -50,22 +137,69 @@ class ByteArrayInputStream(
         return l
     }
 
+    /**
+     * Returns the number of bytes that can be read from the [ByteArrayInputStream]
+     *
+     * @return the number of bytes that can be read from the [ByteArrayInputStream]
+     * @throws IOException if an I/O error occurs
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     override fun available(): Int {
         return count - pos
     }
 
+    /**
+     * Marks the current position in the [ByteArrayInputStream]
+     *
+     * @param readlimit The read limit
+     * @throws IOException if an I/O error occurs
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     override fun mark(readlimit: Int) {
         mark = pos
     }
 
+    /**
+     * Resets the [ByteArrayInputStream] to the last mark
+     *
+     * @throws IOException if an I/O error occurs
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     override fun reset() {
         pos = mark
     }
 
+    /**
+     * Returns if the [ByteArrayInputStream] supports marking
+     *
+     * @return if the [ByteArrayInputStream] supports marking
+     * @throws IOException if an I/O error occurs
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     */
     override fun markSupported(): Boolean {
         return true
     }
 
+    /**
+     * Closes the [ByteArrayInputStream]
+     *
+     * @throws IOException if an I/O error occurs
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     override fun close() {
         buf = ByteArray(0)
         pos = 0
@@ -73,6 +207,15 @@ class ByteArrayInputStream(
         count = 0
     }
 
+    /**
+     * Returns a string representation of the [ByteArrayInputStream]
+     *
+     * @return a string representation of the [ByteArrayInputStream]
+     *
+     * @since 0.1.0
+     * @version 0.1.1
+     * @author Nicolas Schmidt &lt;@nsc-de&gt;
+     */
     override fun toString(): String {
         return "ByteArrayInputStream[pos=$pos, mark=$mark, count=$count]"
     }
