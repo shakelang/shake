aeaad40f6aaf54a466b9b8a24d2560034de9d214
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-10-28 22:25:45 +0200

    Create environment submodule

---
+++ util/environment/build.gradle.kts
@@ -0,0 +1,17 @@
+import io.github.shakelang.shake.conventions.mpp.dependencies
+plugins {
+    id("io.github.shakelang.shake.conventions.mpp.all")
+    id("io.github.shakelang.shake.conventions.mpp.publishing")
+}
+
+group = "io.github.shakelang.util.colorlib"
+version = "0.1.0"
+description = "Utility for working with colors in console applications (Kotlin Multiplatform)"
+
+val projectName = name
+
+kotlin {
+    dependencies {
+        testImplementation(kotlin("test"))
+    }
+}
