0739f1ea285e55b5fdc96eaa283e45f4dea45513
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-01 11:23:02 +0100

    Update publishing config

--- buildSrc/src/main/kotlin/conventions/publishing.gradle.kts
+++ buildSrc/src/main/kotlin/conventions/publishing.gradle.kts
@@ -2,9 +2,13 @@ package conventions
 
 import gradle.kotlin.dsl.accessors._4ad077ad74816558e52d7069eb18a2f7.publishing
 import gradle.kotlin.dsl.accessors._4ad077ad74816558e52d7069eb18a2f7.signing
+import gradle.kotlin.dsl.accessors._bed5c5adebf3efe38c055e8c4dfb71ee.kotlin
 import org.gradle.crypto.checksum.Checksum
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.maven
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
+import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependency
 
 plugins {
     id("maven-publish")
@@ -12,17 +16,47 @@ plugins {
     id("org.gradle.crypto.checksum")
 }
 
+tasks.register("listDependencies") {
+    group = "info"
+    doLast {
+
+        kotlin.sourceSets.all {
+            println("Source Set: $name, Configuration: $implementationConfigurationName, Dependencies:")
+
+            configurations.getByName(this.implementationConfigurationName).dependencies.forEach {
+                val dep = mutableListOf<String>()
+                if (it.group != null) dep.add(it.group!!)
+                if (it.name != null) dep.add(it.name)
+                if (it.version != null) dep.add(it.version.toString())
+                println("  - ${if(it is NpmDependency) "npm" else "maven"} ${dep.joinToString(":")}")
+            }
+        }
+    }
+}
+
+
+kotlin {
+    val publicationsFromMainHost =
+        listOf(jvm(), js()).map { it.name } + "kotlinMultiplatform"
+}
+
 publishing {
+
     repositories {
         maven("Sonatype") {
             name = "Sonatype"
             url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
 
-            val _username = System.getenv("GRADLE_SONATYPE_USERNAME") ?: project.properties["sonatype.username"] as String?
-            val _password = System.getenv("GRADLE_SONATYPE_PASSWORD") ?: project.properties["sonatype.password"] as String?
+            val _username =
+                System.getenv("GRADLE_SONATYPE_USERNAME") ?: project.properties["sonatype.username"] as String?
+            val _password =
+                System.getenv("GRADLE_SONATYPE_PASSWORD") ?: project.properties["sonatype.password"] as String?
 
             if (_username == null || _password == null) {
-                logger.log(LogLevel.WARN, "No Sonatype credentials found, skipping Sonatype publishing configuration")
+                logger.log(
+                    LogLevel.WARN,
+                    "No Sonatype credentials found, skipping Sonatype publishing configuration"
+                )
                 return@maven
             }
 
@@ -36,7 +70,8 @@ publishing {
             name = "GitHub"
             url = uri("https://maven.pkg.github.com/shakelang/shake")
 
-            val _username = System.getenv("GRADLE_GITHUB_USERNAME") ?: project.properties["github.username"] as String?
+            val _username =
+                System.getenv("GRADLE_GITHUB_USERNAME") ?: project.properties["github.username"] as String?
             val _token = System.getenv("GRADLE_GITHUB_TOKEN") ?: project.properties["github.token"] as String?
 
             if (_username == null || _token == null) {
@@ -49,9 +84,9 @@ publishing {
                 password = _token
             }
         }
+        mavenLocal()
     }
 }
-
 //tasks.withType(PublishToMavenRepository::class.java) {
 //    println(this.name)
 //
