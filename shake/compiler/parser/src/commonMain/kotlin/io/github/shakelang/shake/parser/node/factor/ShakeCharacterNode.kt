d4e46c3a345272f9c499cd34a60f706f50a5fe35
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-09 17:49:22 +0100

    Move factors to one file

--- shake/compiler/parser/src/commonMain/kotlin/io/github/shakelang/shake/parser/node/factor/ShakeCharacterNode.kt
+++
@@ -1,8 +0,0 @@
-package io.github.shakelang.shake.parser.node.factor
-
-import io.github.shakelang.shake.parser.node.ShakeValuedNodeImpl
-import io.github.shakelang.shake.util.parseutils.characters.position.PositionMap
-
-class ShakeCharacterNode(map: PositionMap, val value: Char) : ShakeValuedNodeImpl(map) {
-    override fun toJson(): Map<String, *> = mapOf("name" to "CharacterNode", "value" to "$value")
-}
