944c6993bb975536eedcdeaa3b3a8eaa91a82889
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-12 02:01:47 +0100

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,3 @@
-val KOTLIN_VERSION = "1.9.10"
-
 plugins {
     `kotlin-dsl`
 }
@@ -11,8 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:$KOTLIN_VERSION")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.4")
     implementation("com.github.node-gradle:gradle-node-plugin:7.0.1")
     implementation("gradle.plugin.org.gradle.crypto:checksum:1.4.0")
