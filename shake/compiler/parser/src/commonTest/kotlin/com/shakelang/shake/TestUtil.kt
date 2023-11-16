e1fb954ae87b7d3bd936d018f6dab74e96e7bef6
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-16 23:57:48 +0100

    Create utility function allCombinations

--- shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/TestUtil.kt
+++ shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/TestUtil.kt
@@ -1,9 +1,29 @@
-@file:JvmName("TestUtil")
-
 package com.shakelang.shake
 
-import kotlin.jvm.JvmName
 import kotlin.reflect.KClass
+import kotlin.test.Test
 
 expect fun assertType(expected: KClass<*>, actual: Any)
 expect fun assertArrayEquals(expected: Array<*>, actual: Array<*>)
+
+/**
+ * Returns a stream of all possible permutations of the given list.
+ */
+// a b c -> {a, b c}, {a, c b}, {b, a c}, {b, c a}, {c, a b}, {c, b a}
+fun <T> allCombinations(list: List<T>): Sequence<List<T>> {
+    return sequence {
+        val indices = list.indices.toList()
+
+        for (i in indices.indices) {
+            // get sublist without i
+            val subList = list.subList(0, i) + list.subList(i + 1, list.size)
+            if (subList.isEmpty()) {
+                yield(listOf(list[i]))
+                continue
+            }
+            for(combination in allCombinations(subList)) {
+                yield(listOf(list[i]) + combination)
+            }
+        }
+    }
+}
