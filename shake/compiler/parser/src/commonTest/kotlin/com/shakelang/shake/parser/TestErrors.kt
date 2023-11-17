8d65d9a0565b425210462b94e07218d2c5715ee7
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-17 15:37:37 +0100

    Migrate TestErrors assertions to kotest

--- shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/parser/TestErrors.kt
+++ shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/parser/TestErrors.kt
@@ -2,62 +2,59 @@ package com.shakelang.shake.parser
 
 import com.shakelang.shake.parser.ShakeParserImpl.ParserError
 import com.shakelang.shake.parser.node.ShakeIfNode
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
-import kotlin.test.assertSame
+import io.kotest.matchers.shouldBe
 
 class TestErrors : FreeSpec({
     "test LPAREN" {
-        val error = assertFailsWith(ParserError::class) {
+        val error = shouldThrow<ParserError> {
             ParserTestUtil.parseStatement("<TestLPAREN>", "if test", ShakeIfNode::class)
         }
 
-        assertSame(3, error.start.index)
-        assertSame(6, error.end.index)
-        assertEquals("Expecting '('", error.details)
-        assertEquals("<TestLPAREN>:1:4", error.marker.source)
-        assertEquals("1  if test", error.marker.preview)
-        assertEquals("      ^^^^", error.marker.marker)
+        error.start.index shouldBe 3
+        error.end.index shouldBe 6
+        error.details shouldBe "Expecting '('"
+        error.marker.source shouldBe "<TestLPAREN>:1:4"
+        error.marker.preview shouldBe "1  if test"
+        error.marker.marker shouldBe  "      ^^^^"
     }
 
     "test RPAREN" {
-        val error = assertFailsWith(ParserError::class) {
+        val error = shouldThrow<ParserError> {
             ParserTestUtil.parseStatement("<TestRPAREN>", "if(test{", ShakeIfNode::class)
         }
-        assertSame(7, error.start.index)
-        assertSame(7, error.end.index)
-        assertEquals("Expecting ')'", error.details)
-        assertEquals("<TestRPAREN>:1:8", error.marker.source)
-        assertEquals("1  if(test{", error.marker.preview)
-        assertEquals("          ^", error.marker.marker)
-
+        error.start.index shouldBe 7
+        error.end.index shouldBe 7
+        error.details shouldBe "Expecting ')'"
+        error.marker.source shouldBe "<TestRPAREN>:1:8"
+        error.marker.preview shouldBe "1  if(test{"
+        error.marker.marker shouldBe  "          ^"
     }
 
     "test LCURL" {
-        val error = assertFailsWith(ParserError::class) {
+        val error = shouldThrow <ParserError> {
             ParserTestUtil.parseStatement("<TestLCURL>", "if(test) {", ShakeIfNode::class)
         }
-        assertSame(9, error.start.index)
-        assertSame(9, error.end.index)
-        assertEquals("Expecting '}'", error.details)
-        assertEquals("<TestLCURL>:1:10", error.marker.source)
-        assertEquals("1  if(test) {", error.marker.preview)
-        assertEquals("            ^", error.marker.marker)
+        error.start.index shouldBe 9
+        error.end.index shouldBe 9
+        error.details shouldBe "Expecting '}'"
+        error.marker.source shouldBe "<TestLCURL>:1:10"
+        error.marker.preview shouldBe "1  if(test) {"
+        error.marker.marker shouldBe  "            ^"
     }
 
 
     "test RCURL" {
-        val error = assertFailsWith(ParserError::class) {
+        val error = shouldThrow<ParserError> {
             ParserTestUtil.parseStatement("<TestAwaitSemicolonError>", "for(var i = 0 i<10) {", ShakeIfNode::class)
         }
 
-        assertSame(14, error.start.index)
-        assertSame(14, error.end.index)
-        assertEquals("Expecting semicolon at this point", error.details)
-        assertEquals("<TestAwaitSemicolonError>:1:15", error.marker.source)
-        assertEquals("1  for(var i = 0 i<10) {", error.marker.preview)
-        assertEquals("                 ^", error.marker.marker)
+         error.start.index shouldBe 14
+        error.end.index shouldBe 14
+        error.details shouldBe "Expecting semicolon at this point"
+        error.marker.source shouldBe "<TestAwaitSemicolonError>:1:15"
+        error.marker.preview shouldBe "1  for(var i = 0 i<10) {"
+        error.marker.marker shouldBe  "                 ^"
     }
 })
