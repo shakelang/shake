b6c7651aa507dde04fa5434a7896bf4f237ef067
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-17 13:13:54 +0100

    Migrate InterfaceTests assertions to kotest

--- shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/parser/InterfaceTests.kt
+++ shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/parser/InterfaceTests.kt
@@ -5,12 +5,10 @@ import com.shakelang.shake.parser.node.ShakeAccessDescriber
 import com.shakelang.shake.parser.node.ShakeVariableType
 import com.shakelang.shake.parser.node.functions.ShakeFunctionDeclarationNode
 import com.shakelang.shake.parser.node.objects.ShakeClassDeclarationNode
-import com.shakelang.shake.parser.node.objects.ShakeClassType
 import com.shakelang.shake.parser.node.variables.ShakeVariableDeclarationNode
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.assertions.throwables.shouldThrowWithMessage
 import io.kotest.core.spec.style.FreeSpec
-import kotlin.test.*
+import io.kotest.matchers.shouldBe
 
 class InterfaceTests : FreeSpec({
     "final interface" {
@@ -28,22 +26,22 @@ class InterfaceTests : FreeSpec({
         "${accessPrefix}interface" {
 
             val tree = ParserTestUtil.parse("<${accessPrefix}interface test>", "${accessPrefix}interface test {}")
-            assertEquals(1, tree.children.size)
+            tree.children.size shouldBe 1
             assertType(ShakeClassDeclarationNode::class, tree.children[0])
             val node = tree.children[0] as ShakeClassDeclarationNode
-            assertSame(access, node.access)
-            assertFalse(node.isStatic)
-            assertFalse(node.isFinal)
-            assertEquals("test", node.name)
-            assertSame(0, node.fields.size)
-            assertSame(0, node.methods.size)
-            assertSame(0, node.classes.size)
+            node.access shouldBe access
+            node.isStatic shouldBe false
+            node.isFinal shouldBe false
+            node.name shouldBe "test"
+            node.fields.size shouldBe 0
+            node.methods.size shouldBe 0
+            node.classes.size shouldBe 0
 
         }
 
         "${accessPrefix}final interface" {
 
-            assertFailsWith<ShakeParserImpl.ParserError> {
+            shouldThrow<ShakeParserImpl.ParserError> {
                 ParserTestUtil.parse("<${accessPrefix}interface test>", "${accessPrefix}final interface test {}")
             }
 
@@ -56,24 +54,24 @@ class InterfaceTests : FreeSpec({
 
                 val tree =
                     ParserTestUtil.parse("<${accessPrefix}interface test>", "${accessPrefix}interface test { ${accessPrefix2}int i = 0; }")
-                assertEquals(1, tree.children.size)
+                tree.children.size shouldBe 1
                 assertType(ShakeClassDeclarationNode::class, tree.children[0])
                 val node = tree.children[0] as ShakeClassDeclarationNode
-                assertSame(access, node.access)
-                assertFalse(node.isStatic)
-                assertFalse(node.isFinal)
-                assertEquals("test", node.name)
-                assertSame(1, node.fields.size)
-                assertSame(0, node.methods.size)
-                assertSame(0, node.classes.size)
+                node.access shouldBe access
+                node.isStatic shouldBe false
+                node.isFinal shouldBe false
+                node.name shouldBe "test"
+                node.fields.size shouldBe 1
+                node.methods.size shouldBe 0
+                node.classes.size shouldBe 0
                 assertType(ShakeVariableDeclarationNode::class, node.fields[0])
                 val variable = node.fields[0]
-                assertSame(ShakeVariableType.Type.INTEGER, variable.type.type)
-                assertSame(access, variable.access)
-                assertNotNull(variable.value)
-                assertEquals("i", variable.name)
-                assertFalse(variable.isStatic)
-                assertFalse(variable.isFinal)
+                variable.type.type shouldBe ShakeVariableType.Type.INTEGER
+                variable.access shouldBe access2
+                variable.value shouldBe 0
+                variable.name shouldBe "i"
+                variable.isStatic shouldBe false
+                variable.isFinal shouldBe false
 
             }
 
@@ -81,24 +79,24 @@ class InterfaceTests : FreeSpec({
 
                 val tree =
                     ParserTestUtil.parse("<${accessPrefix}interface test>", "${accessPrefix}interface test { ${accessPrefix2}void f() {} }")
-                assertEquals(1, tree.children.size)
+                tree.children.size shouldBe 1
                 assertType(ShakeClassDeclarationNode::class, tree.children[0])
                 val node = tree.children[0] as ShakeClassDeclarationNode
-                assertSame(access, node.access)
-                assertFalse(node.isStatic)
-                assertFalse(node.isFinal)
-                assertEquals("test", node.name)
-                assertSame(0, node.fields.size)
-                assertSame(1, node.methods.size)
-                assertSame(0, node.classes.size)
+                node.access shouldBe access
+                node.isStatic shouldBe false
+                node.isFinal shouldBe false
+                node.name shouldBe "test"
+                node.fields.size shouldBe 0
+                node.methods.size shouldBe 1
+                node.classes.size shouldBe 0
                 assertType(ShakeFunctionDeclarationNode::class, node.methods[0])
                 val function = node.methods[0]
-                assertSame(ShakeVariableType.Type.VOID, function.type.type)
-                assertSame(access, function.access)
-                assertSame(0, function.args.size)
-                assertEquals("f", function.name)
-                assertFalse(function.isStatic)
-                assertFalse(function.isFinal)
+                function.type shouldBe ShakeVariableType.VOID
+                function.access shouldBe access2
+                function.args.size shouldBe 0
+                function.name shouldBe "f"
+                function.isStatic shouldBe false
+                function.isFinal shouldBe false
 
             }
 
@@ -106,26 +104,25 @@ class InterfaceTests : FreeSpec({
 
                 val tree =
                     ParserTestUtil.parse("<${accessPrefix}interface test>", "${accessPrefix}interface test { ${accessPrefix2}class Test {} }")
-                assertEquals(1, tree.children.size)
+                tree.children.size shouldBe 1
                 assertType(ShakeClassDeclarationNode::class, tree.children[0])
                 val node = tree.children[0] as ShakeClassDeclarationNode
-                assertSame(access, node.access)
-                assertFalse(node.isStatic)
-                assertFalse(node.isFinal)
-                assertEquals("test", node.name)
-                assertSame(0, node.fields.size)
-                assertSame(0, node.methods.size)
-                assertSame(1, node.classes.size)
+                node.access shouldBe access
+                node.isStatic shouldBe false
+                node.isFinal shouldBe false
+                node.name shouldBe "test"
+                node.fields.size shouldBe 0
+                node.methods.size shouldBe 0
+                node.classes.size shouldBe 1
                 assertType(ShakeClassDeclarationNode::class, node.classes[0])
                 val clazz = node.classes[0]
-                assertSame(access2, clazz.access)
-                assertFalse(clazz.isStatic)
-                assertFalse(clazz.isFinal)
-                assertEquals("Test", clazz.name)
-                assertSame(0, clazz.fields.size)
-                assertSame(0, clazz.methods.size)
-                assertSame(0, clazz.classes.size)
-
+                clazz.access shouldBe access2
+                clazz.isStatic shouldBe false
+                clazz.isFinal shouldBe false
+                clazz.name shouldBe "Test"
+                clazz.fields.size shouldBe 0
+                clazz.methods.size shouldBe 0
+                clazz.classes.size shouldBe 0
             }
         }
     }
