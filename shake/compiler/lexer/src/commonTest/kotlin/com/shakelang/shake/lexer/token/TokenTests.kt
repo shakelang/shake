f496080cec5a6bb1e527dddd8d47d2d5673ea733
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-20 12:15:26 +0100

    Migrate tests of TokenTests to kotest

--- shake/compiler/lexer/src/commonTest/kotlin/com/shakelang/shake/lexer/token/TokenTests.kt
+++ shake/compiler/lexer/src/commonTest/kotlin/com/shakelang/shake/lexer/token/TokenTests.kt
@@ -1,46 +1,35 @@
 package com.shakelang.shake.lexer.token
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotEquals
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 
-class TokenTests {
+class TokenTests : FreeSpec({
 
-    @Test
-    fun testTokenOneArgument() {
+    "token one argument" {
         val token = ShakeToken(ShakeTokenType.IDENTIFIER, "test", 7, 10)
-        assertEquals(ShakeTokenType.IDENTIFIER, token.type)
-        assertEquals("test", token.value)
-        assertEquals(7, token.start)
-        assertEquals(10, token.end)
+        token.type shouldBe ShakeTokenType.IDENTIFIER
+        token.value shouldBe "test"
+        token.start shouldBe 7
+        token.end shouldBe 10
     }
 
-    @Test
-    fun testTokenTwoArguments() {
-        val token = ShakeToken(ShakeTokenType.IDENTIFIER, "test", 7, 10)
-        assertEquals(ShakeTokenType.IDENTIFIER, token.type)
-        assertEquals("test", token.value)
-        assertEquals(7, token.start)
-        assertEquals(10, token.end)
-    }
-
-    @Test
-    fun testTokenEquals() {
+    "token equals" {
         val token0 = ShakeToken(ShakeTokenType.IDENTIFIER, "test", 7, 10)
         val token1 = ShakeToken(ShakeTokenType.IDENTIFIER, "test", 7, 10)
         val token2 = ShakeToken(ShakeTokenType.IDENTIFIER, "test", 7, 11)
         val token3 = ShakeToken(ShakeTokenType.IDENTIFIER, "test2", 8, 10)
         val token4 = ShakeToken(ShakeTokenType.ASSIGN, 8, 10)
 
-        assertEquals(token0, token0)
-        assertEquals(token0, token1)
-        assertEquals(token0, token2)
-        assertNotEquals(token0, token3)
-        assertNotEquals(token0, token4)
+        token0 shouldBe token0
+        token0 shouldBe token1
+        token0 shouldBe  token2
+        token0 shouldNotBe token3
+        token0 shouldNotBe token4
 
-        assertEquals(token1, token0)
-        assertEquals(token2, token0)
-        assertNotEquals(token3, token0)
-        assertNotEquals(token4, token0)
+        token1 shouldBe token0
+        token2 shouldBe token0
+        token3 shouldNotBe token0
+        token4 shouldNotBe token0
     }
-}
+})
