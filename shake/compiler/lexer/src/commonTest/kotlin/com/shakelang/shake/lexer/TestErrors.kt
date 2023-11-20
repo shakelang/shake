44b774121dde7e8ed935c41d308e9cf7c800872d
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-20 22:32:04 +0100

    Migrate TestErrors to kotest

--- shake/compiler/lexer/src/commonTest/kotlin/com/shakelang/shake/lexer/TestErrors.kt
+++ shake/compiler/lexer/src/commonTest/kotlin/com/shakelang/shake/lexer/TestErrors.kt
@@ -2,90 +2,75 @@ package com.shakelang.shake.lexer
 
 import com.shakelang.shake.util.parseutils.characters.streaming.CharacterInputStream
 import com.shakelang.shake.util.parseutils.characters.streaming.SourceCharacterInputStream
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
-import kotlin.test.assertSame
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
-class TestErrors {
-    @Test
-    fun testStringMustEndError() {
+@Suppress("unused")
+class TestErrors : FreeSpec({
+
+    "testStringMustEndError" {
         // String type 1 not finished
-        val error = assertFailsWith(ShakeLexingBase.LexerError::class) {
+        val error = shouldThrow<ShakeLexingBase.LexerError> {
             val chars: CharacterInputStream = SourceCharacterInputStream("<tests>", "\"test")
             val lexer = ShakeLexer(chars)
             lexer.makeTokens()
         }
 
-        // System.out.println(error.toString());
-        assertSame(4, error.start.index)
-        assertSame(4, error.end.index)
-        assertEquals("String must end with a '\"'", error.details)
-        assertEquals("<tests>:1:5", error.marker.source)
-        assertEquals("1  \"test", error.marker.preview)
-        assertEquals("       ^", error.marker.marker)
-
-        // System.out.println(Formatting.FGColor.GREEN + "\u2705 Correct error was thrown" + Formatting.RESET);
+        error.start.index shouldBe 4
+        error.end.index shouldBe 4
+        error.details shouldBe "String must end with a '\"'"
+        error.marker.source shouldBe "<tests>:1:5"
+        error.marker.preview shouldBe "1  \"test"
+        error.marker.marker shouldBe "       ^"
     }
 
-    @Test
-    fun testUnknownEscapeSequenceError() {
+    "testUnknownEscapeSequenceError" {
         // Unknown escape sequence (using \a here)
-        val error = assertFailsWith(ShakeLexingBase.LexerError::class) {
+        val error = shouldThrow<ShakeLexingBase.LexerError> {
             val chars: CharacterInputStream = SourceCharacterInputStream("<tests>", "\"\\a\"")
             val lexer = ShakeLexer(chars)
             lexer.makeTokens()
         }
 
-        // System.out.println(error.toString());
-        assertSame(2, error.start.index)
-        assertSame(2, error.end.index)
-        assertEquals("Unknown escape sequence '\\a'", error.details)
-        assertEquals("<tests>:1:3", error.marker.source)
-        assertEquals("1  \"\\a\"", error.marker.preview)
-        assertEquals("     ^", error.marker.marker)
-
-        // System.out.println(Formatting.FGColor.GREEN + "\u2705 Correct error was thrown" + Formatting.RESET);
+        error.start.index shouldBe 2
+        error.end.index shouldBe 2
+        error.details shouldBe "Unknown escape sequence '\\a'"
+        error.marker.source shouldBe "<tests>:1:3"
+        error.marker.preview shouldBe "1  \"\\a\""
+        error.marker.marker shouldBe "     ^"
     }
 
-    @Test
-    fun testExpectHexCharacterError() {
+    "testExpectHexCharacterError" {
         // Wrong input to unicode character
-        val error = assertFailsWith(ShakeLexingBase.LexerError::class) {
+        val error = shouldThrow<ShakeLexingBase.LexerError> {
             val chars: CharacterInputStream = SourceCharacterInputStream("<tests>", "\"\\uatea\"")
             val lexer = ShakeLexer(chars)
             lexer.makeTokens()
         }
 
-        // System.out.println(error.toString());
-        assertSame(4, error.start.index)
-        assertSame(4, error.end.index)
-        assertEquals("Expecting hex char", error.details)
-        assertEquals("<tests>:1:5", error.marker.source)
-        assertEquals("1  \"\\uatea\"", error.marker.preview)
-        assertEquals("       ^", error.marker.marker)
-
-        // System.out.println(Formatting.FGColor.GREEN + "\u2705 Correct error was thrown" + Formatting.RESET);
+        error.start.index shouldBe 4
+        error.end.index shouldBe 4
+        error.details shouldBe "Expecting hex char"
+        error.marker.source shouldBe "<tests>:1:5"
+        error.marker.preview shouldBe "1  \"\\uatea\""
+        error.marker.marker shouldBe "       ^"
     }
 
-    @Test
-    fun testUnexpectedTokenError() {
+    "testUnexpectedTokenError" {
         // Unexpected Token
-        val error = assertFailsWith(ShakeLexingBase.LexerError::class) {
+        val error = shouldThrow<ShakeLexingBase.LexerError> {
             val chars: CharacterInputStream = SourceCharacterInputStream("<tests>", "\u00dc")
             val lexer = ShakeLexer(chars)
             lexer.makeTokens()
         }
 
-        // System.out.println(error.toString());
-        assertSame(0, error.start.index)
-        assertSame(0, error.end.index)
-        assertEquals("Unrecognised Token: '\u00dc'", error.details)
-        assertEquals("UnexpectedTokenError", error.exceptionName)
-        assertEquals("<tests>:1:1", error.marker.source)
-        assertEquals("1  \u00dc", error.marker.preview)
-        assertEquals("   ^", error.marker.marker)
-
-        // System.out.println(Formatting.FGColor.GREEN + "\u2705 Correct error was thrown" + Formatting.RESET);
+        error.start.index shouldBe 0
+        error.end.index shouldBe 0
+        error.details shouldBe "Unrecognised Token: '\u00dc'"
+        error.exceptionName shouldBe "UnexpectedTokenError"
+        error.marker.source shouldBe "<tests>:1:1"
+        error.marker.preview shouldBe "1  \u00dc"
+        error.marker.marker shouldBe "   ^"
     }
-}
+})
