c57ee2ba6fdf3cb0af94656774ad97aaa31921c5
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-20 11:04:50 +0100

    Migrate tests of OnDemandLexingTokenInputStreamTests to kotest

--- shake/compiler/lexer/build.gradle.kts
+++ shake/compiler/lexer/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.shakelang.shake.util.changelog.resolveVersion
+import conventions.KOTEST_VERSION
 import conventions.dependencies
 import conventions.projectGroup
 
@@ -15,5 +16,12 @@ kotlin {
     dependencies {
         implementation(project(":util:parseutils"))
         testImplementation(kotlin("test"))
+        testImplementation("io.kotest:kotest-framework-engine:$KOTEST_VERSION")
+        testImplementation("io.kotest:kotest-assertions-core:$KOTEST_VERSION")
+        testImplementation("io.kotest:kotest-property:$KOTEST_VERSION")
+
+        jvmTest {
+            implementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
+        }
     }
 }
