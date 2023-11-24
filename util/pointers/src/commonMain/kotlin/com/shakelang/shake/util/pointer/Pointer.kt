37c330b1cae9e33cdb782b9a83e0ad6c179a6051
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-24 14:06:51 +0100

    Finish writing documentation for pointer library

--- util/pointers/src/commonMain/kotlin/com/shakelang/shake/util/pointer/Pointer.kt
+++ util/pointers/src/commonMain/kotlin/com/shakelang/shake/util/pointer/Pointer.kt
@@ -254,35 +254,168 @@ fun <T> T.point(): Pointer<T> = Pointer.of(this)
  */
 fun <T> T.mutablePoint(): MutablePointer<T> = Pointer.mutableOf(this)
 
+/**
+ * Create a list of pointers pointing to the values of the list
+ * @receiver The list of values the pointers should point to
+ * @return The list of pointers
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 fun <T> Iterable<T>.points(): PointerList<T> = map { Pointer.of(it) }
-fun <T> Iterable<T>.mutablePoints(): MutablePointerList<T> = map { Pointer.of(it) }.toMutableList()
 
+/**
+ * A mutable pointer points to a value in memory. It can be used to pass a value by reference.
+ * The advantage of a pointer is that it can be used to change the value of a variable
+ * in a function.
+ *
+ * @param T The type of the value the pointer points to
+ * @since 0.1.0
+ * @version 0.1.0
+ * @see Pointer
+ * @see LateInitPointer
+ */
 interface MutablePointer<T> : Pointer<T> {
+
+    /**
+     * The value the pointer points to
+     */
     override var value: T
 }
 
+/**
+ * A late init pointer points to a value in memory. It can be used to pass a value by reference.
+ * The advantage of a pointer is that it can be used to change the value of a variable
+ * in a function.
+ *
+ * The difference between a [LateInitPointer] and a [MutablePointer] is that a [LateInitPointer]
+ * can be initialized later and a [LateInitPointer] can only be initialized once.
+ *
+ * @param T The type of the value the pointer points to
+ * @since 0.1.0
+ * @version 0.1.0
+ * @see Pointer
+ * @see MutablePointer
+ * @see LateInitMutablePointer
+ */
 interface LateInitPointer<T> : Pointer<T> {
+
+    /**
+     * The value the pointer points to
+     */
     override val value: T
+
+    /**
+     * Is the pointer initialized
+     */
     val isInitialized: Boolean
+
+    /**
+     * Initialize the pointer
+     *
+     * @param value The value the pointer should point to
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     fun init(value: T)
 }
 
+/**
+ * A mutable late init pointer points to a value in memory. It can be used to pass a value by reference.
+ * The advantage of a pointer is that it can be used to change the value of a variable
+ * in a function.
+ *
+ * A [LateInitMutablePointer] combines the features of a [MutablePointer] and a [LateInitPointer].
+ *
+ * @param T The type of the value the pointer points to
+ * @since 0.1.0
+ * @version 0.1.0
+ * @see Pointer
+ * @see MutablePointer
+ * @see LateInitPointer
+ * @see LateInitMutablePointer
+ */
 interface LateInitMutablePointer<T> : MutablePointer<T>, LateInitPointer<T> {
+    /**
+     * The value the pointer points to
+     */
     override var value: T
 }
 
+/**
+ * A PointerList refers to a list of pointers.
+ *
+ * @param T The type of the value the pointers point to
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 typealias PointerList<T> = List<Pointer<T>>
+
+/**
+ * A MutablePointerList refers to a list of mutable pointers.
+ *
+ * @param T The type of the value the pointers point to
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 typealias MutablePointerList<T> = MutableList<Pointer<T>>
 
+/**
+ * You can create a list that points to the values of a [PointerList] with this function.
+ *
+ * @receiver The list of pointers
+ * @return The list of values
+ * @since 0.1.0
+ * @version 0.1.0
+ * @see PointerList
+ * @see PointingList
+ */
 fun <T> PointerList<T>.values(): PointingList<T> = PointingList.from(this)
+
+/**
+ * You can create a list that points to the values of a [MutablePointerList] with this function.
+ *
+ * @receiver The list of pointers
+ * @return The list of values
+ * @since 0.1.0
+ * @version 0.1.0
+ * @see MutablePointerList
+ * @see MutablePointingList
+ */
 fun <T> MutablePointerList<T>.values(): MutablePointingList<T> = MutablePointingList.from(this)
 
+/**
+ * A PointingList refers to a list of values that are pointed to by pointers.
+ *
+ * @param T The type of the value the pointers point to
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 interface PointingList<T> : List<T> {
+
+    /**
+     * The list of pointers
+     */
     val pointers: List<Pointer<T>>
 
+    /**
+     * The implementation of the [PointingList].
+     * @param T The type of the value the pointers point to
+     * @since 0.1.0
+     * @version 0.1.0
+     * @see PointingList
+     */
     private class Impl<T>(
+
+        /**
+         * The list of pointers
+         */
         override val pointers: List<Pointer<T>>
+
     ) : PointingList<T> {
+
+        /**
+         * Returns the element at the specified index in the list.
+         */
         override val size: Int
             get() = pointers.size
 
@@ -293,10 +426,16 @@ interface PointingList<T> : List<T> {
             return pointers[index].value
         }
 
+        /**
+         * Returns `true` if the list is empty (contains no elements), `false` otherwise.
+         */
         override fun isEmpty(): Boolean {
             return pointers.isEmpty()
         }
 
+        /**
+         * Returns an iterator over the elements of this list in proper sequence.
+         */
         override fun iterator(): Iterator<T> {
             val iterator = pointers.iterator()
             return object : Iterator<T> {
@@ -353,26 +492,63 @@ interface PointingList<T> : List<T> {
             return pointers.indexOfFirst { it.value == element }
         }
 
+        /**
+         * Returns `true` if all elements in the specified collection are contained in this collection.
+         */
         override fun containsAll(elements: Collection<T>): Boolean {
             return pointers.all { it.value in elements }
         }
 
+        /**
+         * Returns `true` if the specified element is contained in this collection.
+         */
         override fun contains(element: T): Boolean {
             return pointers.any { it.value == element }
         }
     }
 
     companion object {
+
+        /**
+         * Create a new [PointingList] from the given list of pointers
+         *
+         * @param it The list of pointers
+         * @return The new [PointingList]
+         * @since 0.1.0
+         * @version 0.1.0
+         */
         fun <T> from(it: List<Pointer<T>>): PointingList<T> {
             return Impl(it)
         }
     }
 }
 
+
+/**
+ * A MutablePointingList refers to a list of values that are pointed to by mutable pointers.
+ *
+ * @param T The type of the value the pointers point to
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 interface MutablePointingList<T> : PointingList<T>, MutableList<T> {
+
+    /**
+     * The list of pointers
+     */
     override val pointers: MutableList<Pointer<T>>
 
+    /**
+     * The implementation of the [MutablePointingList].
+     * @param T The type of the value the pointers point to
+     * @since 0.1.0
+     * @version 0.1.0
+     * @see MutablePointingList
+     */
     class Impl<T>(
+        /**
+         * The list of pointers
+         */
         override val pointers: MutablePointerList<T>
     ) : MutablePointingList<T> {
         override val size: Int
@@ -476,24 +652,63 @@ interface MutablePointingList<T> : PointingList<T>, MutableList<T> {
     }
 
     companion object {
+
+        /**
+         * Create a new [MutablePointingList] from the given list of pointers
+         *
+         * @param it The list of pointers
+         * @return The new [MutablePointingList]
+         * @since 0.1.0
+         * @version 0.1.0
+         */
         fun <T> from(it: MutablePointerList<T>): MutablePointingList<T> {
             return Impl(it)
         }
     }
 }
 
+/**
+ * Make sure that the value of the pointer is not null
+ * @param msg The message of the exception that will be thrown if the value is null
+ * @return The pointer
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 fun <T> Pointer<T?>.notNull(msg: String? = null): Pointer<T> {
     return this.transform { it ?: throw IllegalStateException(msg ?: "null value not allowed") }
 }
 
+/**
+ * A register for pointers.
+ * @param T The type of the value the pointers point to
+ * @since 0.1.0
+ * @version 0.1.0
+ * @see Pointer
+ * @see MutablePointer
+ */
 class PointerRegister<T> {
+
+    /**
+     * The list of pointers
+     */
     private val pointers = mutableListOf<Pointer<T>?>()
+
+    /**
+     * The list of unused indices
+     */
     private val unusedIndices = mutableListOf<Int>()
 
     init {
         instances.add(this)
     }
 
+    /**
+     * Register a pointer to the register (and return the index of the pointer)
+     * @param pointer The pointer to register
+     * @return The index of the pointer
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     fun register(pointer: Pointer<T>): Int {
         val index = unusedIndices.removeLastOrNull()
         if (index != null) pointers[index] = pointer
@@ -504,6 +719,13 @@ class PointerRegister<T> {
         return index
     }
 
+    /**
+     * Get the index of the given pointer
+     * @param pointer The pointer
+     * @return The index of the pointer or -1 if the pointer is not registered
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     fun indexOf(pointer: Pointer<Any?>): Int {
         this.pointers.forEachIndexed { index, p ->
             if (p == pointer) return index
@@ -511,62 +733,155 @@ class PointerRegister<T> {
         return -1
     }
 
+    /**
+     * Unregister the pointer at the given index
+     * @param index The index of the pointer
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     fun unregister(index: Int) {
         pointers[index] = null
         unusedIndices.add(index)
     }
 
+    /**
+     * Get the pointer at the given index
+     * @throws IllegalStateException If the pointer is not registered
+     * @param index The index of the pointer
+     * @return The pointer
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     fun get(index: Int): Pointer<T> {
         return pointers[index] ?: throw IllegalStateException("pointer at index $index is null")
     }
 
+    /**
+     * Get the pointer at the given index or null if the pointer is not registered
+     * @param index The index of the pointer
+     * @return The pointer or null
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     fun getOrNull(index: Int): Pointer<T>? {
         return pointers[index]
     }
 
-    fun getPosition(pointer: Pointer<T>): Int {
-        return pointers.indexOf(pointer)
-    }
-
+    /**
+     * Create a late init pointer inside the register
+     * @return The new pointer
+     * @since 0.1.0
+     * @version 0.1.0
+     * @see LateInitPointer
+     * @see Pointer.late
+     */
     fun createPointer(): LateInitPointer<T> {
         val pointer = Pointer.late<T>()
         register(pointer)
         return pointer
     }
 
+    /**
+     * Create a late init pointer inside the register
+     *
+     * @param value The value the pointer should point to
+     * @return The new pointer
+     * @since 0.1.0
+     * @version 0.1.0
+     * @see LateInitPointer
+     * @see Pointer.late
+     */
     fun createPointer(value: T): Pointer<T> {
         val pointer = Pointer.of(value)
         register(pointer)
         return pointer
     }
 
+    /**
+     * Create a mutable pointer inside the register
+     *
+     * @return The new pointer
+     * @since 0.1.0
+     * @version 0.1.0
+     * @see MutablePointer
+     * @see Pointer.mutableOf
+     */
     fun createMutablePointer(): LateInitMutablePointer<T> {
         val pointer = Pointer.lateMutable<T>()
         register(pointer)
         return pointer
     }
 
+    /**
+     * Create a mutable pointer inside the register
+     *
+     * @param value The value the pointer should point to
+     * @return The new pointer
+     * @since 0.1.0
+     * @version 0.1.0
+     * @see MutablePointer
+     * @see Pointer.mutableOf
+     */
     fun createMutablePointer(value: T): MutablePointer<T> {
         val pointer = Pointer.mutableOf(value)
         register(pointer)
         return pointer
     }
 
+    /**
+     * Destroy the register
+     * @since 0.1.0
+     * @version 0.1.0
+     */
+    fun destroy() {
+        instances.remove(this)
+    }
+
+    /**
+     * Destroy all contained pointers
+     * @since 0.1.0
+     * @version 0.1.0
+     */
+    fun destroyAllContainedPointers() {
+        pointers.forEach { it?.unregister() }
+    }
+
     companion object {
+        /**
+         * The list of all instances of pointer registers
+         */
         val instances = mutableListOf<PointerRegister<*>>()
     }
 }
 
+/**
+ * Register the pointer to the given register
+ * @param register The register
+ * @return The index of the pointer
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 fun <T> Pointer<T>.register(register: PointerRegister<T>): Int {
     return register.register(this)
 }
 
+/**
+ * Unregister the pointer from the given register
+ * @param register The register
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 fun <T> Pointer<T>.unregister(register: PointerRegister<T>) {
-    val index = register.getPosition(this)
+    val index = register.indexOf(this)
     if (index == -1) throw IllegalStateException("pointer is not registered")
     register.unregister(index)
 }
 
+/**
+ * Close the pointer (unregister it from all registers)
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 fun Pointer<*>.unregister() {
     PointerRegister.instances.forEach {
         val index = it.indexOf(this)
@@ -574,6 +889,11 @@ fun Pointer<*>.unregister() {
     }
 }
 
+/**
+ * Close the pointer (unregister it from all registers)
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 fun Pointer<*>.close() {
     unregister()
 }
