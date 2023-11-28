bfbccf56c7af723524f2b960dd8cd205dce8d3a0
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-28 20:05:12 +0100

    Split up StorageFormat

---
+++ shake/bytecode/interpreter/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/format/Class.kt
@@ -0,0 +1,157 @@
+package com.shakelang.shake.bytecode.interpreter.format
+
+import com.shakelang.shake.bytecode.interpreter.format.pool.ConstantPool
+import com.shakelang.shake.bytecode.interpreter.format.pool.MutableConstantPool
+import com.shakelang.shake.util.io.streaming.input.DataInputStream
+import com.shakelang.shake.util.io.streaming.output.ByteArrayOutputStream
+import com.shakelang.shake.util.io.streaming.output.DataOutputStream
+
+
+open class Class(
+    open val pool: ConstantPool,
+    open val nameConstant: Int,
+    open val superNameConstant: Int,
+    open val interfacesConstants: List<Int>,
+    open val fields: List<Int>,
+    open val methods: List<Int>,
+    open val subClasses: List<Int>,
+) {
+
+    val name: String get() = pool.getUtf8(nameConstant).value
+    val superName: String get() = pool.getUtf8(superNameConstant).value
+    val interfaces: List<String> get() = interfacesConstants.map { pool.getUtf8(it).value }
+
+    fun dump(stream: DataOutputStream) {
+        stream.writeInt(nameConstant)
+        stream.writeInt(superNameConstant)
+        stream.writeInt(interfacesConstants.size)
+        for (interfaceConstant in interfacesConstants) {
+            stream.writeInt(interfaceConstant)
+        }
+        stream.writeInt(fields.size)
+        for (field in fields) {
+            stream.writeInt(field)
+        }
+        stream.writeInt(methods.size)
+        for (method in methods) {
+            stream.writeInt(method)
+        }
+        stream.writeInt(subClasses.size)
+        for (subClass in subClasses) {
+            stream.writeInt(subClass)
+        }
+    }
+
+    fun dump(): ByteArray {
+        val byteStream = ByteArrayOutputStream()
+        val stream = DataOutputStream(byteStream)
+        dump(stream)
+        return byteStream.toByteArray()
+    }
+
+    companion object {
+        fun fromStream(stream: DataInputStream): Class {
+            val name = stream.readInt()
+            val superName = stream.readInt()
+            val interfacesCount = stream.readInt()
+            val interfaces = mutableListOf<Int>()
+            for (i in 0 until interfacesCount) {
+                interfaces.add(stream.readInt())
+            }
+            val fieldsCount = stream.readInt()
+            val fields = mutableListOf<Int>()
+            for (i in 0 until fieldsCount) {
+                fields.add(stream.readInt())
+            }
+            val methodsCount = stream.readInt()
+            val methods = mutableListOf<Int>()
+            for (i in 0 until methodsCount) {
+                methods.add(stream.readInt())
+            }
+            val subClassesCount = stream.readInt()
+            val subClasses = mutableListOf<Int>()
+            for (i in 0 until subClassesCount) {
+                subClasses.add(stream.readInt())
+            }
+            return Class(
+                ConstantPool.fromStream(stream),
+                name,
+                superName,
+                interfaces,
+                fields,
+                methods,
+                subClasses
+            )
+        }
+    }
+}
+
+class MutableClass(
+    override val pool: MutableConstantPool,
+    override var nameConstant: Int,
+    override var superNameConstant: Int,
+    override var interfacesConstants: MutableList<Int>,
+    override var fields: MutableList<Int>,
+    override var methods: MutableList<Int>,
+    override var subClasses: MutableList<Int>,
+) : Class(pool, nameConstant, superNameConstant, interfacesConstants, fields, methods, subClasses) {
+    fun setName(name: String) {
+        nameConstant = pool.resolveUtf8(name)
+    }
+
+    fun setSuperName(superName: String) {
+        superNameConstant = pool.resolveUtf8(superName)
+    }
+
+    fun addInterface(interfaceName: String) {
+        interfacesConstants.add(pool.resolveUtf8(interfaceName))
+    }
+
+    fun addField(field: Int) {
+        fields.add(field)
+    }
+
+    fun addMethod(method: Int) {
+        methods.add(method)
+    }
+
+    fun addSubClass(subClass: Int) {
+        subClasses.add(subClass)
+    }
+
+    companion object {
+        fun fromStream(stream: DataInputStream): MutableClass {
+            val name = stream.readInt()
+            val superName = stream.readInt()
+            val interfacesCount = stream.readInt()
+            val interfaces = mutableListOf<Int>()
+            for (i in 0 until interfacesCount) {
+                interfaces.add(stream.readInt())
+            }
+            val fieldsCount = stream.readInt()
+            val fields = mutableListOf<Int>()
+            for (i in 0 until fieldsCount) {
+                fields.add(stream.readInt())
+            }
+            val methodsCount = stream.readInt()
+            val methods = mutableListOf<Int>()
+            for (i in 0 until methodsCount) {
+                methods.add(stream.readInt())
+            }
+            val subClassesCount = stream.readInt()
+            val subClasses = mutableListOf<Int>()
+            for (i in 0 until subClassesCount) {
+                subClasses.add(stream.readInt())
+            }
+            return MutableClass(
+                MutableConstantPool.fromStream(stream),
+                name,
+                superName,
+                interfaces,
+                fields,
+                methods,
+                subClasses
+            )
+        }
+    }
+}
---
+++ shake/bytecode/interpreter/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/format/pool/ConstantPoolEntry.kt
@@ -0,0 +1,173 @@
+package com.shakelang.shake.bytecode.interpreter.format.pool
+
+import com.shakelang.shake.util.io.streaming.input.DataInputStream
+import com.shakelang.shake.util.io.streaming.output.ByteArrayOutputStream
+import com.shakelang.shake.util.io.streaming.output.DataOutputStream
+
+
+sealed class ConstantPoolEntry {
+
+    abstract override fun toString(): String
+    abstract fun dump(stream: DataOutputStream)
+
+    fun dump(): ByteArray {
+        val byteStream = ByteArrayOutputStream()
+        val stream = DataOutputStream(byteStream)
+        dump(stream)
+        return byteStream.toByteArray()
+    }
+
+    class Utf8Constant(val value: String) : ConstantPoolEntry() {
+        override fun toString(): String {
+            return "Utf8Constant(value='$value')"
+        }
+
+        override fun dump(stream: DataOutputStream) {
+            stream.writeByte(1)
+            stream.writeUTF8(value)
+        }
+
+        companion object {
+            fun fromStream(stream: DataInputStream): Utf8Constant {
+                return Utf8Constant(stream.readUTF())
+            }
+        }
+
+    }
+
+    class ByteConstant(val value: Byte) : ConstantPoolEntry() {
+        override fun toString(): String {
+            return "ByteConstant(value=$value)"
+        }
+
+        override fun dump(stream: DataOutputStream) {
+            stream.writeByte(2)
+            stream.writeByte(value)
+        }
+
+        companion object {
+            fun fromStream(stream: DataInputStream): ByteConstant {
+                return ByteConstant(stream.readByte())
+            }
+        }
+    }
+
+    class ShortConstant(val value: Short) : ConstantPoolEntry() {
+        override fun toString(): String {
+            return "ShortConstant(value=$value)"
+        }
+
+        override fun dump(stream: DataOutputStream) {
+            stream.writeByte(3)
+            stream.writeShort(value)
+        }
+
+        companion object {
+            fun fromStream(stream: DataInputStream): ShortConstant {
+                return ShortConstant(stream.readShort())
+            }
+        }
+    }
+
+    class IntConstant(val value: Int) : ConstantPoolEntry() {
+        override fun toString(): String {
+            return "IntConstant(value=$value)"
+        }
+
+        override fun dump(stream: DataOutputStream) {
+            stream.writeByte(4)
+            stream.writeInt(value)
+        }
+
+        companion object {
+            fun fromStream(stream: DataInputStream): IntConstant {
+                return IntConstant(stream.readInt())
+            }
+        }
+    }
+
+    class LongConstant(val value: Long) : ConstantPoolEntry() {
+        override fun toString(): String {
+            return "LongConstant(value=$value)"
+        }
+
+        override fun dump(stream: DataOutputStream) {
+            stream.writeByte(5)
+            stream.writeLong(value)
+        }
+
+        companion object {
+            fun fromStream(stream: DataInputStream): LongConstant {
+                return LongConstant(stream.readLong())
+            }
+        }
+    }
+
+    class FloatConstant(val value: Float) : ConstantPoolEntry() {
+        override fun toString(): String {
+            return "FloatConstant(value=$value)"
+        }
+
+        override fun dump(stream: DataOutputStream) {
+            stream.writeByte(6)
+            stream.writeFloat(value)
+        }
+
+        companion object {
+            fun fromStream(stream: DataInputStream): FloatConstant {
+                return FloatConstant(stream.readFloat())
+            }
+        }
+    }
+
+    class DoubleConstant(val value: Double) : ConstantPoolEntry() {
+        override fun toString(): String {
+            return "DoubleConstant(value=$value)"
+        }
+
+        override fun dump(stream: DataOutputStream) {
+            stream.writeByte(7)
+            stream.writeDouble(value)
+        }
+
+        companion object {
+            fun fromStream(stream: DataInputStream): DoubleConstant {
+                return DoubleConstant(stream.readDouble())
+            }
+        }
+    }
+
+    class ClassConstant(val identifier: Int) : ConstantPoolEntry() {
+        override fun toString(): String {
+            return "ClassConstant(identifier=$identifier)"
+        }
+
+        override fun dump(stream: DataOutputStream) {
+            stream.writeByte(8)
+            stream.writeInt(identifier)
+        }
+
+        companion object {
+            fun fromStream(stream: DataInputStream): ClassConstant {
+                return ClassConstant(stream.readInt())
+            }
+        }
+    }
+
+    companion object {
+        fun fromStream(stream: DataInputStream): ConstantPoolEntry {
+            val type = stream.readByte()
+            return when (type) {
+                1.toByte() -> Utf8Constant.fromStream(stream)
+                2.toByte() -> ByteConstant.fromStream(stream)
+                3.toByte() -> ShortConstant.fromStream(stream)
+                4.toByte() -> IntConstant.fromStream(stream)
+                5.toByte() -> LongConstant.fromStream(stream)
+                6.toByte() -> FloatConstant.fromStream(stream)
+                7.toByte() -> DoubleConstant.fromStream(stream)
+                8.toByte() -> ClassConstant.fromStream(stream)
+                else -> throw Exception("Unknown constant pool entry type: $type")
+            }
+        }
+    }
+}
