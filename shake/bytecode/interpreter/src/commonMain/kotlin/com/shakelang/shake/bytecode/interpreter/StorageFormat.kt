20c460704b3166f23557dc3bedc35786a18d94e3
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-11-28 19:01:26 +0100

    Create MutableConstantPool

--- shake/bytecode/interpreter/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/StorageFormat.kt
+++ shake/bytecode/interpreter/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/StorageFormat.kt
@@ -5,8 +5,8 @@ import com.shakelang.shake.util.io.streaming.input.dataStream
 import com.shakelang.shake.util.io.streaming.output.ByteArrayOutputStream
 import com.shakelang.shake.util.io.streaming.output.DataOutputStream
 
-class ConstantPool (
-    val entries: List<ConstantPoolEntry>
+open class ConstantPool (
+    open val entries: List<ConstantPoolEntry>
 ) {
     companion object {
         fun fromStream(stream: DataInputStream): ConstantPool {
@@ -24,6 +24,193 @@ class ConstantPool (
 
         fun fromList(list: List<ConstantPoolEntry>): ConstantPool {
             return ConstantPool(list)
+        }
+    }
+}
+
+
+class MutableConstantPool(
+    override val entries: MutableList<ConstantPoolEntry> = mutableListOf()
+) : ConstantPool(entries) {
+    fun add(entry: ConstantPoolEntry) {
+        (entries as MutableList).add(entry)
+    }
+
+    fun createUtf8(value: String): Int {
+        val entry = ConstantPoolEntry.Utf8Constant(value)
+        add(entry)
+        return entries.indexOf(entry)
+    }
+
+    fun createByte(value: Byte): Int {
+        val entry = ConstantPoolEntry.ByteConstant(value)
+        add(entry)
+        return entries.indexOf(entry)
+    }
+
+    fun createShort(value: Short): Int {
+        val entry = ConstantPoolEntry.ShortConstant(value)
+        add(entry)
+        return entries.indexOf(entry)
+    }
+
+    fun createInt(value: Int): Int {
+        val entry = ConstantPoolEntry.IntConstant(value)
+        add(entry)
+        return entries.indexOf(entry)
+    }
+
+    fun createLong(value: Long): Int {
+        val entry = ConstantPoolEntry.LongConstant(value)
+        add(entry)
+        return entries.indexOf(entry)
+    }
+
+    fun createFloat(value: Float): Int {
+        val entry = ConstantPoolEntry.FloatConstant(value)
+        add(entry)
+        return entries.indexOf(entry)
+    }
+
+    fun createDouble(value: Double): Int {
+        val entry = ConstantPoolEntry.DoubleConstant(value)
+        add(entry)
+        return entries.indexOf(entry)
+    }
+
+    fun createClass(identifier: Int): Int {
+        val entry = ConstantPoolEntry.ClassConstant(identifier)
+        add(entry)
+        return entries.indexOf(entry)
+    }
+
+    fun createClass(name: String) = createClass(createUtf8(name))
+
+    fun findUtf8(value: String): Int? {
+        for (i in entries.indices) {
+            val entry = entries[i]
+            if (entry is ConstantPoolEntry.Utf8Constant && entry.value == value) {
+                return i
+            }
+        }
+        return null
+    }
+
+    fun findByte(value: Byte): Int? {
+        for (i in entries.indices) {
+            val entry = entries[i]
+            if (entry is ConstantPoolEntry.ByteConstant && entry.value == value) {
+                return i
+            }
+        }
+        return null
+    }
+
+    fun findShort(value: Short): Int? {
+        for (i in entries.indices) {
+            val entry = entries[i]
+            if (entry is ConstantPoolEntry.ShortConstant && entry.value == value) {
+                return i
+            }
+        }
+        return null
+    }
+
+    fun findInt(value: Int): Int? {
+        for (i in entries.indices) {
+            val entry = entries[i]
+            if (entry is ConstantPoolEntry.IntConstant && entry.value == value) {
+                return i
+            }
+        }
+        return null
+    }
+
+    fun findLong(value: Long): Int? {
+        for (i in entries.indices) {
+            val entry = entries[i]
+            if (entry is ConstantPoolEntry.LongConstant && entry.value == value) {
+                return i
+            }
+        }
+        return null
+    }
+
+    fun findFloat(value: Float): Int? {
+        for (i in entries.indices) {
+            val entry = entries[i]
+            if (entry is ConstantPoolEntry.FloatConstant && entry.value == value) {
+                return i
+            }
+        }
+        return null
+    }
+
+    fun findDouble(value: Double): Int? {
+        for (i in entries.indices) {
+            val entry = entries[i]
+            if (entry is ConstantPoolEntry.DoubleConstant && entry.value == value) {
+                return i
+            }
+        }
+        return null
+    }
+
+    fun findClass(identifier: Int): Int? {
+        for (i in entries.indices) {
+            val entry = entries[i]
+            if (entry is ConstantPoolEntry.ClassConstant && entry.identifier == identifier) {
+                return i
+            }
+        }
+        return null
+    }
+
+    fun findClass(name: String): Int? {
+        val identifier = findUtf8(name) ?: return null
+        return findClass(identifier)
+    }
+
+    fun getUtf8(identifier: String) = findUtf8(identifier) ?: createUtf8(identifier)
+    fun getByte(identifier: Byte) = findByte(identifier) ?: createByte(identifier)
+    fun getShort(identifier: Short) = findShort(identifier) ?: createShort(identifier)
+    fun getInt(identifier: Int) = findInt(identifier) ?: createInt(identifier)
+    fun getLong(identifier: Long) = findLong(identifier) ?: createLong(identifier)
+    fun getFloat(identifier: Float) = findFloat(identifier) ?: createFloat(identifier)
+    fun getDouble(identifier: Double) = findDouble(identifier) ?: createDouble(identifier)
+    fun getClass(identifier: Int) = findClass(identifier) ?: createClass(identifier)
+    fun getClass(identifier: String) = findClass(identifier) ?: createClass(identifier)
+
+    fun dump(stream: DataOutputStream) {
+        stream.writeInt(entries.size)
+        for (entry in entries) {
+            entry.dump(stream)
+        }
+    }
+
+    fun dump(): ByteArray {
+        val byteStream = ByteArrayOutputStream()
+        val stream = DataOutputStream(byteStream)
+        dump(stream)
+        return byteStream.toByteArray()
+    }
+
+    companion object {
+        fun fromStream(stream: DataInputStream): MutableConstantPool {
+            val entries = mutableListOf<ConstantPoolEntry>()
+            val count = stream.readInt()
+            for (i in 0 until count) {
+                entries.add(ConstantPoolEntry.fromStream(stream))
+            }
+            return MutableConstantPool(entries)
+        }
+
+        fun fromByteArray(array: ByteArray): MutableConstantPool {
+            return fromStream(array.dataStream())
+        }
+
+        fun fromList(list: List<ConstantPoolEntry>): MutableConstantPool {
+            return MutableConstantPool(list.toMutableList())
         }
     }
 }
