44560c13a868d44933f94cde5be19bc9b85f2745
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-12-12 19:43:20 +0100

    Update Types

--- shake/bytecode/interpreter/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/Types.kt
+++ shake/bytecode/interpreter/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/Types.kt
@@ -1,30 +1,25 @@
 package com.shakelang.shake.bytecode.interpreter
 
-import com.shakelang.shake.util.io.streaming.output.DataOutputStream
-
-class ShakeInterpreterClass(
-    val methods: List<ShakeInterpreterMethod>,
+interface ShakeInterpreterClass {
+    val methods: List<ShakeInterpreterMethod>
     val fields: List<ShakeInterpreterField>
-)
-
-class ShakeInterpreterMethod(
-    val qualifiedName: String,
-    val simpleName: String,
-    val isStatic: Boolean,
-    val returnType: ShakeInterpreterType,
-    val parameters: List<ShakeInterpreterType>,
-    val code: ByteArray
-) {
-    fun dump(stream: DataOutputStream) {
-    }
 }
 
-class ShakeInterpreterField(
-    val qualifiedName: String,
-    val simpleName: String,
-    val isStatic: Boolean,
+interface ShakeInterpreterMethod {
+    val qualifiedName: String
+    val simpleName: String
+    val isStatic: Boolean
+    val returnType: ShakeInterpreterType
+    val parameters: List<ShakeInterpreterType>
+    val code: ByteArray
+}
+
+interface ShakeInterpreterField {
+    val qualifiedName: String
+    val simpleName: String
+    val isStatic: Boolean
     val type: ShakeInterpreterType
-)
+}
 
 open class ShakeInterpreterType(
     val name: String,
