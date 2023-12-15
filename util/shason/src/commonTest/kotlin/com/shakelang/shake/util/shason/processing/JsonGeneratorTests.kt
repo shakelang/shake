b84ce2fceaa766d3f4a639f3512996bfb3d5ea4b
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-12-15 11:15:47 +0100

    Update shason tests to kotest

--- util/shason/src/commonTest/kotlin/com/shakelang/shake/util/shason/processing/JsonGeneratorTests.kt
+++ util/shason/src/commonTest/kotlin/com/shakelang/shake/util/shason/processing/JsonGeneratorTests.kt
@@ -3,187 +3,131 @@ package com.shakelang.shake.util.shason.processing
 import com.shakelang.shake.util.shason.elements.JsonBooleanElement
 import com.shakelang.shake.util.shason.elements.JsonNullElement
 import com.shakelang.shake.util.shason.elements.JsonStringElement
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
-class JsonGeneratorTests {
+class JsonGeneratorTests : FreeSpec({
 
-    @Test
-    fun testGenerateByte() {
-        assertEquals("42", JsonGenerator.generate(42.toByte()))
-        assertEquals("0", JsonGenerator.generate(0.toByte()))
-        assertEquals("-1", JsonGenerator.generate((-1).toByte()))
+    "generate byte" {
+        JsonGenerator.generate(42.toByte()) shouldBe "42"
+        JsonGenerator.generate(0.toByte()) shouldBe "0"
+        JsonGenerator.generate((-1).toByte()) shouldBe "-1"
     }
 
-    @Test
-    fun testGenerateShort() {
-        assertEquals("42", JsonGenerator.generate(42.toShort()))
-        assertEquals("0", JsonGenerator.generate(0.toShort()))
-        assertEquals("-1", JsonGenerator.generate((-1).toShort()))
+    "generate short" {
+        JsonGenerator.generate(42.toShort()) shouldBe "42"
+        JsonGenerator.generate(0.toShort()) shouldBe "0"
+        JsonGenerator.generate((-1).toShort()) shouldBe "-1"
     }
 
-    @Test
-    fun testGenerateInt() {
-        assertEquals("42", JsonGenerator.generate(42))
-        assertEquals("0", JsonGenerator.generate(0))
-        assertEquals("-1", JsonGenerator.generate(-1))
+    "generate int" {
+        JsonGenerator.generate(42) shouldBe "42"
+        JsonGenerator.generate(0) shouldBe "0"
+        JsonGenerator.generate(-1) shouldBe "-1"
     }
 
-    @Test
-    fun testGenerateLong() {
-        assertEquals("42", JsonGenerator.generate(42.toLong()))
-        assertEquals("0", JsonGenerator.generate(0.toLong()))
-        assertEquals("-1", JsonGenerator.generate((-1).toLong()))
+    "generate long" {
+        JsonGenerator.generate(42L) shouldBe "42"
+        JsonGenerator.generate(0L) shouldBe "0"
+        JsonGenerator.generate(-1L) shouldBe "-1"
     }
 
-    @Test
-    fun testGenerateFloat() {
-        assertEquals("42.1", JsonGenerator.generate(42.1.toFloat()))
-        assertEquals("0.1", JsonGenerator.generate(0.1.toFloat()))
-        assertEquals("-1.1", JsonGenerator.generate((-1.1).toFloat()))
+    "generate float" {
+        JsonGenerator.generate(42.1.toFloat()) shouldBe "42.1"
+        JsonGenerator.generate(0.1.toFloat()) shouldBe "0.1"
+        JsonGenerator.generate((-1.1).toFloat()) shouldBe "-1.1"
     }
 
-    @Test
-    fun testGenerateDouble() {
-        assertEquals("42.1", JsonGenerator.generate(42.1))
-        assertEquals("0.1", JsonGenerator.generate(0.1))
-        assertEquals("-1.1", JsonGenerator.generate(-1.1))
+    "generate double" {
+        JsonGenerator.generate(42.1) shouldBe "42.1"
+        JsonGenerator.generate(0.1) shouldBe "0.1"
+        JsonGenerator.generate(-1.1) shouldBe "-1.1"
     }
 
-    @Test
-    fun testGenerateBoolean() {
-        assertEquals("true", JsonGenerator.generate(true))
-        assertEquals("false", JsonGenerator.generate(false))
+    "generate boolean" {
+        JsonGenerator.generate(true) shouldBe "true"
+        JsonGenerator.generate(false) shouldBe "false"
     }
 
-    @Test
-    fun testGenerateArrayEmpty() {
-        assertEquals("[]", JsonGenerator.generate(emptyArray<Any>()))
-        assertEquals("[]", JsonGenerator.generate(emptyArray<Any>(), indentAmount = 2))
-        assertEquals("[]", JsonGenerator.generate(emptyArray<Any>(), indentAmount = 4))
+    "generate array empty" {
+        JsonGenerator.generate(emptyArray<Any>()) shouldBe "[]"
+        JsonGenerator.generate(emptyArray<Any>(), indentAmount = 2) shouldBe "[]"
+        JsonGenerator.generate(emptyArray<Any>(), indentAmount = 4) shouldBe "[]"
     }
 
-    @Test
-    fun testGenerateArrayFilledOneElement() {
-        assertEquals("[\"hello\"]", JsonGenerator.generate(arrayOf("hello")))
-        assertEquals("[\n  \"hello\"\n]", JsonGenerator.generate(arrayOf("hello"), indent = " ".repeat(2)))
-        assertEquals("[\n    \"hello\"\n]", JsonGenerator.generate(arrayOf("hello"), indent = " ".repeat(4)))
+    "generate array filled one element" {
+        JsonGenerator.generate(arrayOf("hello")) shouldBe "[\"hello\"]"
+        JsonGenerator.generate(arrayOf("hello"), indent = " ".repeat(2)) shouldBe "[\n  \"hello\"\n]"
+        JsonGenerator.generate(arrayOf("hello"), indent = " ".repeat(4)) shouldBe "[\n    \"hello\"\n]"
     }
 
-    @Test
-    fun testGenerateArrayFilledMultipleElements() {
-        assertEquals("[\"hello\",\"world\"]", JsonGenerator.generate(arrayOf("hello", "world")))
-        assertEquals(
-            "[\n  \"hello\",\n  \"world\"\n]",
-            JsonGenerator.generate(arrayOf("hello", "world"), indent = " ".repeat(2))
-        )
-        assertEquals(
-            "[\n    \"hello\",\n    \"world\"\n]",
-            JsonGenerator.generate(arrayOf("hello", "world"), indent = " ".repeat(4))
-        )
+    "generate array filled multiple elements" {
+        JsonGenerator.generate(arrayOf("hello", "world")) shouldBe "[\"hello\",\"world\"]"
+        JsonGenerator.generate(arrayOf("hello", "world"), indent = " ".repeat(2)) shouldBe "[\n  \"hello\",\n  \"world\"\n]"
+        JsonGenerator.generate(arrayOf("hello", "world"), indent = " ".repeat(4)) shouldBe "[\n    \"hello\",\n    \"world\"\n]"
     }
 
-    @Test
-    fun testGenerateArrayNested() {
-        assertEquals("[[\"hello\"]]", JsonGenerator.generate(arrayOf(arrayOf("hello"))))
-        assertEquals(
-            "[\n  [\n    \"hello\"\n  ]\n]",
-            JsonGenerator.generate(arrayOf(arrayOf("hello")), indent = " ".repeat(2))
-        )
-        assertEquals(
-            "[\n    [\n        \"hello\"\n    ]\n]",
-            JsonGenerator.generate(arrayOf(arrayOf("hello")), indent = " ".repeat(4))
-        )
+    "generate array nested" {
+        JsonGenerator.generate(arrayOf(arrayOf("hello"))) shouldBe "[[\"hello\"]]"
+        JsonGenerator.generate(arrayOf(arrayOf("hello")), indent = " ".repeat(2)) shouldBe "[\n  [\n    \"hello\"\n  ]\n]"
+        JsonGenerator.generate(arrayOf(arrayOf("hello")), indent = " ".repeat(4)) shouldBe "[\n    [\n        \"hello\"\n    ]\n]"
     }
 
-    @Test
-    fun testGenerateListEmpty() {
-        assertEquals("[]", JsonGenerator.generate(emptyList<Any>()))
-        assertEquals("[]", JsonGenerator.generate(emptyList<Any>(), indentAmount = 2))
-        assertEquals("[]", JsonGenerator.generate(emptyList<Any>(), indentAmount = 4))
+    "generate list empty" {
+        JsonGenerator.generate(emptyList<Any>()) shouldBe "[]"
+        JsonGenerator.generate(emptyList<Any>(), indentAmount = 2) shouldBe "[]"
+        JsonGenerator.generate(emptyList<Any>(), indentAmount = 4) shouldBe "[]"
     }
 
-    @Test
-    fun testGenerateListFilledOneElement() {
-        assertEquals("[\"hello\"]", JsonGenerator.generate(listOf("hello")))
-        assertEquals("[\n  \"hello\"\n]", JsonGenerator.generate(listOf("hello"), indent = " ".repeat(2)))
-        assertEquals("[\n    \"hello\"\n]", JsonGenerator.generate(listOf("hello"), indent = " ".repeat(4)))
+    "generate list filled one element" {
+        JsonGenerator.generate(listOf("hello")) shouldBe "[\"hello\"]"
+        JsonGenerator.generate(listOf("hello"), indent = " ".repeat(2)) shouldBe "[\n  \"hello\"\n]"
+        JsonGenerator.generate(listOf("hello"), indent = " ".repeat(4)) shouldBe "[\n    \"hello\"\n]"
     }
 
-    @Test
-    fun testGenerateListFilledMultipleElements() {
-        assertEquals("[\"hello\",\"world\"]", JsonGenerator.generate(listOf("hello", "world")))
-        assertEquals(
-            "[\n  \"hello\",\n  \"world\"\n]",
-            JsonGenerator.generate(listOf("hello", "world"), indent = " ".repeat(2))
-        )
-        assertEquals(
-            "[\n    \"hello\",\n    \"world\"\n]",
-            JsonGenerator.generate(listOf("hello", "world"), indent = " ".repeat(4))
-        )
+    "generate list filled multiple elements" {
+        JsonGenerator.generate(listOf("hello", "world")) shouldBe "[\"hello\",\"world\"]"
+        JsonGenerator.generate(listOf("hello", "world"), indent = " ".repeat(2)) shouldBe "[\n  \"hello\",\n  \"world\"\n]"
+        JsonGenerator.generate(listOf("hello", "world"), indent = " ".repeat(4)) shouldBe "[\n    \"hello\",\n    \"world\"\n]"
     }
 
-    @Test
-    fun testGenerateListNested() {
-        assertEquals("[[\"hello\"]]", JsonGenerator.generate(listOf(listOf("hello"))))
-        assertEquals(
-            "[\n  [\n    \"hello\"\n  ]\n]",
-            JsonGenerator.generate(listOf(listOf("hello")), indent = " ".repeat(2))
-        )
-        assertEquals(
-            "[\n    [\n        \"hello\"\n    ]\n]",
-            JsonGenerator.generate(listOf(listOf("hello")), indent = " ".repeat(4))
-        )
+    "generate list nested" {
+        JsonGenerator.generate(listOf(listOf("hello"))) shouldBe "[[\"hello\"]]"
+        JsonGenerator.generate(listOf(listOf("hello")), indent = " ".repeat(2)) shouldBe "[\n  [\n    \"hello\"\n  ]\n]"
+        JsonGenerator.generate(listOf(listOf("hello")), indent = " ".repeat(4)) shouldBe "[\n    [\n        \"hello\"\n    ]\n]"
     }
 
-    @Test
-    fun testGenerateMapEmpty() {
-        assertEquals("{}", JsonGenerator.generate(emptyMap<String, Any>()))
-        assertEquals("{}", JsonGenerator.generate(emptyMap<String, Any>(), indentAmount = 2))
-        assertEquals("{}", JsonGenerator.generate(emptyMap<String, Any>(), indentAmount = 4))
+    "generate map empty" {
+        JsonGenerator.generate(emptyMap<String, Any>()) shouldBe "{}"
+        JsonGenerator.generate(emptyMap<String, Any>(), indentAmount = 2) shouldBe "{}"
+        JsonGenerator.generate(emptyMap<String, Any>(), indentAmount = 4) shouldBe "{}"
     }
 
-    @Test
-    fun testGenerateMapFilledOneElement() {
-        assertEquals("{\"hello\":1}", JsonGenerator.generate(mapOf("hello" to 1)))
-        assertEquals("{\n  \"hello\": 1\n}", JsonGenerator.generate(mapOf("hello" to 1), indent = " ".repeat(2)))
-        assertEquals("{\n    \"hello\": 1\n}", JsonGenerator.generate(mapOf("hello" to 1), indent = " ".repeat(4)))
+    "generate map filled one element" {
+        JsonGenerator.generate(mapOf("hello" to 1)) shouldBe "{\"hello\":1}"
+        JsonGenerator.generate(mapOf("hello" to 1), indent = " ".repeat(2)) shouldBe "{\n  \"hello\": 1\n}"
+        JsonGenerator.generate(mapOf("hello" to 1), indent = " ".repeat(4)) shouldBe "{\n    \"hello\": 1\n}"
     }
 
-    @Test
-    fun testGenerateMapFilledMultipleElements() {
-        assertEquals("{\"hello\":1,\"world\":2}", JsonGenerator.generate(mapOf("hello" to 1, "world" to 2)))
-        assertEquals(
-            "{\n  \"hello\": 1,\n  \"world\": 2\n}",
-            JsonGenerator.generate(mapOf("hello" to 1, "world" to 2), indent = " ".repeat(2))
-        )
-        assertEquals(
-            "{\n    \"hello\": 1,\n    \"world\": 2\n}",
-            JsonGenerator.generate(mapOf("hello" to 1, "world" to 2), indent = " ".repeat(4))
-        )
+    "generate map filled multiple elements" {
+        JsonGenerator.generate(mapOf("hello" to 1, "world" to 2)) shouldBe "{\"hello\":1,\"world\":2}"
+        JsonGenerator.generate(mapOf("hello" to 1, "world" to 2), indent = " ".repeat(2)) shouldBe "{\n  \"hello\": 1,\n  \"world\": 2\n}"
+        JsonGenerator.generate(mapOf("hello" to 1, "world" to 2), indent = " ".repeat(4)) shouldBe "{\n    \"hello\": 1,\n    \"world\": 2\n}"
     }
 
-    @Test
-    fun testGenerateMapNested() {
-        assertEquals("{\"hello\":{\"world\":1}}", JsonGenerator.generate(mapOf("hello" to mapOf("world" to 1))))
-        assertEquals(
-            "{\n  \"hello\": {\n    \"world\": 1\n  }\n}",
-            JsonGenerator.generate(mapOf("hello" to mapOf("world" to 1)), indent = " ".repeat(2))
-        )
-        assertEquals(
-            "{\n    \"hello\": {\n        \"world\": 1\n    }\n}",
-            JsonGenerator.generate(mapOf("hello" to mapOf("world" to 1)), indent = " ".repeat(4))
-        )
+    "generate map nested" {
+        JsonGenerator.generate(mapOf("hello" to mapOf("world" to 1))) shouldBe "{\"hello\":{\"world\":1}}"
+        JsonGenerator.generate(mapOf("hello" to mapOf("world" to 1)), indent = " ".repeat(2)) shouldBe "{\n  \"hello\": {\n    \"world\": 1\n  }\n}"
+        JsonGenerator.generate(mapOf("hello" to mapOf("world" to 1)), indent = " ".repeat(4)) shouldBe "{\n    \"hello\": {\n        \"world\": 1\n    }\n}"
     }
 
-    @Test
-    fun testGenerateJsonElement() {
-        assertEquals("\"42\"", JsonGenerator.generate(JsonStringElement("42")))
-        assertEquals("null", JsonGenerator.generate(JsonNullElement.INSTANCE))
-        assertEquals("null", JsonGenerator.generate(JsonNullElement.INSTANCE, indentAmount = 2))
-        assertEquals("null", JsonGenerator.generate(JsonNullElement.INSTANCE, indentAmount = 4))
-        assertEquals("\"42\"", JsonGenerator.generate(JsonStringElement("42"), indentAmount = 2))
-        assertEquals("true", JsonGenerator.generate(JsonBooleanElement.TRUE))
-        assertEquals("false", JsonGenerator.generate(JsonBooleanElement.FALSE))
+    "generate JsonElement" {
+        JsonGenerator.generate(JsonStringElement("42")) shouldBe "\"42\""
+        JsonGenerator.generate(JsonNullElement.INSTANCE) shouldBe "null"
+        JsonGenerator.generate(JsonNullElement.INSTANCE, indentAmount = 2) shouldBe "null"
+        JsonGenerator.generate(JsonNullElement.INSTANCE, indentAmount = 4) shouldBe "null"
+        JsonGenerator.generate(JsonStringElement("42"), indentAmount = 2) shouldBe "\"42\""
+        JsonGenerator.generate(JsonBooleanElement.TRUE) shouldBe "true"
+        JsonGenerator.generate(JsonBooleanElement.FALSE) shouldBe "false"
     }
-}
+})
--- util/shason/src/commonTest/kotlin/com/shakelang/shake/util/shason/processing/JsonParserTests.kt
+++ util/shason/src/commonTest/kotlin/com/shakelang/shake/util/shason/processing/JsonParserTests.kt
@@ -1,83 +1,142 @@
 package com.shakelang.shake.util.shason.processing
 
-import com.shakelang.shake.util.shason.elements.JsonPrimitive
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
-class JsonParserTests {
+class JsonParserTests : FreeSpec({
 
-    @Test
-    fun testParseInteger() {
+    "parseInteger" {
         val parsed = TestUtilities.parse("JsonParserTests#testParseInteger()", "42")
-        assertTrue(parsed.isJsonPrimitive(), "Integer parsing should return a JsonPrimitive")
-        assertTrue((parsed as JsonPrimitive).isInt(), "Integer parsing should return a JsonIntegerElement")
-        assertEquals(42, parsed.toJsonPrimitive().toInt().value)
+        parsed.isJsonPrimitive() shouldBe true
+        parsed.toJsonPrimitive().isInt() shouldBe true
+        parsed.toJsonPrimitive().toInt().value shouldBe 42
     }
 
-    @Test
-    fun testParseDouble() {
+    "parseDouble" {
         val parsed = TestUtilities.parse("JsonParserTests#testParseDouble()", "42.2")
-        assertTrue(parsed.isJsonPrimitive(), "Double parsing should return a JsonPrimitive")
-        assertTrue((parsed as JsonPrimitive).isDouble(), "Double parsing should return a JsonDoubleElement")
-        assertEquals(42.2, parsed.toJsonPrimitive().toDouble().value)
+        parsed.isJsonPrimitive() shouldBe true
+        parsed.toJsonPrimitive().isDouble() shouldBe true
+        parsed.toJsonPrimitive().toDouble().value shouldBe 42.2
     }
 
-    @Test
-    fun testParseTrue() {
+    "parseTrue" {
         val parsed = TestUtilities.parse("JsonParserTests#testParseTrue()", "true")
-        assertTrue(parsed.isJsonPrimitive(), "Boolean parsing should return a JsonPrimitive")
-        assertTrue((parsed as JsonPrimitive).isBoolean(), "Boolean parsing should return a JsonBooleanElement")
-        assertEquals(true, parsed.toJsonPrimitive().toBoolean().value)
+        parsed.isJsonPrimitive() shouldBe true
+        parsed.toJsonPrimitive().isBoolean() shouldBe true
+        parsed.toJsonPrimitive().toBoolean().value shouldBe true
     }
 
-    @Test
-    fun testParseFalse() {
+    "parseFalse" {
         val parsed = TestUtilities.parse("JsonParserTests#testParseTrue()", "false")
-        assertTrue(parsed.isJsonPrimitive(), "Boolean parsing should return a JsonPrimitive")
-        assertTrue((parsed as JsonPrimitive).isBoolean(), "Boolean parsing should return a JsonBooleanElement")
-        assertEquals(false, parsed.toJsonPrimitive().toBoolean().value)
+        parsed.isJsonPrimitive() shouldBe true
+        parsed.toJsonPrimitive().isBoolean() shouldBe true
+        parsed.toJsonPrimitive().toBoolean().value shouldBe false
     }
 
-    @Test
-    fun testParseString() {
+    "parseString" {
         val parsed = TestUtilities.parse("JsonParserTests#testParseString()", "\"hello\"")
-        assertTrue(parsed.isJsonPrimitive(), "String parsing should return a JsonPrimitive")
-        assertTrue((parsed as JsonPrimitive).isString(), "String parsing should return a JsonStringElement")
-        assertEquals("hello", parsed.toJsonPrimitive().toStringElement().value)
+        parsed.isJsonPrimitive() shouldBe true
+        parsed.toJsonPrimitive().isString() shouldBe true
+        parsed.toJsonPrimitive().toStringElement().value shouldBe "hello"
     }
 
-    @Test
-    fun testParseArrayEmpty() {
+    "parseArrayEmpty" {
         val parsed = TestUtilities.parse("JsonParserTests#testParseArrayEmpty()", "[]")
-        assertTrue(parsed.isJsonArray(), "Array parsing should return a JsonArray")
-        assertEquals(0, parsed.toJsonArray().size, "Parsing of empty array should return an empty JsonArray")
+        parsed.isJsonArray() shouldBe true
+        parsed.toJsonArray().size shouldBe 0
     }
 
-    @Test
-    fun testParseArrayFilled() {
+    "parseArrayFilled" {
         val parsed = TestUtilities.parse("JsonParserTests#testParseArrayFilled()", "[1, 2]")
-        assertTrue(parsed.isJsonArray(), "Array parsing should return a JsonArray")
-        assertEquals(2, parsed.toJsonArray().size, "Parsing of empty array should return an empty JsonArray")
-        assertEquals(1, parsed.toJsonArray()[0].toJsonPrimitive().toInt().value)
-        assertEquals(2, parsed.toJsonArray()[1].toJsonPrimitive().toInt().value)
+        parsed.isJsonArray() shouldBe true
+        parsed.toJsonArray().size shouldBe 2
+        parsed.toJsonArray()[0].toJsonPrimitive().toInt().value shouldBe 1
+        parsed.toJsonArray()[1].toJsonPrimitive().toInt().value shouldBe 2
     }
 
-    @Test
-    fun testParseObjectEmpty() {
+    "parseObjectEmpty" {
         val parsed = TestUtilities.parse("JsonParserTests#testParseObjectEmpty()", "{}")
-        assertTrue(parsed.isJsonObject(), "Object parsing should return a JsonObject")
-        assertEquals(0, parsed.toJsonObject().size, "Parsing of empty object should return an empty JsonObject")
+        parsed.isJsonObject() shouldBe true
+        parsed.toJsonObject().size shouldBe 0
     }
 
-    @Test
-    fun testParseObjectFilled() {
+    "parseObjectFilled" {
         val parsed = TestUtilities.parse("JsonParserTests#testParseObjectFilled()", "{\"one\": 1, \"two\": 2}")
-        assertTrue(parsed.isJsonObject(), "Object parsing should return a JsonObject")
-        assertEquals(2, parsed.toJsonObject().size)
-        assertTrue(parsed.toJsonObject().contains("one"), "Should contain key one")
-        assertTrue(parsed.toJsonObject().contains("two"), "Should contain key two")
-        assertEquals(1, parsed.toJsonObject()["one"]!!.toJsonPrimitive().toInt().value)
-        assertEquals(2, parsed.toJsonObject()["two"]!!.toJsonPrimitive().toInt().value)
+        parsed.isJsonObject() shouldBe true
+        parsed.toJsonObject().size shouldBe 2
+        parsed.toJsonObject().contains("one") shouldBe true
+        parsed.toJsonObject().contains("two") shouldBe true
+        parsed.toJsonObject()["one"]!!.toJsonPrimitive().toInt().value shouldBe 1
+        parsed.toJsonObject()["two"]!!.toJsonPrimitive().toInt().value shouldBe 2
     }
-}
+
+    "parseObjectFilledWithSpaces" {
+        val parsed = TestUtilities.parse("JsonParserTests#testParseObjectFilled()", "{ \"one\" : 1, \"two\" : 2 }")
+        parsed.isJsonObject() shouldBe true
+        parsed.toJsonObject().size shouldBe 2
+        parsed.toJsonObject().contains("one") shouldBe true
+        parsed.toJsonObject().contains("two") shouldBe true
+        parsed.toJsonObject()["one"]!!.toJsonPrimitive().toInt().value shouldBe 1
+        parsed.toJsonObject()["two"]!!.toJsonPrimitive().toInt().value shouldBe 2
+    }
+
+    "parseObjectFilledWithNewLines" {
+        val parsed = TestUtilities.parse("JsonParserTests#testParseObjectFilled()", "{\n\"one\": 1,\n\"two\": 2\n}")
+        parsed.isJsonObject() shouldBe true
+        parsed.toJsonObject().size shouldBe 2
+        parsed.toJsonObject().contains("one") shouldBe true
+        parsed.toJsonObject().contains("two") shouldBe true
+        parsed.toJsonObject()["one"]!!.toJsonPrimitive().toInt().value shouldBe 1
+        parsed.toJsonObject()["two"]!!.toJsonPrimitive().toInt().value shouldBe 2
+    }
+
+    "parseObjectFilledWithTabs" {
+        val parsed = TestUtilities.parse("JsonParserTests#testParseObjectFilled()", "{\t\"one\": 1,\t\"two\": 2\t}")
+        parsed.isJsonObject() shouldBe true
+        parsed.toJsonObject().size shouldBe 2
+        parsed.toJsonObject().contains("one") shouldBe true
+        parsed.toJsonObject().contains("two") shouldBe true
+        parsed.toJsonObject()["one"]!!.toJsonPrimitive().toInt().value shouldBe 1
+        parsed.toJsonObject()["two"]!!.toJsonPrimitive().toInt().value shouldBe 2
+    }
+
+    "parseObjectFilledWithMixed" {
+        val parsed = TestUtilities.parse("JsonParserTests#testParseObjectFilled()", "{\t\"one\": 1,\n\"two\": 2\t}")
+        parsed.isJsonObject() shouldBe true
+        parsed.toJsonObject().size shouldBe 2
+        parsed.toJsonObject().contains("one") shouldBe true
+        parsed.toJsonObject().contains("two") shouldBe true
+        parsed.toJsonObject()["one"]!!.toJsonPrimitive().toInt().value shouldBe 1
+        parsed.toJsonObject()["two"]!!.toJsonPrimitive().toInt().value shouldBe 2
+    }
+
+    "parseObjectFilledWithMixedAndSpaces" {
+        val parsed = TestUtilities.parse("JsonParserTests#testParseObjectFilled()", "{\t\"one\" : 1,\n\"two\" : 2\t}")
+        parsed.isJsonObject() shouldBe true
+        parsed.toJsonObject().size shouldBe 2
+        parsed.toJsonObject().contains("one") shouldBe true
+        parsed.toJsonObject().contains("two") shouldBe true
+        parsed.toJsonObject()["one"]!!.toJsonPrimitive().toInt().value shouldBe 1
+        parsed.toJsonObject()["two"]!!.toJsonPrimitive().toInt().value shouldBe 2
+    }
+
+    "parseObjectFilledWithMixedAndNewLines" {
+        val parsed = TestUtilities.parse("JsonParserTests#testParseObjectFilled()", "{\t\"one\": 1,\n\"two\": 2\t}")
+        parsed.isJsonObject() shouldBe true
+        parsed.toJsonObject().size shouldBe 2
+        parsed.toJsonObject().contains("one") shouldBe true
+        parsed.toJsonObject().contains("two") shouldBe true
+        parsed.toJsonObject()["one"]!!.toJsonPrimitive().toInt().value shouldBe 1
+        parsed.toJsonObject()["two"]!!.toJsonPrimitive().toInt().value shouldBe 2
+    }
+
+    "parseObjectFilledWithMixedAndTabs" {
+        val parsed = TestUtilities.parse("JsonParserTests#testParseObjectFilled()", "{\t\"one\": 1,\t\"two\": 2\n}")
+        parsed.isJsonObject() shouldBe true
+        parsed.toJsonObject().size shouldBe 2
+        parsed.toJsonObject().contains("one") shouldBe true
+        parsed.toJsonObject().contains("two") shouldBe true
+        parsed.toJsonObject()["one"]!!.toJsonPrimitive().toInt().value shouldBe 1
+        parsed.toJsonObject()["two"]!!.toJsonPrimitive().toInt().value shouldBe 2
+    }
+})
