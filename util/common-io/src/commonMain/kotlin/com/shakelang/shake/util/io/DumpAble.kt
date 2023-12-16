2a01a3d53d5284a3498cd3f4a385e2e86f46211e
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-12-16 14:21:32 +0100

    Introduce common-io/DumpAble

---
+++ util/common-io/src/commonMain/kotlin/com/shakelang/shake/util/io/DumpAble.kt
@@ -0,0 +1,38 @@
+package com.shakelang.shake.util.io
+
+import com.shakelang.shake.util.io.streaming.output.ByteArrayOutputStream
+import com.shakelang.shake.util.io.streaming.output.DataOutputStream
+import com.shakelang.shake.util.io.streaming.output.OutputStream
+
+/**
+ * An interface for objects that can be dumped to a OutputStreams
+ */
+interface DumpAble {
+
+    /**
+     * Dump the object to a [DataOutputStream]
+     *
+     * @param stream The [DataOutputStream] to dump the object to
+     */
+    fun dump(stream: DataOutputStream)
+
+    /**
+     * Dump the object to an [OutputStream]
+     * @param stream The [OutputStream] to dump the object to
+     */
+    fun dump(stream: OutputStream) {
+        dump(DataOutputStream(stream))
+    }
+
+    /**
+     * Dump the object to an [ByteArray]
+     *
+     * @return The [ByteArray] with the dumped object
+     */
+    fun dump(): ByteArray {
+        val stream = ByteArrayOutputStream()
+        dump(stream)
+        return stream.toByteArray()
+    }
+
+}
