cfd207cba257ae3151e12ee8dce0792fb5e54d70
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-12-20 15:57:46 +0100

    Introduce types for unsigned primitives

--- shake/bytecode/interpreter/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/wrapper/ShakeInterpreterType.kt
+++ shake/bytecode/interpreter/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/wrapper/ShakeInterpreterType.kt
@@ -36,25 +36,33 @@ open class ShakeInterpreterType(
 
     companion object {
 
-        val INT = ShakeInterpreterType("int", Type.INT)
-        val FLOAT = ShakeInterpreterType("float", Type.FLOAT)
-        val DOUBLE = ShakeInterpreterType("double", Type.DOUBLE)
-        val LONG = ShakeInterpreterType("long", Type.LONG)
-        val SHORT = ShakeInterpreterType("short", Type.SHORT)
-        val BYTE = ShakeInterpreterType("byte", Type.BYTE)
-        val CHAR = ShakeInterpreterType("char", Type.CHAR)
-        val BOOLEAN = ShakeInterpreterType("boolean", Type.BOOLEAN)
-        val VOID = ShakeInterpreterType("void", Type.VOID)
+        val BYTE = ShakeInterpreterType("B", Type.BYTE)
+        val SHORT = ShakeInterpreterType("S", Type.SHORT)
+        val INT = ShakeInterpreterType("I", Type.INT)
+        val LONG = ShakeInterpreterType("J", Type.LONG)
+        val UNSIGNED_BYTE = ShakeInterpreterType("b", Type.BYTE)
+        val UNSIGNED_SHORT = ShakeInterpreterType("s", Type.SHORT)
+        val UNSIGNED_INT = ShakeInterpreterType("i", Type.INT)
+        val UNSIGNED_LONG = ShakeInterpreterType("j", Type.LONG)
+        val FLOAT = ShakeInterpreterType("F", Type.FLOAT)
+        val DOUBLE = ShakeInterpreterType("D", Type.DOUBLE)
+        val CHAR = ShakeInterpreterType("C", Type.CHAR)
+        val BOOLEAN = ShakeInterpreterType("Z", Type.BOOLEAN)
+        val VOID = ShakeInterpreterType("V", Type.VOID)
 
         fun of(storage: TypeDescriptor, classpath: ShakeClasspath): ShakeInterpreterType {
             return when (storage) {
                 is TypeDescriptor.ByteType -> BYTE
-                is TypeDescriptor.CharType -> CHAR
-                is TypeDescriptor.DoubleType -> DOUBLE
-                is TypeDescriptor.FloatType -> FLOAT
+                is TypeDescriptor.ShortType -> SHORT
                 is TypeDescriptor.IntType -> INT
                 is TypeDescriptor.LongType -> LONG
-                is TypeDescriptor.ShortType -> SHORT
+                is TypeDescriptor.UnsignedByteType -> UNSIGNED_BYTE
+                is TypeDescriptor.UnsignedShortType -> UNSIGNED_SHORT
+                is TypeDescriptor.UnsignedIntType -> UNSIGNED_INT
+                is TypeDescriptor.UnsignedLongType -> UNSIGNED_LONG
+                is TypeDescriptor.FloatType -> FLOAT
+                is TypeDescriptor.DoubleType -> DOUBLE
+                is TypeDescriptor.CharType -> CHAR
                 is TypeDescriptor.BooleanType -> BOOLEAN
                 is TypeDescriptor.VoidType -> VOID
                 is TypeDescriptor.ArrayType -> ArrayType(of(storage.type, classpath))
