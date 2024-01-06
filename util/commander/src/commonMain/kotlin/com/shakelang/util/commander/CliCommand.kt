e67f65781dfab5e77ca44d0bcd00bace026b3770
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2024-01-06 20:13:51 +0100

    Refactor commander package

--- util/commander/src/commonMain/kotlin/com/shakelang/util/commander/CliCommand.kt
+++ util/commander/src/commonMain/kotlin/com/shakelang/util/commander/CliCommand.kt
@@ -1,71 +1,5 @@
 package com.shakelang.util.commander
 
-abstract class ValueException(message: String) : RuntimeException(message)
-
-/**
- * Error thrown when an argument is not valid
- */
-class ValueValidationException(message: String) : ValueException(message)
-
-/**
- * Error thrown when a required argument is missing
- */
-class MissingArgumentException(message: String) : ValueException(message)
-
-/**
- * Error thrown when unknown arguments are passed
- */
-class UnknownArgumentException(message: String) : ValueException(message)
-
-class CliOption(
-    val command: CliCommand,
-    val name: String,
-    val aliases: Array<String> = arrayOf(),
-    val shortAlias: Array<String> = arrayOf(),
-    val description: String? = null,
-    val required: Boolean = false,
-    val hasValue: Boolean = false,
-    val defaultValue: String? = null,
-    val valueName: String? = null,
-    val valueDescription: String? = null,
-    val valueValidator: ValueValidator? = null,
-) {
-    fun getUsage(): String {
-        val builder = StringBuilder()
-        builder.append("--$name")
-        if (shortAlias.isNotEmpty() || aliases.isNotEmpty()) {
-            val aliases = arrayOf(
-                *shortAlias.map { "-$it" }.toTypedArray(),
-                *aliases.map { "--$it" }.toTypedArray()
-            )
-            builder.append(" (", aliases.joinToString(", "), ")")
-        }
-        if (valueName != null) builder.append(" <$valueName>")
-        return builder.toString()
-    }
-}
-
-interface CliCommandAppendable
-
-class CliArgument(
-    val command: CliCommand,
-    val name: String,
-    val description: String? = null,
-    val required: Boolean = false,
-    val hasValue: Boolean = false,
-    val defaultValue: String? = null,
-    val valueName: String? = null,
-    val valueDescription: String? = null,
-    val valueValidator: ValueValidator? = null,
-) {
-    fun getUsage(): String {
-        val builder = StringBuilder()
-        builder.append("<$name>")
-        if (valueName != null) builder.append(" <$valueName>")
-        return builder.toString()
-    }
-}
-
 class CliCommand(
     val parent: CliCommand? = null,
     val name: String,
@@ -203,7 +137,8 @@ class CliCommand(
         action: ((CliCommand, Array<String>) -> Unit)? = null,
         init: (CliCommand.() -> Unit)? = null,
     ): CliCommand {
-        val command = CliCommand(this, name, aliases, description, mutableListOf(), mutableListOf(), mutableListOf(), action)
+        val command =
+            CliCommand(this, name, aliases, description, mutableListOf(), mutableListOf(), mutableListOf(), action)
         commands.add(command)
         if (init != null) command.init()
         return this
@@ -285,193 +220,7 @@ class CliCommand(
 
 }
 
-class ParseResult(
-    val stack : MutableList<CommandStackEntry>
-) {
-    fun getOptionValueByName(alias: String): Array<Value>? {
-        for (entry in stack) {
-            val value = entry.options[alias]
-            if (value != null) return value
-        }
-        return null
-    }
-
-    fun getArgumentValueByName(alias: String): Value? {
-        for (entry in stack) {
-            val value = entry.arguments[alias]
-            if (value != null) return value
-        }
-        return null
-    }
-
-    fun getValueByName(alias: String): List<Value>? {
-        for (entry in stack) {
-            if(entry.options.containsKey(alias)) return entry.options[alias]?.toList()
-            if(entry.arguments.containsKey(alias)) return listOf(entry.arguments[alias]!!)
-        }
-        return null
-    }
-}
-
-class CommandStackEntry(
-    val alias: String,
-    val command: CliCommand,
-) {
-    val name: String = command.name
-    val arguments: MutableMap<String, Value> = mutableMapOf()
-    val options: MutableMap<String, Array<Value>> = mutableMapOf()
-}
-
-
-typealias ValueTransformer<T> = (String) -> T
-typealias NullableValueTransformer<T> = (String?) -> T
-typealias ValueValidator = (String?) -> Unit
-
-fun ValueValidator.accepts(value: String?): Boolean {
-    return try {
-        this(value)
-        true
-    } catch (e: ValueException) {
-        false
-    }
-}
-
-
-
-data class Value(
-    val name: String,
-    val value: String?,
-) {
-    fun <T> to(valueTransformer: ValueTransformer<T>) = valueTransformer(value ?: throw NullPointerException("Value is null"))
-    fun toByte(): Byte = value?.toByte() ?: throw NumberFormatException("Value is null")
-    fun toShort(): Short = value?.toShort() ?: throw NumberFormatException("Value is null")
-    fun toInt(): Int = value?.toInt() ?: throw NumberFormatException("Value is null")
-    fun toLong(): Long = value ?.toLong() ?: throw NumberFormatException("Value is null")
-    fun toFloat(): Float = value?.toFloat() ?: throw NumberFormatException("Value is null")
-    fun toDouble(): Double = value?.toDouble() ?: throw NumberFormatException("Value is null")
-    fun toBoolean(): Boolean = value?.toBoolean() ?: throw NumberFormatException("Value is null")
-    override fun toString(): String = value ?: throw NullPointerException("Value is null")
-
-    fun <T> toOrNull(valueTransformer: NullableValueTransformer<T>) = valueTransformer(value)
-    fun toByteOrNull(): Byte? = value?.toByteOrNull()
-    fun toShortOrNull(): Short? = value?.toShortOrNull()
-    fun toIntOrNull(): Int? = value?.toIntOrNull()
-    fun toLongOrNull(): Long? = value?.toLongOrNull()
-    fun toFloatOrNull(): Float? = value?.toFloatOrNull()
-    fun toDoubleOrNull(): Double? = value?.toDoubleOrNull()
-    fun toBooleanOrNull(): Boolean? = value?.toBoolean()
-    fun toStringOrNull(): String? = value
-
-    fun validate(valueValidator: ValueValidator) = valueValidator(value)
-    fun validate(valueValidator: NullableValueTransformer<Any>) = valueValidator(value) != null
-    fun validateByte(): Boolean = value?.toByteOrNull() != null
-    fun validateShort(): Boolean = value?.toShortOrNull() != null
-    fun validateInt(): Boolean = value?.toIntOrNull() != null
-    fun validateLong(): Boolean = value?.toLongOrNull() != null
-    fun validateFloat(): Boolean = value?.toFloatOrNull() != null
-    fun validateDouble(): Boolean = value?.toDoubleOrNull() != null
-    fun validateBoolean(): Boolean = value?.toBoolean() != null
-    fun validateString(): Boolean = value != null
-
-    fun isNull(): Boolean = value == null
-    fun isNotNull(): Boolean = value != null
-    fun isByte(): Boolean = value?.toByteOrNull() != null
-    fun isShort(): Boolean = value?.toShortOrNull() != null
-    fun isInt(): Boolean = value?.toIntOrNull() != null
-    fun isLong(): Boolean = value?.toLongOrNull() != null
-    fun isFloat(): Boolean = value?.toFloatOrNull() != null
-    fun isDouble(): Boolean = value?.toDoubleOrNull() != null
-    fun isBoolean(): Boolean = value?.toBoolean() != null
-    fun isString(): Boolean = value != null
-}
-
-class OptionCreationContext {
-
-    lateinit var name: String
-    val aliases: MutableList<String> = mutableListOf()
-    val shortAliases: MutableList<String> = mutableListOf()
-    var description: String? = null
-    var required: Boolean = false
-    var hasValue: Boolean = false
-    var defaultValue: String? = null
-    var valueName: String? = null
-    var valueDescription: String? = null
-    var valueValidator: ValueValidator? = null
-
-    fun alias(vararg aliases: String) {
-        this.aliases.addAll(aliases)
-    }
-
-    fun shortAlias(vararg aliases: String) {
-        this.shortAliases.addAll(aliases)
-    }
-
-    fun description(description: String) {
-        this.description = (this.description?.plus("\n") ?: "") + description
-    }
-
-    fun required(required: Boolean = true) {
-        this.required = required
-    }
-
-    fun generate(command: CliCommand): CliOption {
-
-        if(!::name.isInitialized) throw IllegalStateException("Name is not initialized")
-
-        return CliOption(
-            command,
-            name,
-            aliases.toTypedArray(),
-            shortAliases.toTypedArray(),
-            description,
-            required,
-            hasValue,
-            defaultValue,
-            valueName,
-            valueDescription,
-            valueValidator
-        )
-    }
-}
-
-class ArgumentCreationContext {
-
-    lateinit var name: String
-    var description: String? = null
-    var required: Boolean = false
-    var hasValue: Boolean = false
-    var defaultValue: String? = null
-    var valueName: String? = null
-    var valueDescription: String? = null
-    var valueValidator: ValueValidator? = null
-
-    fun description(description: String) {
-        this.description = (this.description?.plus("\n") ?: "") + description
-    }
-
-    fun required(required: Boolean = true) {
-        this.required = required
-    }
-
-    fun generate(command: CliCommand): CliArgument {
-
-        if(!::name.isInitialized) throw IllegalStateException("Name is not initialized")
-
-        return CliArgument(
-            command,
-            name,
-            description,
-            required,
-            hasValue,
-            defaultValue,
-            valueName,
-            valueDescription,
-            valueValidator
-        )
-    }
-}
-
-class CommandCreationContext {
+class CliCommandCreationContext {
 
     lateinit var name: String
     val aliases: MutableList<String> = mutableListOf()
@@ -497,8 +246,8 @@ class CommandCreationContext {
         arguments.add { cliArgument }
     }
 
-    fun argument(init: ArgumentCreationContext.() -> Unit) {
-        val context = ArgumentCreationContext()
+    fun argument(init: CliArgumentCreationContext.() -> Unit) {
+        val context = CliArgumentCreationContext()
         context.init()
         arguments.add { context.generate(it) }
     }
@@ -532,8 +281,8 @@ class CommandCreationContext {
         options.add { cliOption }
     }
 
-    fun option(init: OptionCreationContext.() -> Unit) {
-        val context = OptionCreationContext()
+    fun option(init: CliOptionCreationContext.() -> Unit) {
+        val context = CliOptionCreationContext()
         context.init()
         options.add { context.generate(it) }
     }
@@ -571,8 +320,8 @@ class CommandCreationContext {
         commands.add { cliCommand }
     }
 
-    fun command(init: CommandCreationContext.() -> Unit) {
-        val context = CommandCreationContext()
+    fun command(init: CliCommandCreationContext.() -> Unit) {
+        val context = CliCommandCreationContext()
         context.init()
         commands.add { context.generate(it) }
     }
@@ -582,9 +331,9 @@ class CommandCreationContext {
         aliases: Array<String> = arrayOf(),
         description: String? = null,
         action: ((CliCommand, Array<String>) -> Unit)? = null,
-        init: (CommandCreationContext.() -> Unit)? = null,
+        init: (CliCommandCreationContext.() -> Unit)? = null,
     ) {
-        val context = CommandCreationContext()
+        val context = CliCommandCreationContext()
         context.name = name
         context.aliases.addAll(aliases)
         context.description = description
