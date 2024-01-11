d50dfb812ef4fc79ed539971b07927f53d6d263d
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2024-01-11 18:56:58 +0100

    Update PathDescriptor kdoc

--- shake/bytecode/conventions/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/format/descriptor/PathDescriptor.kt
+++ shake/bytecode/conventions/src/commonMain/kotlin/com/shakelang/shake/bytecode/interpreter/format/descriptor/PathDescriptor.kt
@@ -29,18 +29,55 @@ package com.shakelang.shake.bytecode.interpreter.format.descriptor
  *
  * The method `shake.lang.String.Builder::build` would be represented as `shake/lang/String:Builder:build`.
  *
+ * See [specification](https://spec.shakelang.com/bytecode/storage-format#path-signature)
+ *
  * @param packageEntities The package entities
  * @param classEntities The class entities
  * @param entity The entity
  * @constructor Creates a new [PathDescriptor]
- * @see [the docs](https://specification.shakelang.com/bytecode/storage-format#path-signature) for more information
+ *
+ * @since 0.1.0
+ * @version 0.1.0
  */
 class PathDescriptor(
+
+    /**
+     * The package entities
+     * @return The package entities
+     * @see packagePath
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     val packageEntities: Array<String>,
+
+    /**
+     * The class entities
+     * @return The class entities
+     * @see classPath
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     val classEntities: Array<String>,
+
+    /**
+     * The entity
+     * @return The entity
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     val entity: String,
 ) {
 
+    /**
+     * String representation of the [PathDescriptor]
+     *
+     * [Specification](https://spec.shakelang.com/bytecode/storage-format#path-signature)
+     *
+     * @return The string representation of the [PathDescriptor]
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     */
     val descriptor
         get() = buildString {
             packageEntities.forEach { append("$it/") }
@@ -48,16 +85,46 @@ class PathDescriptor(
             append(entity)
         }
 
+    /**
+     * The package path
+     * @return The package path
+     * @since 0.1.0
+     * @version 0.1.0
+     * @see packageEntities
+     */
     val packagePath get() = packageEntities.joinToString("/")
+
+    /**
+     * The class path
+     * @return The class path
+     * @since 0.1.0
+     * @version 0.1.0
+     * @see classEntities
+     */
     val classPath get() = classEntities.joinToString(":")
+
+    /**
+     * The full path
+     * @return The full path
+     * @since 0.1.0
+     * @version 0.1.0
+     * @see packagePath
+     * @see classPath
+     */
+    val path get() = "$packagePath/$classPath"
 
     companion object {
 
         /**
          * Parse a [PathDescriptor] from a [String]
          *
+         * See [specification](https://spec.shakelang.com/bytecode/storage-format#path-signature)
+         *
          * @param descriptor The descriptor to parse
          * @return The parsed [PathDescriptor]
+         *
+         * @since 0.1.0
+         * @version 0.1.0
          */
         fun parse(descriptor: String): PathDescriptor {
             val packageEntities = mutableListOf<String>()
