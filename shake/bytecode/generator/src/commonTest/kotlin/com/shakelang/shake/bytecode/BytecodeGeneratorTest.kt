fe673390948f24e5a0f58d3c490dc18ad46b8457
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2024-01-13 18:42:16 +0100

    Add a few more tests for method generation

--- shake/bytecode/generator/src/commonTest/kotlin/com/shakelang/shake/bytecode/BytecodeGeneratorTest.kt
+++ shake/bytecode/generator/src/commonTest/kotlin/com/shakelang/shake/bytecode/BytecodeGeneratorTest.kt
@@ -11,7 +11,7 @@ import io.kotest.matchers.shouldBe
 class BytecodeGeneratorTest : FreeSpec(
     {
 
-        "execute a method" {
+        "call native with params" {
             val processor = createBaseProcessor()
 
             processor.loadSynthetic(
@@ -48,10 +48,150 @@ class BytecodeGeneratorTest : FreeSpec(
 
             val output = stdout.toByteArray().joinToString { it.toInt().toChar().toString() }
             output shouldBe "3"
-            ticks shouldBe 6
         }
 
         "call a method" {
+            val processor = createBaseProcessor()
+
+            processor.loadSynthetic(
+                "test.shake",
+                """
+                package test
+                
+                void main() {
+                    test();
+                }
+                
+                void test() {
+                    print(1)
+                }
+                
+                """.trimIndent(),
+            )
+            processor.finish()
+
+            // println(json.stringify(processor.project.toJson()))
+
+            val generator = ShakeBytecodeGenerator()
+            val out = generator.generateProject(processor.project)
+
+            val stdout = ByteArrayOutputStream()
+
+            // Execute test code
+            val classPath = ShakeClasspath.create(out)
+
+            // Get test package
+            val testPackage = classPath.packages.find { it.name == "test" }!!.storages[0]
+            println(BytecodeStringGenerator(testPackage).generate(testPackage).joinToString("\n"))
+
+            val interpreter = ShakeInterpreter(classPath)
+            interpreter.process.setOut(stdout)
+
+            interpreter.putFunctionOnStack(
+                "test/main()V",
+                byteArrayOf(),
+            )
+            val ticks = interpreter.run()
+
+            val output = stdout.toByteArray().joinToString("") { it.toInt().toChar().toString() }
+            output shouldBe "1"
+        }
+
+        "call a method (with return value)" {
+            val processor = createBaseProcessor()
+
+            processor.loadSynthetic(
+                "test.shake",
+                """
+                package test
+                
+                void main() {
+                    print(test())
+                }
+                
+                int test() {
+                    return 1
+                }
+                
+                """.trimIndent(),
+            )
+            processor.finish()
+
+            // println(json.stringify(processor.project.toJson()))
+
+            val generator = ShakeBytecodeGenerator()
+            val out = generator.generateProject(processor.project)
+
+            val stdout = ByteArrayOutputStream()
+
+            // Execute test code
+            val classPath = ShakeClasspath.create(out)
+
+            // Get test package
+            val testPackage = classPath.packages.find { it.name == "test" }!!.storages[0]
+            println(BytecodeStringGenerator(testPackage).generate(testPackage).joinToString("\n"))
+
+            val interpreter = ShakeInterpreter(classPath)
+            interpreter.process.setOut(stdout)
+
+            interpreter.putFunctionOnStack(
+                "test/main()V",
+                byteArrayOf(),
+            )
+            val ticks = interpreter.run()
+
+            val output = stdout.toByteArray().joinToString("") { it.toInt().toChar().toString() }
+            output shouldBe "1"
+        }
+
+        "call a method (with params)" {
+            val processor = createBaseProcessor()
+
+            processor.loadSynthetic(
+                "test.shake",
+                """
+                package test
+                
+                void main() {
+                    test(3)
+                }
+                
+                void test(int i) {
+                    print(i)
+                }
+                
+                """.trimIndent(),
+            )
+            processor.finish()
+
+            // println(json.stringify(processor.project.toJson()))
+
+            val generator = ShakeBytecodeGenerator()
+            val out = generator.generateProject(processor.project)
+
+            val stdout = ByteArrayOutputStream()
+
+            // Execute test code
+            val classPath = ShakeClasspath.create(out)
+
+            // Get test package
+            val testPackage = classPath.packages.find { it.name == "test" }!!.storages[0]
+            println(BytecodeStringGenerator(testPackage).generate(testPackage).joinToString("\n"))
+
+            val interpreter = ShakeInterpreter(classPath)
+            interpreter.process.setOut(stdout)
+
+            interpreter.putFunctionOnStack(
+                "test/main()V",
+                byteArrayOf(),
+            )
+            val ticks = interpreter.run()
+
+            val output = stdout.toByteArray().joinToString("") { it.toInt().toChar().toString() }
+            output shouldBe "3"
+        }
+
+        "call a method (with parameters and return value)" {
             val processor = createBaseProcessor()
 
             processor.loadSynthetic(
@@ -96,7 +236,6 @@ class BytecodeGeneratorTest : FreeSpec(
 
             val output = stdout.toByteArray().joinToString("") { it.toInt().toChar().toString() }
             output shouldBe "3"
-            ticks shouldBe 13
         }
     },
 )
