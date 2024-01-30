2b628ccf492fa76f0be99ca5290697f1f40b54b1
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-01-30 18:19:25 +0100

    Add embed plugin to shakelib

--- shake/compiler/shakelib/build.gradle.kts
+++ shake/compiler/shakelib/build.gradle.kts
@@ -1,5 +1,6 @@
-// import com.shakelang.util.changelog.resolveVersion
-// import com.shakelang.util.embed.plugin.embedConfiguration
+import com.shakelang.util.changelog.resolveVersion
+import com.shakelang.util.embed.plugin.Embed
+import com.shakelang.util.embed.plugin.embedConfiguration
 import conventions.dependencies
 import conventions.projectGroup
 import dev.icerock.gradle.MRVisibility
@@ -17,10 +18,12 @@ repositories {
 }
 
 group = projectGroup("shake.compiler")
-// version = resolveVersion()
+version = resolveVersion()
 description = "Utilities for parsing stuff with kotlin"
 
 val projectName = name
+
+apply<Embed>()
 
 multiplatformResources {
     multiplatformResourcesPackage = "com.shakelang.shake.shakelib"
@@ -30,21 +33,38 @@ multiplatformResources {
     multiplatformResourcesVisibility = MRVisibility.Public
     disableStaticFrameworkWarning = true
 }
-//
-// embedConfiguration {
-//    distPackage.set("com.shakelang.shake.shakelib")
-//    distName.set("ShakeLib")
-//    baseDir.set(projectDir.path + "/src/commonMain/resources")
-//    embed("**/*.shake")
-// }
+
+embedConfiguration {
+    distPackage.set("com.shakelang.shake.shakelib")
+    distName.set("ShakeLib")
+    baseDir.set(projectDir.path + "/src/commonMain/resources")
+    embed("**/*.shake")
+}
 
 kotlin {
+    // Create source-set for generated resources
+    val generatedMain = sourceSets.create("generatedMain") {
+        kotlin.srcDir("$buildDir/generated/embed")
+        this@create.dependencies {
+            this@dependencies.implementation("com.shakelang.util.embed:api:0.1.0")
+        }
+    }
+
+    val commonMain by sourceSets.getting
+    commonMain.dependsOn(generatedMain)
+
     dependencies {
         implementation("dev.icerock.moko:resources:0.23.0")
+        implementation("com.shakelang.util.embed:api:0.1.0")
         implementation("dev.icerock.moko:resources-compose:0.23.0") // for compose multiplatform
         testImplementation("dev.icerock.moko:resources-test:0.23.0")
     }
 }
+
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
+    dependsOn("fileBuilder")
+}
+
 listOf(
     "compileKotlinJs",
     "compileKotlinJvm",
