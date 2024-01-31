947bf3fbccefac8e38b599ca221ee4fbef94d5bb
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-01-31 14:08:03 +0100

    Release embed.api@0.1.1 & embed.plugin@0.2.0

--- shake/compiler/shakelib/build.gradle.kts
+++ shake/compiler/shakelib/build.gradle.kts
@@ -1,6 +1,5 @@
 import com.shakelang.util.changelog.resolveVersion
 import com.shakelang.util.embed.plugin.Embed
-import com.shakelang.util.embed.plugin.embedConfiguration
 import conventions.projectGroup
 
 plugins {
@@ -21,10 +20,10 @@ description = "Utilities for parsing stuff with kotlin"
 val projectName = name
 
 apply<Embed>()
-
-embedConfiguration {
-    sourceSet.set("commonMain")
-    distPackage.set("com.shakelang.shake.shakelib")
-    distName.set("ShakeLib")
-    embed("**/*.shake")
-}
+//
+// embedConfiguration {
+//    sourceSet.set("commonMain")
+//    distPackage.set("com.shakelang.shake.shakelib")
+//    distName.set("ShakeLib")
+//    embed("**/*.shake")
+// }
