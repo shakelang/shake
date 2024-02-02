f7210e8cba32f69854b469c30180055a3aa826cb
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-02-02 15:55:44 +0100

    Add more parser tests

--- shake/compiler/parser/build.gradle.kts
+++ shake/compiler/parser/build.gradle.kts
@@ -51,3 +51,65 @@ getEmbedExtension(project).configuration {
     embed("**/*.json")
 }
 
+fun String.forFileName() = this.replace(" ", "_").replace(".", "_").replace(",", "_").replace("(", "_").replace(")", "_")
+inner class Template(val name: String) {
+    val shakeFile = project.file("test-templates/$name.shake")
+    val jsonFile = project.file("test-templates/$name.json")
+
+    val shake = shakeFile.readText()
+    val json = jsonFile.readText()
+}
+
+tasks.register("generateTests") {
+    description = "Generates the test files for the parser"
+
+    doLast {
+        run {
+            // Class Tests
+            val classTestDirectory = project.file("src/commonTest/resources/tests/classes")
+            classTestDirectory.mkdirs()
+
+            run {
+                // Class Fields
+                val classFieldsDirectory = classTestDirectory.resolve("fields")
+                classFieldsDirectory.mkdirs()
+
+                val types = listOf(
+                    "byte" to "byte",
+                    "short" to "short",
+                    "int" to "integer",
+                    "long" to "long",
+                    "float" to "float",
+                    "double" to "double",
+                    "char" to "char",
+                    "boolean" to "boolean",
+                    "unsigned byte" to "unsigned_byte",
+                    "unsigned short" to "unsigned_short",
+                    "unsigned int" to "unsigned_integer",
+                    "unsigned long" to "unsigned_long",
+                )
+
+                val template = Template("class-field")
+
+                types.forEach { type ->
+                    val file = classFieldsDirectory.resolve("class_field_${type.second.forFileName()}.shake")
+                    file.writeText(
+                        template.shake
+                            .replace("%type%", type.first)
+                            .replace("%type_name%", type.second)
+                            .replace("%name%", "field")
+
+                    )
+
+                    val jsonFile = classFieldsDirectory.resolve("class_field_${type.second.forFileName()}.json")
+                    jsonFile.writeText(
+                        template.json
+                            .replace("%type%", type.first)
+                            .replace("%type_name%", type.second)
+                            .replace("%name%", "field")
+                    )
+                }
+            }
+        }
+    }
+}
