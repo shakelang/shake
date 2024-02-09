3ae83b1b67292bde986bbfd19cee805837dfb06f
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-02-09 18:45:18 +0100

    Migrate class test generator to kotlin

--- shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/parser/api/Test.kt
+++ shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/parser/api/Test.kt
@@ -121,7 +121,7 @@ class Tests : FreeSpec(
                         combineAttributes().forEachIndexed { i, attributeInfo ->
 
                             val templates = listOf(
-                                template("methods/parameter-method") to "method",
+                                template("methods/parameter2-method") to "method",
                             )
 
 
@@ -151,6 +151,77 @@ class Tests : FreeSpec(
                                 }
                             }
                         }
+                    }
+                }
+            }
+
+
+            provider {
+
+                name = "classes"
+
+                primitiveTypes.forEach { (type, typeName) ->
+                    combineAttributes().forEachIndexed { i, attributeInfo ->
+
+                        val templates = listOf(
+//                            template("classes/class") to "class",
+                            template("classes/class-field") to "class_field",
+                            template("classes/initialized-class-field") to "class_field_initialized",
+//                            template("classes/inner-class") to "inner_class",
+                        )
+
+
+                        templates.forEach {
+
+                            val template = it.first
+
+                            it.first.apply(
+                                replaceTemplate(
+                                    "%access%" to attributeInfo.accessModifier,
+                                    "%static%" to if (attributeInfo.isStatic) "true" else "false",
+                                    "%final%" to if (attributeInfo.isFinal) "true" else "false",
+                                    "%attributes%" to attributeInfo.attributeString,
+                                    "%type%" to type,
+                                    "%type_name%" to typeName,
+                                    "%name%" to "test",
+                                ),
+                            )
+
+                            test("${typeName}_${it.second}$i", isIgnored = false) {
+                                this.input = template.code
+                                this.expectedJson = template.json
+                            }
+                        }
+                    }
+                }
+
+                combineAttributes().forEachIndexed { i, attributeInfo ->
+
+                    val templates = listOf(
+                        template("classes/class") to "class",
+                        template("classes/inner-class") to "inner_class",
+                    )
+
+
+                    templates.forEach {
+
+                        val template = it.first
+
+                        it.first.apply(
+                            replaceTemplate(
+                                "%access%" to attributeInfo.accessModifier,
+                                "%static%" to if (attributeInfo.isStatic) "true" else "false",
+                                "%final%" to if (attributeInfo.isFinal) "true" else "false",
+                                "%attributes%" to attributeInfo.attributeString,
+                                "%name%" to "test",
+                            ),
+                        )
+
+                        test("${it.second}$i", isIgnored = false) {
+                            this.input = template.code
+                            this.expectedJson = template.json
+                        }
+
                     }
                 }
             }
