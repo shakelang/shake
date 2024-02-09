b76e68894732ef4a758ec102e408332d6d411ef3
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-02-09 15:26:52 +0100

    Fix typing of FlatStream

--- util/common-io/src/commonMain/kotlin/com/shakelang/util/io/streaming/Stream.kt
+++ util/common-io/src/commonMain/kotlin/com/shakelang/util/io/streaming/Stream.kt
@@ -359,6 +359,44 @@ class IndexedMapStream<T, R>(
 }
 
 /**
+ * A stream that flattens a stream of streams
+ * @param T The type of the elements in the stream
+ * @property stream The stream of streams to flatten
+ * @constructor Create a new flat stream
+ * @since 0.5.0
+ */
+class FlatStream<T>(
+
+    private val stream: Stream<out Stream<T>>,
+
+) : Stream<T> {
+
+    private var currentStream: Stream<T>? = null
+
+    /**
+     * Read the next element from the stream
+     * @return The next element
+     * @since 0.5.0
+     */
+    override fun read(): T {
+        if (currentStream == null || !currentStream!!.hasNext()) {
+            if (!stream.hasNext()) throw IllegalStateException("Stream is empty")
+            currentStream = stream.read()
+        }
+        return currentStream!!.read()
+    }
+
+    /**
+     * Check if the stream has a next element
+     * @return If the stream has a next element
+     * @since 0.5.0
+     */
+    override fun hasNext(): Boolean {
+        return currentStream?.hasNext() ?: stream.hasNext()
+    }
+}
+
+/**
  * Create a Stream from a [Sequence]
  * @receiver The sequence to create the stream from
  * @return The created stream
@@ -397,3 +435,5 @@ fun <T> Iterator<T>.stream() = IteratorStream(this)
  * @since 0.5.0
  */
 fun <T> streamOf(vararg elements: T) = elements.iterator().stream()
+
+fun <T> Stream<out Stream<T>>.flatten() = FlatStream(this)
