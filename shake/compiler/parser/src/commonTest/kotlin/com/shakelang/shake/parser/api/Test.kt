94938c18c5a1b808baf436bec53773d41c2a3e0a
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-02-09 18:37:11 +0100

    Migrate test generator to kotlin

--- shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/parser/api/Test.kt
+++ shake/compiler/parser/src/commonTest/kotlin/com/shakelang/shake/parser/api/Test.kt
@@ -6,7 +6,7 @@ class Tests : FreeSpec(
     {
 
         generateTests {
-            this.provider {
+            provider {
 
                 name = "fields"
 
@@ -38,6 +38,117 @@ class Tests : FreeSpec(
                             test("${typeName}_${it.second}$i", isIgnored = false) {
                                 this.input = template.code
                                 this.expectedJson = template.json
+                            }
+                        }
+                    }
+                }
+            }
+
+            this.provider {
+
+                name = "methods"
+
+                primitiveTypes.forEach { (type, typeName) ->
+                    combineAttributes().forEachIndexed { i, attributeInfo ->
+
+                        val templates = listOf(
+                            template("methods/method") to "method",
+                        )
+
+
+                        templates.forEach {
+
+                            val template = it.first
+
+                            it.first.apply(
+                                com.shakelang.shake.parser.api.replaceTemplate(
+                                    "%access%" to attributeInfo.accessModifier,
+                                    "%static%" to if (attributeInfo.isStatic) "true" else "false",
+                                    "%final%" to if (attributeInfo.isFinal) "true" else "false",
+                                    "%attributes%" to attributeInfo.attributeString,
+                                    "%type%" to type,
+                                    "%type_name%" to typeName,
+                                    "%name%" to "m",
+                                ),
+                            )
+
+                            test("0/${typeName}_${it.second}$i", isIgnored = false) {
+                                this.input = template.code
+                                this.expectedJson = template.json
+                            }
+                        }
+                    }
+                }
+
+                primitiveTypes.forEach { (type, typeName) ->
+                    primitiveTypes.forEach { (type0, typeName0) ->
+                        combineAttributes().forEachIndexed { i, attributeInfo ->
+
+                            val templates = listOf(
+                                template("methods/parameter-method") to "method",
+                            )
+
+
+                            templates.forEach {
+
+                                val template = it.first
+
+                                it.first.apply(
+                                    com.shakelang.shake.parser.api.replaceTemplate(
+                                        "%access%" to attributeInfo.accessModifier,
+                                        "%static%" to if (attributeInfo.isStatic) "true" else "false",
+                                        "%final%" to if (attributeInfo.isFinal) "true" else "false",
+                                        "%attributes%" to attributeInfo.attributeString,
+                                        "%type%" to type,
+                                        "%type_name%" to typeName,
+                                        "%name%" to "m",
+                                        "%type0_name%" to typeName0,
+                                        "%type0%" to type0,
+                                    ),
+                                )
+
+                                test("1/${typeName}/${typeName0}/${it.second}$i", isIgnored = false) {
+                                    this.input = template.code
+                                    this.expectedJson = template.json
+                                }
+                            }
+                        }
+                    }
+                }
+
+                primitiveTypes.forEach { (type0, typeName0) ->
+                    primitiveTypes.forEach { (type1, typeName1) ->
+                        combineAttributes().forEachIndexed { i, attributeInfo ->
+
+                            val templates = listOf(
+                                template("methods/parameter-method") to "method",
+                            )
+
+
+                            templates.forEach {
+
+                                val template = it.first
+
+                                it.first.apply(
+                                    com.shakelang.shake.parser.api.replaceTemplate(
+                                        "%access%" to attributeInfo.accessModifier,
+                                        "%static%" to if (attributeInfo.isStatic) "true" else "false",
+                                        "%final%" to if (attributeInfo.isFinal) "true" else "false",
+                                        "%attributes%" to attributeInfo.attributeString,
+                                        "%type%" to "void",
+                                        "%type_name%" to "void",
+                                        "%name%" to "m",
+                                        "%type0_name%" to typeName0,
+                                        "%type0%" to type0,
+                                        "%type1_name%" to typeName1,
+                                        "%type1%" to type1,
+                                    ),
+                                )
+
+                                test("2/${typeName0}/${typeName1}/${it.second}$i", isIgnored = false) {
+                                    this.input = template.code
+                                    this.expectedJson = template.json
+                                }
                             }
                         }
                     }
