0821db4703f3878f5ba3feaa977ad372797c2c1e
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-02-10 14:36:32 +0100

    Add kdoc to processor

--- shake/compiler/processor/src/commonMain/kotlin/com/shakelang/shake/processor/program/types/ShakeDeclaration.kt
+++ shake/compiler/processor/src/commonMain/kotlin/com/shakelang/shake/processor/program/types/ShakeDeclaration.kt
@@ -1,10 +1,36 @@
 package com.shakelang.shake.processor.program.types
 
+/**
+ * Represents a declaration in the Shake language, such as a variable, a function, or a class.
+ * A declaration is a named entity that can be assigned a value or type.
+ *
+ * @since 0.1.0
+ */
 interface ShakeDeclaration : ShakeAssignable {
+
+    /**
+     * The name of the declaration.
+     */
     val name: String
+
+    /**
+     * A unique name for the declaration, used internally for identification.
+     */
     val uniqueName: String
+
+    /**
+     * The fully qualified name of the declaration, including its scope and namespace.
+     */
     val qualifiedName: String
 
+    // The use function is commented out. If it's part of the interface, uncomment and document it.
     // fun use(scope: ShakeScope): ShakeUsage
+
+    /**
+     * Converts the declaration to a JSON representation.
+     * Useful for serialization or debugging purposes.
+     *
+     * @return A map containing the JSON representation of the declaration.
+     */
     fun toJson(): Map<String, Any?>
 }
--- shake/compiler/processor/src/commonMain/kotlin/com/shakelang/shake/processor/program/types/ShakeParameter.kt
+++ shake/compiler/processor/src/commonMain/kotlin/com/shakelang/shake/processor/program/types/ShakeParameter.kt
@@ -1,11 +1,37 @@
 package com.shakelang.shake.processor.program.types
 
+/**
+ * Represents a parameter in the Shake language.
+ * Parameters are used in functions or constructors and can be assigned values.
+ *
+ * @since 0.1.0
+ */
 interface ShakeParameter : ShakeAssignable {
+
+    /**
+     * The name of the parameter.
+     */
     val name: String
 
-    // TODO uniqueName
-    val uniqueName: String get() = "param_$name"
+    /**
+     * The unique name of the parameter, typically used internally.
+     * By default, it is prefixed with "param_" followed by the parameter's name.
+     */
+    // TODO uniqueName is not necessarily unique
+    val uniqueName: String
+        get() = "param_$name"
+
+    /**
+     * The type of the parameter.
+     */
     override val type: ShakeType
+
+    /**
+     * Converts the parameter to a JSON representation.
+     * Useful for serialization or debugging purposes.
+     *
+     * @return A map representing the JSON structure of this parameter.
+     */
     fun toJson(): Map<String, Any?> = mapOf(
         "name" to name,
         "type" to type.toJson(),
