f3ed56f7c5612bcc151f447399c3da4728a89004
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-03-07 11:29:41 +0100

    Introduce AbstractParser

--- util/shason/src/commonMain/kotlin/com/shakelang/util/shason/processing/JsonParser.kt
+++ util/shason/src/commonMain/kotlin/com/shakelang/util/shason/processing/JsonParser.kt
@@ -1,9 +1,7 @@
 package com.shakelang.util.shason.processing
 
-import com.shakelang.util.parseutils.CompilerError
-import com.shakelang.util.parseutils.ParserErrorFactory
 import com.shakelang.util.parseutils.characters.Characters
-import com.shakelang.util.parseutils.characters.position.Position
+import com.shakelang.util.parseutils.parser.AbstractParser
 import com.shakelang.util.shason.elements.*
 import com.shakelang.util.shason.processing.JsonTokenType.*
 
@@ -15,18 +13,13 @@ import com.shakelang.util.shason.processing.JsonTokenType.*
 @Suppress("unused")
 class JsonParser(
 
-    /**
-     * The [JsonToken]s to parse ([JsonTokenInputStream])
-     * @since 0.1.0
-     * @version 0.1.0
-     */
-    val tokens: JsonTokenInputStream,
+    tokens: JsonTokenInputStream,
 
-) {
+) : AbstractParser<JsonTokenInputStream, JsonElement>(tokens) {
 
-    fun parse(): JsonElement {
+    override fun parse(): JsonElement {
         val ret = parseValue()
-        if (tokens.hasNext()) throw errorFactory.createErrorAtCurrent("Input not finished")
+        if (input.hasNext()) throw errorFactory.createErrorAtCurrent("Input not finished")
         return ret
     }
 
@@ -36,7 +29,7 @@ class JsonParser(
      * @version 0.1.0
      */
     private fun parseValue(): JsonElement {
-        val next = tokens.next()
+        val next = input.next()
         return when (next.type) {
             LCURL -> parseMap()
             LSQUARE -> parseArray()
@@ -60,16 +53,16 @@ class JsonParser(
         val map = mutableJsonObjectOf()
         var next = true
 
-        while (tokens.hasNext() && next) {
-            val key = if (tokens.peek().type == STRING) Characters.parseString(tokens.next().value) else break
-            if (tokens.next().type != COLON) throw errorFactory.createErrorAtCurrent("Expecting ':'")
+        while (input.hasNext() && next) {
+            val key = if (input.peek().type == STRING) Characters.parseString(input.next().value) else break
+            if (input.next().type != COLON) throw errorFactory.createErrorAtCurrent("Expecting ':'")
             map[key] = parseValue()
 
-            next = tokens.peek().type == COMMA
-            if (next) tokens.skip()
+            next = input.peek().type == COMMA
+            if (next) input.skip()
         }
 
-        if (tokens.next().type != RCURL) throw errorFactory.createErrorAtCurrent("Expecting '}'")
+        if (input.next().type != RCURL) throw errorFactory.createErrorAtCurrent("Expecting '}'")
 
         return map
     }
@@ -83,30 +76,14 @@ class JsonParser(
         val arr = mutableJsonArrayOf()
         var next = true
 
-        while (tokens.hasNext() && next) {
-            if (tokens.peek().type == RSQUARE) break
+        while (input.hasNext() && next) {
+            if (input.peek().type == RSQUARE) break
             arr.add(parseValue())
-            next = tokens.peek().type == COMMA
-            if (next) tokens.skip()
+            next = input.peek().type == COMMA
+            if (next) input.skip()
         }
 
-        if (tokens.next().type != RSQUARE) throw errorFactory.createErrorAtCurrent("Expecting ']")
+        if (input.next().type != RSQUARE) throw errorFactory.createErrorAtCurrent("Expecting ']")
         return arr
     }
-
-    // ****************************************************************************
-    // Errors
-
-    private inner class JsonParserErrorFactory : ParserErrorFactory<ParserError>(
-        { message, start, end -> ParserError(message, start, end) },
-        tokens,
-    )
-    private val errorFactory = JsonParserErrorFactory()
-
-    /**
-     * A [CompilerError] thrown by the [JsonParser]
-     * @since 0.1.0
-     * @version 0.3.0
-     */
-    private class ParserError(message: String, start: Position, end: Position) : CompilerError(message, start, end)
 }
