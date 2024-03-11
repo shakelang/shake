439ed5a71e135ed98d16e4d5ec59dfba06b279b9
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-03-11 13:39:08 +0100

    Update plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,13 +12,13 @@ repositories {
 dependencies {
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.5")
+    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.6")
     implementation("com.github.node-gradle:gradle-node-plugin:7.0.2")
     implementation("gradle.plugin.org.gradle.crypto:checksum:1.4.0")
     implementation("gradle.plugin.com.hierynomus.gradle.plugins:license-gradle-plugin:0.16.1")
     implementation("com.shakelang.util.changelog:plugin:0.7.5")
     implementation("com.shakelang.util:sarifmerge:0.2.3")
-    implementation("com.shakelang.util.embed:plugin:0.2.2")
+    implementation("com.shakelang.util.embed:plugin:0.2.3")
     implementation("io.codearte.gradle.nexus:gradle-nexus-staging-plugin:0.30.0")
     implementation("io.kotest:kotest-framework-multiplatform-plugin-gradle:5.8.0")
 }
