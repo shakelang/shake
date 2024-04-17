e11bcc2f97f4015767177c8b9764753532ec5040
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-04-17 19:43:09 +0200

    Migrate shakespeare tests to new TestSpec

--- shake/shakespeare/src/commonTest/kotlin/com/shakelang/shake/shakespeare/spec/ClassSpecTests.kt
+++ shake/shakespeare/src/commonTest/kotlin/com/shakelang/shake/shakespeare/spec/ClassSpecTests.kt
@@ -2,68 +2,107 @@ package com.shakelang.shake.shakespeare.spec
 
 import com.shakelang.shake.shakespeare.spec.code.CodeSpec
 import com.shakelang.util.testlib.FlatTestSpec
+import com.shakelang.util.testlib.describe
 import io.kotest.matchers.shouldBe
 
 class ConstructorSpecTests : FlatTestSpec(
     {
 
-        it("should create a constructor") {
-            val parameters = listOf<ParameterSpec>()
-            val body = CodeSpec.empty()
-            val constructor = ConstructorSpec(parameters, body)
-            constructor.parameters shouldBe parameters
-            constructor.body shouldBe body
+        describe("create") {
+            it("should create a constructor") {
+                val parameters = listOf<ParameterSpec>()
+                val body = CodeSpec.empty()
+                val constructor = ConstructorSpec(parameters, body)
+                constructor.parameters shouldBe parameters
+                constructor.body shouldBe body
+            }
+
+            it("should create a named constructor") {
+                val identifier = NamespaceSpec("name")
+                val parameters = listOf<ParameterSpec>()
+                val body = CodeSpec.empty()
+                val constructor = ConstructorSpec(parameters, body, identifier)
+                constructor.name shouldBe identifier
+                constructor.parameters shouldBe parameters
+                constructor.body shouldBe body
+            }
         }
 
-        it("should create a named constructor") {
-            val identifier = NamespaceSpec("name")
-            val parameters = listOf<ParameterSpec>()
-            val body = CodeSpec.empty()
-            val constructor = ConstructorSpec(parameters, body, identifier)
-            constructor.name shouldBe identifier
-            constructor.parameters shouldBe parameters
-            constructor.body shouldBe body
+        describe("generate") {
+
+            it("should generate a constructor") {
+                val parameters = listOf<ParameterSpec>()
+                val body = CodeSpec.empty()
+                val constructor = ConstructorSpec(parameters, body)
+                constructor.generate(GenerationContext()) shouldBe "public constructor() {}"
+            }
+
+            it("should generate a named constructor") {
+                val identifier = NamespaceSpec("name")
+                val parameters = listOf<ParameterSpec>()
+                val body = CodeSpec.empty()
+                val constructor = ConstructorSpec(parameters, body, identifier)
+                constructor.generate(GenerationContext()) shouldBe "public constructor name() {}"
+            }
         }
 
-        it("should generate a constructor") {
-            val parameters = listOf<ParameterSpec>()
-            val body = CodeSpec.empty()
-            val constructor = ConstructorSpec(parameters, body)
-            constructor.generate(GenerationContext()) shouldBe "public constructor() {}"
-        }
+        describe("builder") {
+            it("should create a constructor") {
+                val parameters = listOf<ParameterSpec>()
+                val body = CodeSpec.empty()
+                val constructor = ConstructorSpec.builder()
+                    .parameters(parameters)
+                    .body(body)
+                    .build()
+                constructor.parameters shouldBe parameters
+                constructor.body shouldBe body
+                constructor.name shouldBe null
+            }
 
-        it("should generate a named constructor") {
-            val identifier = NamespaceSpec("name")
-            val parameters = listOf<ParameterSpec>()
-            val body = CodeSpec.empty()
-            val constructor = ConstructorSpec(parameters, body, identifier)
-            constructor.generate(GenerationContext()) shouldBe "public constructor name() {}"
-        }
-
-        it("builder should create a constructor") {
-            val parameters = listOf<ParameterSpec>()
-            val body = CodeSpec.empty()
-            val constructor = ConstructorSpec.builder()
-                .parameters(parameters)
-                .body(body)
-                .build()
-            constructor.parameters shouldBe parameters
-            constructor.body shouldBe body
-            constructor.name shouldBe null
-        }
-
-        it("builder should create a named constructor") {
-            val identifier = NamespaceSpec("name")
-            val parameters = listOf<ParameterSpec>()
-            val body = CodeSpec.empty()
-            val constructor = ConstructorSpec.builder()
-                .name(identifier)
-                .parameters(parameters)
-                .body(body)
-                .build()
-            constructor.name shouldBe identifier
-            constructor.parameters shouldBe parameters
-            constructor.body shouldBe body
+            it("should create a named constructor") {
+                val identifier = NamespaceSpec("name")
+                val parameters = listOf<ParameterSpec>()
+                val body = CodeSpec.empty()
+                val constructor = ConstructorSpec.builder()
+                    .name(identifier)
+                    .parameters(parameters)
+                    .body(body)
+                    .build()
+                constructor.name shouldBe identifier
+                constructor.parameters shouldBe parameters
+                constructor.body shouldBe body
+            }
         }
     },
 )
+
+class ClassSpecTests : FlatTestSpec({
+
+    describe("create") {
+
+        val it = ClassSpec(
+            "ClassName",
+            listOf(),
+            listOf(),
+            listOf(),
+            listOf(),
+            isAbstract = false,
+            isFinal = false,
+            isStatic = false,
+            isNative = false,
+            AccessModifier.PUBLIC,
+        )
+
+        it.name shouldBe "ClassName"
+        it.methods shouldBe listOf()
+        it.fields shouldBe listOf()
+        it.constructors shouldBe listOf()
+        it.classes shouldBe listOf()
+
+        it.isAbstract shouldBe false
+        it.isFinal shouldBe false
+        it.isStatic shouldBe false
+        it.isNative shouldBe false
+        it.accessModifier shouldBe AccessModifier.PUBLIC
+    }
+})
