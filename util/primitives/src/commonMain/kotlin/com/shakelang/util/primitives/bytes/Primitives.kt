c856dc07daebcaa0a569c84fdda4b22c88e0f7f9
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-05-08 17:40:20 +0200

    Test primitives api

--- util/primitives/src/commonMain/kotlin/com/shakelang/util/primitives/bytes/Primitives.kt
+++ util/primitives/src/commonMain/kotlin/com/shakelang/util/primitives/bytes/Primitives.kt
@@ -59,14 +59,14 @@ fun Int.toBytes(): ByteArray = toBytesBE()
  * @return The ByteArray
  */
 fun Long.toBytesLE() = byteArrayOf(
-    (this shr 56).toByte(),
-    (this shr 48).toByte(),
-    (this shr 40).toByte(),
-    (this shr 32).toByte(),
-    (this shr 24).toByte(),
-    (this shr 16).toByte(),
-    (this shr 8).toByte(),
     this.toByte(),
+    (this shr 8).toByte(),
+    (this shr 16).toByte(),
+    (this shr 24).toByte(),
+    (this shr 32).toByte(),
+    (this shr 40).toByte(),
+    (this shr 48).toByte(),
+    (this shr 56).toByte(),
 )
 
 /**
--- util/primitives/src/commonTest/kotlin/com/shakelang/util/primitives/bytes/PrimitiveTests.kt
+++ util/primitives/src/commonTest/kotlin/com/shakelang/util/primitives/bytes/PrimitiveTests.kt
@@ -1,182 +1,389 @@
 package com.shakelang.util.primitives.bytes
 
-import io.kotest.core.spec.style.FreeSpec
+import com.shakelang.util.testlib.FlatTestSpec
 import io.kotest.matchers.shouldBe
 
 @Suppress("unused")
-class PrimitiveTests : FreeSpec(
-    {
+class PrimitiveTests : FlatTestSpec({
 
-        "byte to bytes" {
-            val b = 0xFFu.toByte()
-            val bytes = b.toBytes()
-            bytes.size shouldBe 1
-            bytes[0] shouldBe b
+    describe("to bytes") {
+
+        describe("byte") {
+
+            it("byte to bytes") {
+                val b = 0xFFu.toByte()
+                val bytes = b.toBytes()
+                bytes.size shouldBe 1
+                bytes[0] shouldBe b
+            }
         }
 
-        "shorts to bytes" {
-            val s = 0xFFFEu.toShort()
-            val bytes = s.toBytes()
-            bytes.size shouldBe 2
-            bytes[0] shouldBe 0xFFu.toByte()
-            bytes[1] shouldBe 0xFEu.toByte()
+        describe("short") {
+
+            it("toBytesBE()") {
+                val s = 0xFFFEu.toShort()
+                val bytes = s.toBytesBE()
+                bytes.size shouldBe 2
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xFEu.toByte()
+            }
+
+            it("toBytesLE()") {
+                val s = 0xFFFEu.toShort()
+                val bytes = s.toBytesLE()
+                bytes.size shouldBe 2
+                bytes[0] shouldBe 0xFEu.toByte()
+                bytes[1] shouldBe 0xFFu.toByte()
+            }
+
+            it("toBytes()") {
+                val s = 0xFFFEu.toShort()
+                val bytes = s.toBytes()
+                bytes.size shouldBe 2
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xFEu.toByte()
+            }
         }
 
-        "int to bytes" {
-            val i = 0xFFFEFDFCu
-            val bytes = i.toBytes()
-            bytes.size shouldBe 4
-            bytes[0] shouldBe 0xFFu.toByte()
-            bytes[1] shouldBe 0xFEu.toByte()
-            bytes[2] shouldBe 0xFDu.toByte()
-            bytes[3] shouldBe 0xFCu.toByte()
+        describe("int") {
+
+            it("toBytesBE()") {
+                val i = 0xFFFEFDFCu.toInt()
+                val bytes = i.toBytesBE()
+                bytes.size shouldBe 4
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xFEu.toByte()
+                bytes[2] shouldBe 0xFDu.toByte()
+                bytes[3] shouldBe 0xFCu.toByte()
+            }
+
+            it("toBytesLE()") {
+                val i = 0xFFFEFDFCu.toInt()
+                val bytes = i.toBytesLE()
+                bytes.size shouldBe 4
+                bytes[0] shouldBe 0xFCu.toByte()
+                bytes[1] shouldBe 0xFDu.toByte()
+                bytes[2] shouldBe 0xFEu.toByte()
+                bytes[3] shouldBe 0xFFu.toByte()
+            }
+
+            it("toBytes()") {
+                val i = 0xFFFEFDFCu.toInt()
+                val bytes = i.toBytes()
+                bytes.size shouldBe 4
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xFEu.toByte()
+                bytes[2] shouldBe 0xFDu.toByte()
+                bytes[3] shouldBe 0xFCu.toByte()
+            }
         }
 
-        "long to bytes" {
-            val l = 0x0FFEFDFCFBF9F8F7L
-            val bytes = l.toBytes()
-            bytes.size shouldBe 8
-            bytes[0] shouldBe 0x0Fu.toByte()
-            bytes[1] shouldBe 0xFEu.toByte()
-            bytes[2] shouldBe 0xFDu.toByte()
-            bytes[3] shouldBe 0xFCu.toByte()
-            bytes[4] shouldBe 0xFBu.toByte()
-            bytes[5] shouldBe 0xF9u.toByte()
-            bytes[6] shouldBe 0xF8u.toByte()
-            bytes[7] shouldBe 0xF7u.toByte()
+        describe("long") {
+
+            it("toBytesBE()") {
+                val l = 0xFFFEFDFCFBFAF9F8uL.toLong()
+                val bytes = l.toBytesBE()
+                bytes.size shouldBe 8
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xFEu.toByte()
+                bytes[2] shouldBe 0xFDu.toByte()
+                bytes[3] shouldBe 0xFCu.toByte()
+                bytes[4] shouldBe 0xFBu.toByte()
+                bytes[5] shouldBe 0xFAu.toByte()
+                bytes[6] shouldBe 0xF9u.toByte()
+                bytes[7] shouldBe 0xF8u.toByte()
+            }
+
+            it("toBytesLE()") {
+                val l = 0xFFFEFDFCFBFAF9F8uL.toLong()
+                val bytes = l.toBytesLE()
+                bytes.size shouldBe 8
+                bytes[0] shouldBe 0xF8u.toByte()
+                bytes[1] shouldBe 0xF9u.toByte()
+                bytes[2] shouldBe 0xFAu.toByte()
+                bytes[3] shouldBe 0xFBu.toByte()
+                bytes[4] shouldBe 0xFCu.toByte()
+                bytes[5] shouldBe 0xFDu.toByte()
+                bytes[6] shouldBe 0xFEu.toByte()
+                bytes[7] shouldBe 0xFFu.toByte()
+            }
+
+            it("toBytes()") {
+                val l = 0xFFFEFDFCFBFAF9F8uL.toLong()
+                val bytes = l.toBytes()
+                bytes.size shouldBe 8
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xFEu.toByte()
+                bytes[2] shouldBe 0xFDu.toByte()
+                bytes[3] shouldBe 0xFCu.toByte()
+                bytes[4] shouldBe 0xFBu.toByte()
+                bytes[5] shouldBe 0xFAu.toByte()
+                bytes[6] shouldBe 0xF9u.toByte()
+                bytes[7] shouldBe 0xF8u.toByte()
+            }
         }
 
-        "float to bytes" {
-            val f = Float.fromBits(0x3f333333)
-            val bytes = f.toBytes()
-            bytes.size shouldBe 4
-            bytes[0] shouldBe 0x3fu.toByte()
-            bytes[1] shouldBe 0x33u.toByte()
-            bytes[2] shouldBe 0x33u.toByte()
-            bytes[3] shouldBe 0x33u.toByte()
+        describe("float") {
+
+            it("toBytesBE()") {
+                val f = Float.fromBits(0x3f800000)
+                val bytes = f.toBytesBE()
+                bytes.size shouldBe 4
+                bytes[0] shouldBe 0x3fu.toByte()
+                bytes[1] shouldBe 0x80u.toByte()
+                bytes[2] shouldBe 0x00u.toByte()
+                bytes[3] shouldBe 0x00u.toByte()
+            }
+
+            it("toBytesLE()") {
+                val f = Float.fromBits(0x3f800000)
+                val bytes = f.toBytesLE()
+                bytes.size shouldBe 4
+                bytes[0] shouldBe 0x00u.toByte()
+                bytes[1] shouldBe 0x00u.toByte()
+                bytes[2] shouldBe 0x80u.toByte()
+                bytes[3] shouldBe 0x3fu.toByte()
+            }
+
+            it("toBytes()") {
+                val f = Float.fromBits(0x3f800000)
+                val bytes = f.toBytes()
+                bytes.size shouldBe 4
+                bytes[0] shouldBe 0x3fu.toByte()
+                bytes[1] shouldBe 0x80u.toByte()
+                bytes[2] shouldBe 0x00u.toByte()
+                bytes[3] shouldBe 0x00u.toByte()
+            }
         }
 
-        "double to bytes" {
-            val d = Double.fromBits(0x3ff3333333333333)
-            val bytes = d.toBytes()
-            bytes.size shouldBe 8
-            bytes[0] shouldBe 0x3fu.toByte()
-            bytes[1] shouldBe 0xf3u.toByte()
-            bytes[2] shouldBe 0x33u.toByte()
-            bytes[3] shouldBe 0x33u.toByte()
-            bytes[4] shouldBe 0x33u.toByte()
-            bytes[5] shouldBe 0x33u.toByte()
-            bytes[6] shouldBe 0x33u.toByte()
-            bytes[7] shouldBe 0x33u.toByte()
+        describe("double") {
+
+            it("toBytesBE()") {
+                val d = Double.fromBits(0x3ff0000000000000)
+                val bytes = d.toBytesBE()
+                bytes.size shouldBe 8
+                bytes[0] shouldBe 0x3fu.toByte()
+                bytes[1] shouldBe 0xf0u.toByte()
+                bytes[2] shouldBe 0x00u.toByte()
+                bytes[3] shouldBe 0x00u.toByte()
+                bytes[4] shouldBe 0x00u.toByte()
+                bytes[5] shouldBe 0x00u.toByte()
+                bytes[6] shouldBe 0x00u.toByte()
+                bytes[7] shouldBe 0x00u.toByte()
+            }
+
+            it("toBytesLE()") {
+                val d = Double.fromBits(0x3ff0000000000000)
+                val bytes = d.toBytesLE()
+                bytes.size shouldBe 8
+                bytes[0] shouldBe 0x00u.toByte()
+                bytes[1] shouldBe 0x00u.toByte()
+                bytes[2] shouldBe 0x00u.toByte()
+                bytes[3] shouldBe 0x00u.toByte()
+                bytes[4] shouldBe 0x00u.toByte()
+                bytes[5] shouldBe 0x00u.toByte()
+                bytes[6] shouldBe 0xf0u.toByte()
+                bytes[7] shouldBe 0x3fu.toByte()
+            }
+
+            it("toBytes()") {
+                val d = Double.fromBits(0x3ff0000000000000)
+                val bytes = d.toBytes()
+                bytes.size shouldBe 8
+                bytes[0] shouldBe 0x3fu.toByte()
+                bytes[1] shouldBe 0xf0u.toByte()
+                bytes[2] shouldBe 0x00u.toByte()
+                bytes[3] shouldBe 0x00u.toByte()
+                bytes[4] shouldBe 0x00u.toByte()
+                bytes[5] shouldBe 0x00u.toByte()
+                bytes[6] shouldBe 0x00u.toByte()
+                bytes[7] shouldBe 0x00u.toByte()
+            }
         }
 
-        "ubyte to bytes" {
-            val b: UByte = 0xFFu
-            val bytes = b.toBytes()
-            bytes.size shouldBe 1
-            bytes[0] shouldBe 0xFFu.toByte()
+        describe("ubyte") {
+
+            it("ubyte to bytes") {
+                val b: UByte = 0xFFu
+                val bytes = b.toBytes()
+                bytes.size shouldBe 1
+                bytes[0] shouldBe b.toByte()
+            }
         }
 
-        "ushort to bytes" {
-            val s: UShort = 0xFFAAu
-            val bytes = s.toBytes()
-            bytes.size shouldBe 2
-            bytes[0] shouldBe 0xFFu.toByte()
-            bytes[1] shouldBe 0xAAu.toByte()
+        describe("ushort") {
+
+            it("toBytesBE()") {
+                val s: UShort = 0xFFAAu
+                val bytes = s.toBytesBE()
+                bytes.size shouldBe 2
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xAAu.toByte()
+            }
+
+            it("toBytesLE()") {
+                val s: UShort = 0xFFAAu
+                val bytes = s.toBytesLE()
+                bytes.size shouldBe 2
+                bytes[0] shouldBe 0xAAu.toByte()
+                bytes[1] shouldBe 0xFFu.toByte()
+            }
+
+            it("toBytes()") {
+                val s: UShort = 0xFFAAu
+                val bytes = s.toBytes()
+                bytes.size shouldBe 2
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xAAu.toByte()
+            }
         }
 
-        "uint to bytes" {
-            val i = 0xFFAABBCCu
-            val bytes = i.toBytes()
-            bytes.size shouldBe 4
-            bytes[0] shouldBe 0xFFu.toByte()
-            bytes[1] shouldBe 0xAAu.toByte()
-            bytes[2] shouldBe 0xBBu.toByte()
-            bytes[3] shouldBe 0xCCu.toByte()
+        describe("uint") {
+
+            it("toBytesBE()") {
+                val i: UInt = 0xFFAABBCCu
+                val bytes = i.toBytesBE()
+                bytes.size shouldBe 4
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xAAu.toByte()
+                bytes[2] shouldBe 0xBBu.toByte()
+                bytes[3] shouldBe 0xCCu.toByte()
+            }
+
+            it("toBytesLE()") {
+                val i: UInt = 0xFFAABBCCu
+                val bytes = i.toBytesLE()
+                bytes.size shouldBe 4
+                bytes[0] shouldBe 0xCCu.toByte()
+                bytes[1] shouldBe 0xBBu.toByte()
+                bytes[2] shouldBe 0xAAu.toByte()
+                bytes[3] shouldBe 0xFFu.toByte()
+            }
+
+            it("toBytes()") {
+                val i: UInt = 0xFFAABBCCu
+                val bytes = i.toBytes()
+                bytes.size shouldBe 4
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xAAu.toByte()
+                bytes[2] shouldBe 0xBBu.toByte()
+                bytes[3] shouldBe 0xCCu.toByte()
+            }
         }
 
-        "ulong to bytes" {
-            val l = 0xFFAABBCCDDEE0011uL
-            val bytes = l.toBytes()
-            bytes.size shouldBe 8
-            bytes[0] shouldBe 0xFFu.toByte()
-            bytes[1] shouldBe 0xAAu.toByte()
-            bytes[2] shouldBe 0xBBu.toByte()
-            bytes[3] shouldBe 0xCCu.toByte()
-            bytes[4] shouldBe 0xDDu.toByte()
-            bytes[5] shouldBe 0xEEu.toByte()
-            bytes[6] shouldBe 0x00u.toByte()
-            bytes[7] shouldBe 0x11u.toByte()
-        }
+        describe("ulong") {
 
-        "byteOf" {
-            val b = byteOf(1)
-            b shouldBe 1
-        }
+            it("toBytesBE()") {
+                val l: ULong = 0xFFAABBCCDDEE0011uL
+                val bytes = l.toBytesBE()
+                bytes.size shouldBe 8
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xAAu.toByte()
+                bytes[2] shouldBe 0xBBu.toByte()
+                bytes[3] shouldBe 0xCCu.toByte()
+                bytes[4] shouldBe 0xDDu.toByte()
+                bytes[5] shouldBe 0xEEu.toByte()
+                bytes[6] shouldBe 0x00u.toByte()
+                bytes[7] shouldBe 0x11u.toByte()
+            }
 
-        "shortOf" {
-            val s = shortOf(1, 2)
-            s shouldBe 0x0102
-        }
+            it("toBytesLE()") {
+                val l: ULong = 0xFFAABBCCDDEE0011uL
+                val bytes = l.toBytesLE()
+                bytes.size shouldBe 8
+                bytes[0] shouldBe 0x11u.toByte()
+                bytes[1] shouldBe 0x00u.toByte()
+                bytes[2] shouldBe 0xEEu.toByte()
+                bytes[3] shouldBe 0xDDu.toByte()
+                bytes[4] shouldBe 0xCCu.toByte()
+                bytes[5] shouldBe 0xBBu.toByte()
+                bytes[6] shouldBe 0xAAu.toByte()
+                bytes[7] shouldBe 0xFFu.toByte()
+            }
 
-        "intOf" {
-            val i = intOf(1, 2, 3, 4)
-            i shouldBe 0x01020304
+            it("toBytes()") {
+                val l: ULong = 0xFFAABBCCDDEE0011uL
+                val bytes = l.toBytes()
+                bytes.size shouldBe 8
+                bytes[0] shouldBe 0xFFu.toByte()
+                bytes[1] shouldBe 0xAAu.toByte()
+                bytes[2] shouldBe 0xBBu.toByte()
+                bytes[3] shouldBe 0xCCu.toByte()
+                bytes[4] shouldBe 0xDDu.toByte()
+                bytes[5] shouldBe 0xEEu.toByte()
+                bytes[6] shouldBe 0x00u.toByte()
+                bytes[7] shouldBe 0x11u.toByte()
+            }
         }
+    }
 
-        "longOf" {
-            val l = longOf(1, 2, 3, 4, 5, 6, 7, 8)
-            l shouldBe 0x0102030405060708
-        }
+    it("byteOf") {
+        val b = byteOf(1)
+        b shouldBe 1
+    }
 
-        "floatOf" {
-            val f = floatOf(1, 2, 3, 4)
-            f.toBits() shouldBe 0x01020304
-        }
+    it("shortOf") {
+        val s = shortOf(1, 2)
+        s shouldBe 0x0102
+    }
 
-        "doubleOf" {
-            val d = doubleOf(1, 2, 3, 4, 5, 6, 7, 8)
-            d.toBits() shouldBe 0x0102030405060708
-        }
+    it("intOf") {
+        val i = intOf(1, 2, 3, 4)
+        i shouldBe 0x01020304
+    }
 
-        "ubyteOf" {
-            val b = ubyteOf(1)
-            b shouldBe 1u
-        }
+    it("longOf") {
+        val l = longOf(1, 2, 3, 4, 5, 6, 7, 8)
+        l shouldBe 0x0102030405060708
+    }
 
-        "ushortOf" {
-            val s = ushortOf(1, 2)
-            s shouldBe 0x0102u
-        }
+    it("floatOf") {
+        val f = floatOf(1, 2, 3, 4)
+        f.toBits() shouldBe 0x01020304
+    }
 
-        "uintOf" {
-            val i = uintOf(1, 2, 3, 4)
-            i shouldBe 0x01020304u
-        }
+    it("doubleOf") {
+        val d = doubleOf(1, 2, 3, 4, 5, 6, 7, 8)
+        d.toBits() shouldBe 0x0102030405060708
+    }
 
-        "ulongOf" {
-            val l = ulongOf(1, 2, 3, 4, 5, 6, 7, 8)
-            l shouldBe 0x0102030405060708uL
-        }
+    it("ubyteOf") {
+        val b = ubyteOf(1)
+        b shouldBe 1u
+    }
 
-        "unsignedByteOf" {
-            val b = unsignedByteOf(1)
-            b shouldBe 1u
-        }
+    it("ushortOf") {
+        val s = ushortOf(1, 2)
+        s shouldBe 0x0102u
+    }
 
-        "unsignedShortOf" {
-            val s = unsignedShortOf(1, 2)
-            s shouldBe 0x0102u
-        }
+    it("uintOf") {
+        val i = uintOf(1, 2, 3, 4)
+        i shouldBe 0x01020304u
+    }
 
-        "unsignedIntOf" {
-            val i = unsignedIntOf(1, 2, 3, 4)
-            i shouldBe 0x01020304u
-        }
+    it("ulongOf") {
+        val l = ulongOf(1, 2, 3, 4, 5, 6, 7, 8)
+        l shouldBe 0x0102030405060708uL
+    }
 
-        "unsignedLongOf" {
-            val l = unsignedLongOf(1, 2, 3, 4, 5, 6, 7, 8)
-            l shouldBe 0x0102030405060708uL
-        }
-    },
-)
+    it("unsignedByteOf") {
+        val b = unsignedByteOf(1)
+        b shouldBe 1u
+    }
+
+    it("unsignedShortOf") {
+        val s = unsignedShortOf(1, 2)
+        s shouldBe 0x0102u
+    }
+
+    it("unsignedIntOf") {
+        val i = unsignedIntOf(1, 2, 3, 4)
+        i shouldBe 0x01020304u
+    }
+
+    it("unsignedLongOf") {
+        val l = unsignedLongOf(1, 2, 3, 4, 5, 6, 7, 8)
+        l shouldBe 0x0102030405060708uL
+    }
+})
