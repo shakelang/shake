6ca8db82a05ef14e02e47ac3d982c0d19da9af2b
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-05-08 22:55:28 +0200

    Introduce CharSequence#decodeHexString()

--- util/primitives/src/commonMain/kotlin/com/shakelang/util/primitives/bytes/String.kt
+++ util/primitives/src/commonMain/kotlin/com/shakelang/util/primitives/bytes/String.kt
@@ -21,3 +21,21 @@ private fun CharSequence.encodeToByteArray(): ByteArray {
     }
     return bytes
 }
+
+/**
+ * Convert a [ByteArray] to a [String]
+ *
+ * @receiver The [ByteArray] to convert
+ * @return The [String] representation of the [ByteArray]
+ */
+fun CharSequence.decodeHexString(): ByteArray {
+    val len = this.length
+    require(len % 2 == 0) { "Invalid hex string" }
+    val result = ByteArray(len / 2)
+    for (i in 0 until len step 2) {
+        val first = this[i].toString().toInt(16)
+        val second = this[i + 1].toString().toInt(16)
+        result[i / 2] = ((first shl 4) + second).toByte()
+    }
+    return result
+}
